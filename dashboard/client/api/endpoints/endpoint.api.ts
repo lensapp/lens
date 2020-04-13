9c14d07e7782f8f1f1f7a6ab09fe8bd1ebf3b951
Lauri Nevala <lauri.nevala@gmail.com>
2020-04-13 18:20:19 +0300

    Implement endpoints section (#246)
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    Signed-off-by: Cody Belcher <cody.t.belcher@gmail.com>

---
+++ dashboard/client/api/endpoints/endpoint.api.ts
@@ -0,0 +1,122 @@
+import { autobind } from "../../utils";
+import { KubeObject } from "../kube-object";
+import { KubeApi } from "../kube-api";
+
+export interface IEndpointPort {
+  name?: string;
+  protocol: string;
+  port: number;
+}
+
+export interface IEndpointAddress {
+  hostname: string;
+  ip: string;
+  nodeName: string;
+}
+
+export interface IEndpointSubset {
+  addresses: IEndpointAddress[];
+  notReadyAddresses: IEndpointAddress[];
+  ports: IEndpointPort[];
+}
+
+interface ITargetRef {
+  kind: string;
+  namespace: string;
+  name: string;
+  uid: string;
+  resourceVersion: string;
+  apiVersion: string;
+}
+
+export class EndpointAddress implements IEndpointAddress {
+  hostname: string;
+  ip: string;
+  nodeName: string;
+  targetRef?: {
+    kind: string;
+    namespace: string;
+    name: string;
+    uid: string;
+    resourceVersion: string;
+  };
+
+  constructor(data: IEndpointAddress) {
+    Object.assign(this, data)
+  }
+
+  getId() {
+    return this.ip
+  }
+
+  getName() {
+    return this.hostname
+  }
+
+  getTargetRef(): ITargetRef {
+    if (this.targetRef) {
+      return Object.assign(this.targetRef, {apiVersion: "v1"})
+    } else {
+      return null
+    }
+  }
+}
+
+export class EndpointSubset implements IEndpointSubset {
+  addresses: IEndpointAddress[];
+  notReadyAddresses: IEndpointAddress[];
+  ports: IEndpointPort[];
+
+  constructor(data: IEndpointSubset) {
+    Object.assign(this, data)
+  }
+
+  getAddresses(): EndpointAddress[] {
+    const addresses = this.addresses || [];
+    return addresses.map(a => new EndpointAddress(a));
+  }
+
+  getNotReadyAddresses(): EndpointAddress[] {
+    const notReadyAddresses = this.notReadyAddresses || [];
+    return notReadyAddresses.map(a => new EndpointAddress(a));
+  }
+
+  toString(): string {
+    if(!this.addresses) {
+      return ""
+    }
+    return this.addresses.map(address => {
+      return this.ports.map(port => {
+        return `${address.ip}:${port.port}`
+      }).join(", ")
+    }).join(", ")
+  }
+}
+
+@autobind()
+export class Endpoint extends KubeObject {
+  static kind = "Endpoint"
+
+  subsets: IEndpointSubset[]
+
+  getEndpointSubsets(): EndpointSubset[] {
+    const subsets = this.subsets || [];
+    return subsets.map(s => new EndpointSubset(s));
+  }
+
+  toString(): string {
+    if(this.subsets) {
+      return this.getEndpointSubsets().map(es => es.toString()).join(", ")
+    } else {
+      return "<none>"
+    }
+  }
+
+}
+
+export const endpointApi = new KubeApi({
+  kind: Endpoint.kind,
+  apiBase: "/api/v1/endpoints",
+  isNamespaced: true,
+  objectConstructor: Endpoint,
+});
---
+++ dashboard/client/components/+network-endpoints/endpoint-subset-list.tsx
@@ -0,0 +1,164 @@
+import "./endpoint-subset-list.scss"
+
+import React from "react";
+import { observer } from "mobx-react";
+import { EndpointSubset, Endpoint, EndpointAddress} from "../../api/endpoints";
+import { _i18n } from "../../i18n";
+import { DrawerItem, DrawerTitle } from "../drawer";
+import { Trans } from "@lingui/macro";
+import { Table, TableCell, TableHead, TableRow } from "../table";
+import { autobind } from "../../utils";
+import { lookupApiLink } from "../../api/kube-api";
+import { getDetailsUrl } from "../../navigation";
+import { Link } from "react-router-dom";
+
+interface Props {
+  subset: EndpointSubset;
+  endpoint: Endpoint;
+}
+
+@observer
+export class EndpointSubsetList extends React.Component<Props> {
+
+  getAddressTableRow(ip: string) {
+    const { subset } = this.props;
+    const address = subset.getAddresses().find(address => address.getId() == ip);
+    return this.renderAddressTableRow(address)
+  }
+
+  @autobind()
+  getNotReadyAddressTableRow(ip: string) {
+    const { subset} = this.props;
+    const address = subset.getNotReadyAddresses().find(address => address.getId() == ip);
+    return this.renderAddressTableRow(address)
+  }
+
+  @autobind()
+  renderAddressTable(addresses: EndpointAddress[], virtual: boolean) {
+    return (
+      <div>
+        <div className="title flex gaps"><Trans>Addresses</Trans></div>
+        <Table
+          items={addresses}
+          selectable={false}
+          virtual={virtual}
+          scrollable={false}
+          getTableRow={this.getAddressTableRow}
+          className="box grow"
+        >
+          <TableHead>
+            <TableCell className="ip">IP</TableCell>
+            <TableCell className="name"><Trans>Hostname</Trans></TableCell>
+            <TableCell className="target">Target</TableCell>
+          </TableHead>
+          {
+            !virtual && addresses.map(address => this.getAddressTableRow(address.getId()))
+          }
+        </Table>
+      </div>
+    )
+  }
+
+  @autobind()
+  renderAddressTableRow(address: EndpointAddress) {
+    const { endpoint } = this.props;
+    return (
+      <TableRow
+        key={address.getId()}
+        nowrap
+      >
+        <TableCell className="ip">{address.ip}</TableCell>
+        <TableCell className="name">{address.hostname}</TableCell>
+        <TableCell className="target">
+          { address.targetRef && (
+          <Link to={getDetailsUrl(lookupApiLink(address.getTargetRef(), endpoint))}>
+                {address.targetRef.name}
+          </Link>
+          )}
+        </TableCell>
+      </TableRow>
+    );
+  }
+
+  render() {
+    const { subset } = this.props;
+    const addresses = subset.getAddresses();
+    const notReadyAddresses = subset.getNotReadyAddresses();
+    const addressesVirtual = addresses.length > 100;
+    const notReadyAddressesVirtual = notReadyAddresses.length > 100;
+
+    return(
+      <div className="EndpointSubsetList flex column">
+        {addresses.length > 0 && (
+        <div>
+          <div className="title flex gaps"><Trans>Addresses</Trans></div>
+          <Table
+            items={addresses}
+            selectable={false}
+            virtual={addressesVirtual}
+            scrollable={false}
+            getTableRow={this.getAddressTableRow}
+            className="box grow"
+          >
+            <TableHead>
+              <TableCell className="ip">IP</TableCell>
+              <TableCell className="host"><Trans>Hostname</Trans></TableCell>
+              <TableCell className="target">Target</TableCell>
+            </TableHead>
+            { !addressesVirtual && addresses.map(address => this.getAddressTableRow(address.getId())) }
+          </Table>
+        </div>
+        )}
+
+        {notReadyAddresses.length > 0 && (
+        <div>
+          <div className="title flex gaps"><Trans>Not Ready Addresses</Trans></div>
+          <Table
+            items={notReadyAddresses}
+            selectable
+            virtual={notReadyAddressesVirtual}
+            scrollable={false}
+            getTableRow={this.getNotReadyAddressTableRow}
+            className="box grow"
+          >
+            <TableHead>
+              <TableCell className="ip">IP</TableCell>
+              <TableCell className="host"><Trans>Hostname</Trans></TableCell>
+              <TableCell className="target">Target</TableCell>
+            </TableHead>
+            { !notReadyAddressesVirtual && notReadyAddresses.map(address => this.getNotReadyAddressTableRow(address.getId())) }
+          </Table>
+        </div>
+        )}
+
+        <div className="title flex gaps"><Trans>Ports</Trans></div>
+        <Table
+          selectable={false}
+          virtual={false}
+          scrollable={false}
+          className="box grow"
+        >
+          <TableHead>
+            <TableCell className="port"><Trans>Port</Trans></TableCell>
+            <TableCell className="name"><Trans>Name</Trans></TableCell>
+            <TableCell className="protocol">Protocol</TableCell>
+          </TableHead>
+          {
+            subset.ports.map(port => {
+              return (
+                <TableRow
+                  key={port.port}
+                  nowrap
+                >
+                  <TableCell className="name">{port.port}</TableCell>
+                  <TableCell className="name">{port.name}</TableCell>
+                  <TableCell className="node">{port.protocol}</TableCell>
+                </TableRow>
+              );
+            })
+          }
+        </Table>
+      </div>
+    );
+  }
+}
