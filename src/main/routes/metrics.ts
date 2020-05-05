b57ce73ac73513a551c3e5249b7fce3ca9fdb0df
Lauri Nevala <lauri.nevala@gmail.com>
2020-05-05 18:40:42 +0300

    Clean up
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/routes/metrics.ts
+++ src/main/routes/metrics.ts
@@ -2,7 +2,6 @@ import { LensApiRequest } from "../router"
 import { LensApi } from "../lens-api"
 import * as requestPromise from "request-promise-native"
 import { PrometheusProviderRegistry, PrometheusProvider, PrometheusNodeQuery, PrometheusClusterQuery, PrometheusPodQuery, PrometheusPvcQuery, PrometheusIngressQuery, PrometheusQueryOpts} from "../prometheus/provider-registry"
-import logger from "../logger"
 
 type MetricsQuery = string | string[] | {
   [metricName: string]: string;
@@ -15,7 +14,6 @@ class MetricsRoute extends LensApi {
     const query: MetricsQuery = request.payload;
     const serverUrl = `http://127.0.0.1:${cluster.port}/api-kube`
     const metricsUrl = `${serverUrl}/api/v1/namespaces/${cluster.contextHandler.getPrometheusPath()}/proxy${cluster.getPrometheusApiPrefix()}/api/v1/query_range`
-    logger.info(metricsUrl)
     const headers = {
       "Host": `${cluster.id}.localhost:${cluster.port}`,
       "Content-type": "application/json",
