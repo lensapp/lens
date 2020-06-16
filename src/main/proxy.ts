1cce5bfe31a67bf6de1f09afbeb6979d2f61d59c
Lauri Nevala <lauri.nevala@gmail.com>
2020-06-16 14:06:37 +0300

    Remove redundant applyHeaders method
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/proxy.ts
+++ src/main/proxy.ts
@@ -155,13 +155,6 @@ export class LensProxy {
       return
     }
     const contextHandler = cluster.contextHandler
-    try {
-      contextHandler.applyHeaders(req)
-    } catch (error) {
-      res.statusCode = 503
-      res.end()
-      return
-    }
     contextHandler.ensureServer().then(async () => {
       const proxyTarget = await this.getProxyTarget(req, contextHandler)
       if (proxyTarget) {
@@ -174,9 +167,6 @@ export class LensProxy {
 
   protected async handleWsUpgrade(req: http.IncomingMessage, socket: Socket, head: Buffer) {
     const wsServer = this.createWsListener();
-    const cluster = this.clusterManager.getClusterForRequest(req)
-    const contextHandler = cluster.contextHandler
-    contextHandler.applyHeaders(req);
     wsServer.handleUpgrade(req, socket, head, (con) => {
       wsServer.emit("connection", con, req);
     });
