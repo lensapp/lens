15323e3ae28d0c3e0b523371dc415c568cc12908
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-06-18 09:55:49 +0300

    enable nodeintegration for react
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/renderer/_vue/components/ClusterPage.vue
+++ src/renderer/_vue/components/ClusterPage.vue
@@ -108,6 +108,7 @@ export default {
       if (!this.lens.webview) {
         console.log("create webview")
         const webview = document.createElement('webview');
+        webview.setAttribute("nodeintegration", "true");
         webview.addEventListener('did-finish-load', this.lensLoaded);
         webview.src = this.cluster.url;
         this.lens.webview = webview;
