1e401665843a8970261aeb4d32b64f70d7b9a6b8
Sebastian Malton <smalton@mirantis.com>
2020-06-22 11:23:37 -0400

    fix precedence of union of env objects
    Signed-off-by: Sebastian Malton <smalton@mirantis.com>

--- src/main/shell-sync.ts
+++ src/main/shell-sync.ts
@@ -27,8 +27,9 @@ export function shellSync(locale: string) {
     process.env["PATH"] = env.PATH
   }
 
+  // The spread operator allows joining of objects. The precidence is last to first.
   process.env = {
+    ...env,
     ...process.env,
-    ...env
   };
 }
