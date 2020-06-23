dd5e66ba67993ba1af19e3e316106ce20953c78b
Sebastian Malton <sebastian@malton.name>
2020-06-23 08:37:35 -0400

    Fix pasting unicode into terminal sometimes not working (#492)
    * add LANG env with UTF-8 specified
    
    * fix precedence of union of env objects
    
    Signed-off-by: Sebastian Malton <smalton@mirantis.com>
    
    Co-authored-by: Sebastian Malton <smalton@mirantis.com>

--- src/main/shell-sync.ts
+++ src/main/shell-sync.ts
@@ -1,18 +1,35 @@
 import shellEnv = require("shell-env")
 import logger from "./logger"
+import * as os from "os";
 
-export async function shellSync() {
-  const env = await shellEnv()
+interface Env {
+  [key: string]: string;
+}
+
+/**
+ * shellSync loads what would have been the environment if this application was
+ * run from the command line, into the process.env object. This is especially
+ * useful on macos where this always needs to be done.
+ * @param locale Should be electron's `app.getLocale()`
+ */
+export function shellSync(locale: string) {
+  const { shell } = os.userInfo();
+  const env: Env = JSON.parse(JSON.stringify(shellEnv.sync(shell)))
+  if (!env.LANG) {
+    // the LANG env var expects an underscore instead of electron's dash
+    env.LANG = `${locale.replace('-', '_')}.UTF-8`;
+  } else if (!env.LANG.endsWith(".UTF-8")) {
+    env.LANG += ".UTF-8"
+  }
 
   // Overwrite PATH on darwin
   if (process.env.NODE_ENV === "production" && process.platform === "darwin") {
     process.env["PATH"] = env.PATH
   }
 
-  let key = null
-  for(key in env) {
-    if(!env.hasOwnProperty(key) || process.env[key]) continue // skip existing and prototype keys
-    logger.debug("Imported " + key + " from login shell to process environment")
-    process.env[key] = env[key]
-  }
+  // The spread operator allows joining of objects. The precedence is last to first.
+  process.env = {
+    ...env,
+    ...process.env,
+  };
 }
