d7abfe38579fd318918a9a22135282865716c26c
Roman <ixrock@gmail.com>
2020-07-23 16:01:32 +0300

    preferences page -- part 2
    Signed-off-by: Roman <ixrock@gmail.com>

--- src/renderer/theme.store.ts
+++ src/renderer/theme.store.ts
@@ -1,126 +1,20 @@
 import { action, autorun, computed, observable, reaction } from "mobx";
 import { autobind, createStorage } from "./utils";
 import { Notifications } from "./components/notifications";
-
-interface ITheme {
-  name: string;
-  type: "dark" | "light";
-  author?: string;
-  colors?: {
-    [key: string]: string;
-    blue: string;
-    magenta: string;
-    golden: string;
-    halfGray: string;
-    primary: string;
-    textColorPrimary: string;
-    textColorSecondary: string;
-    textColorAccent: string;
-    borderColor: string;
-    borderFaintColor: string;
-    mainBackground: string;
-    contentColor: string;
-    layoutBackground: string;
-    layoutTabsBackground: string;
-    layoutTabsActiveColor: string;
-    layoutTabsLineColor: string;
-    sidebarLogoBackground: string;
-    sidebarActiveColor: string;
-    sidebarSubmenuActiveColor: string;
-    sidebarBackground: string;
-    buttonPrimaryBackground: string;
-    buttonDefaultBackground: string;
-    buttonAccentBackground: string;
-    buttonDisabledBackground: string;
-    tableBgcStripe: string;
-    tableBgcSelected: string;
-    tableHeaderBackground: string;
-    tableHeaderBorderWidth: string;
-    tableHeaderBorderColor: string;
-    tableHeaderColor: string;
-    tableSelectedRowColor: string;
-    helmLogoBackground: string;
-    helmImgBackground: string;
-    helmStableRepo: string;
-    helmIncubatorRepo: string;
-    helmDescriptionHr: string;
-    helmDescriptionBlockqouteColor: string;
-    helmDescriptionBlockqouteBorder: string;
-    helmDescriptionBlockquoteBackground: string;
-    helmDescriptionHeaders: string;
-    helmDescriptionH6: string;
-    helmDescriptionTdBorder: string;
-    helmDescriptionTrBackground: string;
-    helmDescriptionCodeBackground: string;
-    helmDescriptionPreBackground: string;
-    helmDescriptionPreColor: string;
-    colorSuccess: string;
-    colorOk: string;
-    colorInfo: string;
-    colorError: string;
-    colorSoftError: string;
-    colorWarning: string;
-    colorVague: string;
-    colorTerminated: string;
-    dockHeadBackground: string;
-    dockInfoBackground: string;
-    dockInfoBorderColor: string;
-    terminalBackground: string;
-    terminalForeground: string;
-    terminalCursor: string;
-    terminalCursorAccent: string;
-    terminalSelection: string;
-    terminalBlack: string;
-    terminalRed: string;
-    terminalGreen: string;
-    terminalYellow: string;
-    terminalBlue: string;
-    terminalMagenta: string;
-    terminalCyan: string;
-    terminalWhite: string;
-    terminalBrightBlack: string;
-    terminalBrightRed: string;
-    terminalBrightGreen: string;
-    terminalBrightYellow: string;
-    terminalBrightBlue: string;
-    terminalBrightMagenta: string;
-    terminalBrightCyan: string;
-    terminalBrightWhite: string;
-    dialogTextColor: string;
-    dialogBackground: string;
-    dialogHeaderBackground: string;
-    dialogFooterBackground: string;
-    drawerTogglerBackground: string;
-    drawerTitleText: string;
-    drawerSubtitleBackground: string;
-    drawerItemNameColor: string;
-    drawerItemValueColor: string;
-    boxShadow: string;
-    iconActiveColor: string;
-    iconActiveBackground: string;
-    filterAreaBackground: string;
-    chartLiveBarBackgound: string;
-    chartStripesColor: string;
-    chartCapacityColor: string;
-    pieChartDefaultColor: string;
-    selectOptionHoveredColor: string;
-    lineProgressBackground: string;
-    radioActiveBackground: string;
-  };
-}
+import { Theme, ThemeType } from "../common/user-store";
 
 @autobind()
 export class ThemeStore {
   protected style = document.createElement("style");
 
-  readonly defaultTheme: ITheme = {
+  readonly defaultTheme: Theme = {
     name: "kontena-dark",
-    type: "dark",
-    colors: {} as any,
+    type: ThemeType.DARK,
+    colors: {},
   };
 
   @observable activeThemeId = this.defaultTheme.name; // theme's filename without extension
-  @observable themes = observable.map<string, ITheme>([], { deep: false });
+  @observable themes = observable.map<string, Theme>([], { deep: false });
 
   @computed get activeTheme() {
     return this.themes.get(this.activeThemeId) || this.defaultTheme;
@@ -157,7 +51,7 @@ export class ThemeStore {
     })
   }
 
-  protected onChange = (theme: ITheme) => {
+  protected onChange = (theme: Theme) => {
     let cssText = "\n"
     Object.entries(theme.colors).forEach(([propName, color]) => {
       cssText += `--${propName}: ${color} !important;\n`
@@ -165,12 +59,12 @@ export class ThemeStore {
     this.style.textContent = `:root {${cssText}} `;
   }
 
-  async load(themeId: string, { showErrorNotification = true } = {}): Promise<ITheme> {
+  async load(themeId: string, { showErrorNotification = true } = {}): Promise<Theme> {
     if (this.themes.has(themeId)) {
       return this.themes.get(themeId);
     }
     try {
-      const theme: ITheme = require( // eslint-disable-line @typescript-eslint/no-var-requires
+      const theme: Theme = require( // eslint-disable-line @typescript-eslint/no-var-requires
         /* webpackMode: "lazy", webpackChunkName: "theme/[request]" */
         `./themes/${themeId}.json`
       );
