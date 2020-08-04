06d76ead74e93d971d3e31547727793e0f44f1c2
alexfront <alex.andreev.email@gmail.com>
2020-08-04 15:46:42 +0300

    Fixing Cluster Settings general layout bugs
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>

--- src/renderer/components/+cluster-settings/cluster-settings.tsx
+++ src/renderer/components/+cluster-settings/cluster-settings.tsx
@@ -1,25 +1,38 @@
-import "./cluster-settings.scss"
+import "./cluster-settings.scss";
+
 import React from "react";
 import { observer } from "mobx-react";
-import { Features } from "./features" 
-import { Removal } from "./removal"
-import { Status } from "./status"
-import { General } from "./general"
-import { getHostedCluster } from "../../../common/cluster-store"
+import { Features } from "./features";
+import { Removal } from "./removal";
+import { Status } from "./status";
+import { General } from "./general";
+import { getHostedCluster } from "../../../common/cluster-store";
 import { WizardLayout } from "../layout/wizard-layout";
+import { ClusterIcon } from "../cluster-icon";
 
 @observer
 export class ClusterSettings extends React.Component {
   render() {
     const cluster = getHostedCluster();
-    
+    const header = (
+      <>
+        <ClusterIcon
+          cluster={cluster}
+          showErrors={false}
+          showTooltip={false}
+        />
+        <h2>{cluster.preferences.clusterName}</h2>
+      </>
+    );
     return (
-      <WizardLayout className="ClusterSettings">
-        <Status cluster={cluster}></Status>
-        <General cluster={cluster}></General>
-        <Features cluster={cluster}></Features>
-        <Removal cluster={cluster}></Removal>
+      <WizardLayout header={header} className="ClusterSettings">
+        <div className="settings-wrapper">
+          <Status cluster={cluster}></Status>
+          <General cluster={cluster}></General>
+          <Features cluster={cluster}></Features>
+          <Removal cluster={cluster}></Removal>
+        </div>
       </WizardLayout>
-    )
+    );
   }
 }
