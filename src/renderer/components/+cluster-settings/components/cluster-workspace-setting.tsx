f533331864cc1dec9c354979b9746b96aa97b14d
alexfront <alex.andreev.email@gmail.com>
2020-08-05 14:33:46 +0300

    ClusterWorkspaceSettings refactoring/fixing
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>

--- src/renderer/components/+cluster-settings/components/cluster-workspace-setting.tsx
+++ src/renderer/components/+cluster-settings/components/cluster-workspace-setting.tsx
@@ -1,36 +1,36 @@
 import React from "react";
-import { Cluster } from "../../../../main/cluster";
-import { clusterStore } from "../../../../common/cluster-store"
-import { workspaceStore } from "../../../../common/workspace-store"
-import { Select, SelectOption } from "../../../components/select";
-import { GeneralInputStatus } from "./statuses"
-import { observable } from "mobx";
-import { autobind } from "../../../utils";
 import { observer } from "mobx-react";
+import { Link } from "react-router-dom";
+import { workspacesURL } from "../../+workspaces";
+import { workspaceStore } from "../../../../common/workspace-store";
+import { Cluster } from "../../../../main/cluster";
+import { Select } from "../../../components/select";
+import { SubTitle } from "../../layout/sub-title";
 
 interface Props {
-    cluster: Cluster;
+  cluster: Cluster;
 }
 
 @observer
 export class ClusterWorkspaceSetting extends React.Component<Props> {
-  @observable workspace = this.props.cluster.workspace;
-
   render() {
-    return <>
-      <h4>Cluster Workspace</h4>
-      <p>Change cluster workspace:</p>
-      <Select
-        value={workspaceStore.currentWorkspaceId}
-        options={workspaceStore.workspacesList.map(w => ({value: w.id, label: <span>{w.name}</span>}))}
-        onChange={this.changeWorkspace}
-      />
-    </>;
-  }
-
-  @autobind()
-  changeWorkspace({ value: workspace }: SelectOption<string>) {
-    this.workspace = workspace;
-    this.props.cluster.workspace = workspace;
+    return (
+      <>
+        <SubTitle title="Cluster Workspace"/>
+        <p>
+          Define cluster{" "}
+          <Link to={workspacesURL()}>
+            workspace
+          </Link>.
+        </p>
+        <Select
+          value={this.props.cluster.workspace}
+          onChange={({value}) => this.props.cluster.workspace = value}
+          options={workspaceStore.workspacesList.map(w =>
+            ({value: w.id, label: w.name})
+          )}
+        />
+      </>
+    );
   }
 }
