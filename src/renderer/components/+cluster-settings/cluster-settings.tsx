fb124ada7dd6301f9ce72d5338c7feaea0e01ead
Alex Andreev <alex.andreev.email@gmail.com>
2020-08-17 13:55:46 +0300

    Preferences section restyling (#700)
    * Adding 'centering' option to WizardLayout
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Using centered layout in Cluster Settings
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Restyling Preferences section
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Simplifying Helm loading state
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/renderer/components/+cluster-settings/cluster-settings.tsx
+++ src/renderer/components/+cluster-settings/cluster-settings.tsx
@@ -1,7 +1,6 @@
 import "./cluster-settings.scss";
 
 import React from "react";
-import { Link } from "react-router-dom";
 import { observer } from "mobx-react";
 import { Features } from "./features";
 import { Removal } from "./removal";
@@ -11,6 +10,7 @@ import { WizardLayout } from "../layout/wizard-layout";
 import { ClusterIcon } from "../cluster-icon";
 import { Icon } from "../icon";
 import { getMatchedCluster } from "../cluster-manager/cluster-view.route";
+import { navigate } from "../../navigation";
 
 @observer
 export class ClusterSettings extends React.Component {
@@ -25,20 +25,16 @@ export class ClusterSettings extends React.Component {
           showTooltip={false}
         />
         <h2>{cluster.preferences.clusterName}</h2>
-        <Link to="/">
-          <Icon material="close" big/>
-        </Link>
+        <Icon material="close" onClick={() => navigate("/")} big/>
       </>
     );
     return (
       <div className="ClusterSettings">
-        <WizardLayout header={header}>
-          <div className="settings-wrapper">
-            <Status cluster={cluster}></Status>
-            <General cluster={cluster}></General>
-            <Features cluster={cluster}></Features>
-            <Removal cluster={cluster}></Removal>
-          </div>
+        <WizardLayout header={header} centered>
+          <Status cluster={cluster}></Status>
+          <General cluster={cluster}></General>
+          <Features cluster={cluster}></Features>
+          <Removal cluster={cluster}></Removal>
         </WizardLayout>
       </div>
     );
