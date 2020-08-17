405163adbe5ba22febbca099e668145946e9ef57
Alex Andreev <alex.andreev.email@gmail.com>
2020-08-17 13:55:46 +0300

    Preferences section restyling (#700)
    * Adding 'centering' option to WizardLayout
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Using centered layout in Cluster Settings
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Restyling Preferences section
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>
    
    * Simplifying Helm loading state
    
    Signed-off-by: alexfront <alex.andreev.email@gmail.com>

--- src/renderer/components/+preferences/preferences.tsx
+++ src/renderer/components/+preferences/preferences.tsx
@@ -1,5 +1,5 @@
 import "./preferences.scss"
-import React, { Fragment } from "react";
+import React from "react";
 import { observer } from "mobx-react";
 import { action, computed, observable } from "mobx";
 import { t, Trans } from "@lingui/macro";
@@ -15,11 +15,12 @@ import { Notifications } from "../notifications";
 import { Badge } from "../badge";
 import { Spinner } from "../spinner";
 import { themeStore } from "../../theme.store";
+import { history } from "../../navigation";
+import { Tooltip } from "../tooltip";
 
 @observer
 export class Preferences extends React.Component {
   @observable helmLoading = false;
-  @observable helmUpdating = false;
   @observable helmRepos: HelmRepo[] = [];
   @observable helmAddedRepos = observable.map<string, HelmRepo>();
 
@@ -88,9 +89,9 @@ export class Preferences extends React.Component {
       Notifications.ok(<Trans>Helm branch <b>{repo.name}</b> already in use</Trans>)
       return;
     }
-    this.helmUpdating = false;
+    this.helmLoading = true;
     await this.addRepo(repo);
-    this.helmUpdating = false;
+    this.helmLoading = false;
   }
 
   formatHelmOptionLabel = ({ value: repo }: SelectOption<HelmRepo>) => {
@@ -103,104 +104,96 @@ export class Preferences extends React.Component {
     )
   }
 
-  renderInfo() {
-    return (
-      <Fragment>
-        <h2>
-          <Trans>Preferences</Trans>
-        </h2>
-        <div className="info-block flex gaps align-flex-start">
-          <Icon small material="info"/>
-          <p>
-            <Trans>Lens Global Settings</Trans> (<Trans>applicable to all clusters</Trans>)
-          </p>
-        </div>
-      </Fragment>
-    )
-  }
-
   render() {
     const { preferences } = userStore;
+    const header = (
+      <>
+        <h2>Preferences</h2>
+        <Icon material="close" big onClick={history.goBack}/>
+      </>
+    );
     return (
-      <WizardLayout className="Preferences" infoPanel={this.renderInfo()}>
-        <h2><Trans>Color Theme</Trans></h2>
-        <Select
-          options={this.themeOptions}
-          value={preferences.colorTheme}
-          onChange={({ value }: SelectOption) => preferences.colorTheme = value}
-        />
+      <div className="Preferences">
+        <WizardLayout header={header} centered>
+          <h2><Trans>Color Theme</Trans></h2>
+          <Select
+            options={this.themeOptions}
+            value={preferences.colorTheme}
+            onChange={({ value }: SelectOption) => preferences.colorTheme = value}
+          />
 
-        <h2><Trans>Download Mirror</Trans></h2>
-        <Select
-          placeholder={<Trans>Download mirror for kubectl</Trans>}
-          options={this.downloadMirrorOptions}
-          value={preferences.downloadMirror}
-          onChange={({ value }: SelectOption) => preferences.downloadMirror = value}
-        />
+          <h2><Trans>Download Mirror</Trans></h2>
+          <Select
+            placeholder={<Trans>Download mirror for kubectl</Trans>}
+            options={this.downloadMirrorOptions}
+            value={preferences.downloadMirror}
+            onChange={({ value }: SelectOption) => preferences.downloadMirror = value}
+          />
 
-        <h2><Trans>Helm</Trans></h2>
-        <Select
-          placeholder={<Trans>Repositories</Trans>}
-          isLoading={this.helmLoading}
-          isDisabled={this.helmUpdating}
-          options={this.helmOptions}
-          onChange={this.onRepoSelect}
-          formatOptionLabel={this.formatHelmOptionLabel}
-          controlShouldRenderValue={false}
-        />
-        <div className="repos flex gaps align-center">
-          <div className="title">
-            <Trans>Added repos:</Trans>
-          </div>
-          <div className="repos-list">
-            {this.helmLoading && <Spinner/>}
+          <h2><Trans>Helm</Trans></h2>
+          <Select
+            placeholder={<Trans>Repositories</Trans>}
+            isLoading={this.helmLoading}
+            isDisabled={this.helmLoading}
+            options={this.helmOptions}
+            onChange={this.onRepoSelect}
+            formatOptionLabel={this.formatHelmOptionLabel}
+            controlShouldRenderValue={false}
+          />
+          <div className="repos flex gaps column">
+            
             {Array.from(this.helmAddedRepos).map(([name, repo]) => {
+              const tooltipId = `message-${name}`;
               return (
-                <Badge key={name} className="added-repo flex gaps align-center" title={repo.url}>
-                  <span className="repo">{name}</span>
+                <Badge key={name} className="added-repo flex gaps align-center justify-space-between">
+                  <span id={tooltipId} className="repo">{name}</span>
                   <Icon
-                    material="remove_circle_outline"
+                    material="delete"
                     onClick={() => this.removeRepo(repo)}
                     tooltip={<Trans>Remove</Trans>}
                   />
+                  <Tooltip targetId={tooltipId} formatters={{ narrow: true }}>
+                    {repo.url}
+                  </Tooltip>
                 </Badge>
               )
             })}
           </div>
-        </div>
 
-        <h2><Trans>HTTP Proxy</Trans></h2>
-        <Input
-          placeholder={_i18n._(t`Type HTTP proxy url (example: http://proxy.acme.org:8080)`)}
-          value={preferences.httpsProxy || ""}
-          onChange={v => preferences.httpsProxy = v}
-        />
-        <small className="hint">
-          <Trans>Proxy is used only for non-cluster communication.</Trans>
-        </small>
+          <h2><Trans>HTTP Proxy</Trans></h2>
+          <Input
+            theme="round-black"
+            placeholder={_i18n._(t`Type HTTP proxy url (example: http://proxy.acme.org:8080)`)}
+            value={preferences.httpsProxy || ""}
+            onChange={v => preferences.httpsProxy = v}
+          />
+          <small className="hint">
+            <Trans>Proxy is used only for non-cluster communication.</Trans>
+          </small>
 
-        <h2><Trans>Certificate Trust</Trans></h2>
-        <Checkbox
-          label={<Trans>Allow untrusted Certificate Authorities</Trans>}
-          value={preferences.allowUntrustedCAs}
-          onChange={v => preferences.allowUntrustedCAs = v}
-        />
-        <small className="hint">
-          <Trans>This will make Lens to trust ANY certificate authority without any validations.</Trans>{" "}
-          <Trans>Needed with some corporate proxies that do certificate re-writing.</Trans>{" "}
-          <Trans>Does not affect cluster communications!</Trans>
-        </small>
+          <h2><Trans>Certificate Trust</Trans></h2>
+          <Checkbox
+            label={<Trans>Allow untrusted Certificate Authorities</Trans>}
+            value={preferences.allowUntrustedCAs}
+            onChange={v => preferences.allowUntrustedCAs = v}
+          />
+          <small className="hint">
+            <Trans>This will make Lens to trust ANY certificate authority without any validations.</Trans>{" "}
+            <Trans>Needed with some corporate proxies that do certificate re-writing.</Trans>{" "}
+            <Trans>Does not affect cluster communications!</Trans>
+          </small>
 
-        <h2><Trans>Telemetry & Usage Tracking</Trans></h2>
-        <Checkbox
-          label={<Trans>Allow telemetry & usage tracking</Trans>}
-          value={preferences.allowTelemetry}
-          onChange={v => preferences.allowTelemetry = v}
-        />
-        <small className="hint">
-          <Trans>Telemetry & usage data is collected to continuously improve the Lens experience.</Trans>
-        </small>
-      </WizardLayout>
-    )
+          <h2><Trans>Telemetry & Usage Tracking</Trans></h2>
+          <Checkbox
+            label={<Trans>Allow telemetry & usage tracking</Trans>}
+            value={preferences.allowTelemetry}
+            onChange={v => preferences.allowTelemetry = v}
+          />
+          <small className="hint">
+            <Trans>Telemetry & usage data is collected to continuously improve the Lens experience.</Trans>
+          </small>
+        </WizardLayout>
+      </div>
+    );
   }
 }
--- src/renderer/components/layout/wizard-layout.tsx
+++ src/renderer/components/layout/wizard-layout.tsx
@@ -10,25 +10,30 @@ interface Props {
   contentClass?: IClassName;
   infoPanelClass?: IClassName;
   infoPanel?: React.ReactNode;
+  centered?: boolean;  // Centering content horizontally
 }
 
 @observer
 export class WizardLayout extends React.Component<Props> {
   render() {
-    const { className, contentClass, infoPanelClass, infoPanel, header, headerClass, children: content } = this.props;
+    const { className, contentClass, infoPanelClass, infoPanel, header, headerClass, centered, children: content } = this.props;
     return (
-      <div className={cssNames("WizardLayout", className)}>
+      <div className={cssNames("WizardLayout", { centered }, className)}>
         {header && (
           <div className={cssNames("head-col flex gaps align-center", headerClass)}>
             {header}
           </div>
         )}
         <div className={cssNames("content-col flex column gaps", contentClass)}>
-          {content}
+          <div className="flex column gaps">
+            {content}
+          </div>
         </div>
-        <div className={cssNames("info-col flex column gaps", infoPanelClass)}>
-          {infoPanel}
-        </div>
+        {infoPanel && (
+          <div className={cssNames("info-col flex column gaps", infoPanelClass)}>
+            {infoPanel}
+          </div>
+        )}
       </div>
     )
   }
