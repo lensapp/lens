f316ff3cf89bd8d7d92a4c5411f5274c5246c140
Dan Slinky <dan-slinky@cookpad.jp>
2020-08-20 20:03:36 +0100

    Add support for PodDisruptionBudgets
    Signed-off-by: Dan Slinky <dan-slinky@cookpad.jp>

---
+++ src/renderer/components/+config-pod-disruption-budgets/index.ts
@@ -0,0 +1,3 @@
+export * from "./pod-disruption-budgets.route"
+export * from "./pod-disruption-budgets"
+export * from "./pod-disruption-budgets-details"
---
+++ src/renderer/components/+config-pod-disruption-budgets/pod-disruption-budgets.store.ts
@@ -0,0 +1,12 @@
+import { KubeObjectStore } from "../../kube-object.store";
+import { autobind } from "../../utils";
+import { PodDisruptionBudget, pdbApi } from "../../api/endpoints/poddisruptionbudget.api";
+import { apiManager } from "../../api/api-manager";
+
+@autobind()
+export class PodDisruptionBudgetsStore extends KubeObjectStore<PodDisruptionBudget> {
+  api = pdbApi
+}
+
+export const podDisruptionBudgetsStore = new PodDisruptionBudgetsStore();
+apiManager.registerStore(pdbApi, podDisruptionBudgetsStore);
