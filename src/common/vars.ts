4b5a4d8d1d58084290772f66223fb97099442341
Roman <ixrock@gmail.com>
2020-08-27 16:21:53 +0300

    revert `process.env.DEBUG` to "true"
    Signed-off-by: Roman <ixrock@gmail.com>

--- src/common/vars.ts
+++ src/common/vars.ts
@@ -5,7 +5,7 @@ import { defineGlobal } from "./utils/defineGlobal";
 
 export const isMac = process.platform === "darwin"
 export const isWindows = process.platform === "win32"
-export const isDebugging = process.env.DEBUG === "lens";
+export const isDebugging = process.env.DEBUG === "true";
 export const isProduction = process.env.NODE_ENV === "production"
 export const isDevelopment = isDebugging || !isProduction;
 export const isTestEnv = !!process.env.JEST_WORKER_ID;
