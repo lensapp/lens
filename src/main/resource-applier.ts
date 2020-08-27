1d672b68fc5da650c71b798657cdfdf2eccb6996
Lauri Nevala <lauri.nevala@gmail.com>
2020-08-27 13:07:40 +0300

    Use proxy kubeconfig for shell and resource applier
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/resource-applier.ts
+++ src/main/resource-applier.ts
@@ -20,12 +20,12 @@ export class ResourceApplier {
   }
 
   protected async kubectlApply(content: string): Promise<string> {
-    const { kubeCtl, kubeConfigPath } = this.cluster;
+    const { kubeCtl } = this.cluster;
     const kubectlPath = await kubeCtl.getPath()
     return new Promise<string>((resolve, reject) => {
       const fileName = tempy.file({ name: "resource.yaml" })
       fs.writeFileSync(fileName, content)
-      const cmd = `"${kubectlPath}" apply --kubeconfig "${kubeConfigPath}" -o json -f "${fileName}"`
+      const cmd = `"${kubectlPath}" apply --kubeconfig "${this.cluster.getProxyKubeconfigPath()}" -o json -f "${fileName}"`
       logger.debug("shooting manifests with: " + cmd);
       const execEnv: NodeJS.ProcessEnv = Object.assign({}, process.env)
       const httpsProxy = this.cluster.preferences?.httpsProxy
@@ -46,7 +46,7 @@ export class ResourceApplier {
   }
 
   public async kubectlApplyAll(resources: string[]): Promise<string> {
-    const { kubeCtl, kubeConfigPath } = this.cluster;
+    const { kubeCtl } = this.cluster;
     const kubectlPath = await kubeCtl.getPath()
     return new Promise((resolve, reject) => {
       const tmpDir = tempy.directory()
@@ -54,7 +54,7 @@ export class ResourceApplier {
       resources.forEach((resource, index) => {
         fs.writeFileSync(path.join(tmpDir, `${index}.yaml`), resource);
       })
-      const cmd = `"${kubectlPath}" apply --kubeconfig "${kubeConfigPath}" -o json -f "${tmpDir}"`
+      const cmd = `"${kubectlPath}" apply --kubeconfig "${this.cluster.getProxyKubeconfigPath()}" -o json -f "${tmpDir}"`
       console.log("shooting manifests with:", cmd);
       exec(cmd, (error, stdout, stderr) => {
         if (error) {
