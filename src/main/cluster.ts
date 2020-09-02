61c3e32a7c2c585b18c4d8a0bed8390e92913533
Lauri Nevala <lauri.nevala@gmail.com>
2020-09-02 15:28:02 +0300

    Try to reconnect non-accessible clusters on activate (#789)
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/cluster.ts
+++ src/main/cluster.ts
@@ -130,7 +130,7 @@ export class Cluster implements ClusterModel {
     if (!this.eventDisposers.length) {
       this.bindEvents();
     }
-    if (this.disconnected) {
+    if (this.disconnected || !this.accessible) {
       await this.reconnect();
     }
     await this.refresh();
