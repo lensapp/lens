23f38984f72a053393ca8752aaa84c22e4fb6bcf
Trevor Nichols <trevor@nichols.id.au>
2020-09-03 08:39:56 +1000

    Register alternate API base URL if preferred version changes.
    Signed-off-by: Trevor Nichols <trevor@nichols.id.au>

--- src/renderer/api/kube-api-versioned.ts
+++ src/renderer/api/kube-api-versioned.ts
@@ -1,16 +1,35 @@
 import { stringify } from "querystring";
 import { KubeObject } from "./kube-object";
 import { createKubeApiURL } from "./kube-api-parse";
-import { KubeApi, IKubeApiQueryParams } from "./kube-api";
+import { KubeApi, IKubeApiQueryParams, IKubeApiOptions } from "./kube-api";
+import { apiManager } from "./api-manager";
 
 export class VersionedKubeApi<T extends KubeObject = any> extends KubeApi<T> {
   private preferredVersion?: string;
 
+  constructor(opts: IKubeApiOptions<T>) {
+    super(opts);
+
+    this.getPreferredVersion().then(() => {
+      if (this.apiBase != opts.apiBase)
+        apiManager.registerApi(this.apiBase, this);
+    });
+  }
+
+  // override this property to make read-write
+  apiBase: string
+
   async getPreferredVersion() {
     if (this.preferredVersion) return;
 
-    const apiGroupVersion = await this.request.get<{ preferredVersion: { version: string; }; }>(`${this.apiPrefix}/${this.apiGroup}`);
+    const apiGroupVersion = await this.request.get<{ preferredVersion?: { version: string; }; }>(`${this.apiPrefix}/${this.apiGroup}`);
+    
+    if (!apiGroupVersion?.preferredVersion) return;
+
     this.preferredVersion = apiGroupVersion.preferredVersion.version;
+
+    // update apiBase
+    this.apiBase = this.getUrl();
   }
 
   async list({ namespace = "" } = {}, query?: IKubeApiQueryParams): Promise<T[]> {
