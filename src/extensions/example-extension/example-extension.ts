11dce16b6c438123762a4cf13544633e397e96c3
Roman <ixrock@gmail.com>
2020-09-04 15:58:49 +0300

    extensions-api -- added todos for example-extension
    Signed-off-by: Roman <ixrock@gmail.com>

--- src/extensions/example-extension/example-extension.ts
+++ src/extensions/example-extension/example-extension.ts
@@ -1,5 +1,8 @@
 import { LensExtension } from "@lens/extensions"; // fixme: map to generated types from "extension-api.d.ts"
 
+// todo: register custom icon in cluster-menu
+// todo: register custom view by clicking the item
+
 export default class ExampleExtension extends LensExtension {
   async activate(): Promise<any> {
     await super.activate();
