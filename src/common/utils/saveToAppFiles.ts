a4c5ed83c81f6bd0e55d92af6caa00db8267c63a
Jim Ehrismann <jehrismann@mirantis.com>
2020-09-04 10:58:28 -0400

    Restrict file permissions to only the user for pasted kubeconfigs that are kept in the app dir
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- src/common/utils/saveToAppFiles.ts
+++ src/common/utils/saveToAppFiles.ts
@@ -1,11 +1,11 @@
 // Save file to electron app directory (e.g. "/Users/$USER/Library/Application Support/Lens" for MacOS)
 import path from "path";
 import { app, remote } from "electron";
-import { ensureDirSync, writeFileSync } from "fs-extra";
+import { ensureDirSync, writeFileSync, WriteFileOptions } from "fs-extra";
 
-export function saveToAppFiles(filePath: string, contents: any): string {
+export function saveToAppFiles(filePath: string, contents: any, options?: WriteFileOptions): string {
   const absPath = path.resolve((app || remote.app).getPath("userData"), filePath);
   ensureDirSync(path.dirname(absPath));
-  writeFileSync(absPath, contents);
+  writeFileSync(absPath, contents, options);
   return absPath;
 }
