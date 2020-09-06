01d5989894a5a5da199c098d72d0b01e3a258521
Roman <ixrock@gmail.com>
2020-09-06 16:29:06 +0300

    extensions-api -- hide warning "dependency: the request of a dependency is an expression"
    Signed-off-by: Roman <ixrock@gmail.com>

--- webpack.renderer.ts
+++ webpack.renderer.ts
@@ -44,6 +44,11 @@ export function webpackLensRenderer({ showVars = true } = {}): webpack.Configura
       filename: '[name].js',
       chunkFilename: 'chunks/[name].js',
     },
+    stats: {
+      warningsFilter: [
+        /Critical dependency: the request of a dependency is an expression/
+      ]
+    },
     resolve: {
       extensions: [
         '.js', '.jsx', '.json',
