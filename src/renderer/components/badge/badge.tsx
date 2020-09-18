9a9f50ab346da4ca923c663b17b52eaa22f27a9a
Sebastian Malton <sebastian@malton.name>
2020-09-18 02:33:43 -0400

    add a zero width space to Badges to fix double click highlighting issue (#914)
    * add a zero width space to Badges to fix double click highlighting issue
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/badge/badge.tsx
+++ src/renderer/components/badge/badge.tsx
@@ -13,11 +13,18 @@ interface Props extends React.HTMLAttributes<any>, TooltipDecoratorProps {
 export class Badge extends React.Component<Props> {
   render() {
     const { className, label, small, children, ...elemProps } = this.props;
-    return (
+    return <>
       <span className={cssNames("Badge", { small }, className)} {...elemProps}>
         {label}
         {children}
       </span>
-    );
+      { /**
+          * This is a zero-width-space. It makes there be a word seperation
+          * between adjacent Badge's because <span>'s are ignored for browers'
+          * word detection algorithmns use for determining the extent of the
+          * text to highlight on multi-click sequences.
+          */}
+      &#8203;
+    </>
   }
 }
