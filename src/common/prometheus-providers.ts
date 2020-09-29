91803c7dbc11a698b666c87a5aeb203bccf683b2
Lauri Nevala <lauri.nevala@gmail.com>
2020-09-29 23:33:53 +0300

    Add support for Docker Enterprise Container Cloud metrics
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/common/prometheus-providers.ts
+++ src/common/prometheus-providers.ts
@@ -1,9 +1,10 @@
 import { PrometheusLens } from "../main/prometheus/lens";
 import { PrometheusHelm } from "../main/prometheus/helm";
 import { PrometheusOperator } from "../main/prometheus/operator";
+import { PrometheusDECC } from "../main/prometheus/decc";
 import { PrometheusProviderRegistry } from "../main/prometheus/provider-registry";
 
-[PrometheusLens, PrometheusHelm, PrometheusOperator].forEach(providerClass => {
+[PrometheusLens, PrometheusHelm, PrometheusOperator, PrometheusDECC].forEach(providerClass => {
   const provider = new providerClass()
   PrometheusProviderRegistry.registerProvider(provider.id, provider)
 });
