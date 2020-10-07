774979c7cd138b7af207aff716afd0d2cc226281
Sebastian Malton <sebastian@malton.name>
2020-10-07 17:41:00 -0400

    Add some basic unit tests for the following files:
    - src/main/cluster.ts
    - src/main/kube-auth-proxy.ts
    - src/main/kubeconfig-manager.ts
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/cluster.ts
+++ src/main/cluster.ts
@@ -93,7 +93,7 @@ export class Cluster implements ClusterModel {
   async init(port: number) {
     try {
       this.contextHandler = new ContextHandler(this);
-      this.kubeconfigManager = new KubeconfigManager(this, this.contextHandler, port);
+      this.kubeconfigManager = await KubeconfigManager.create(this, this.contextHandler, port);
       this.kubeProxyUrl = `http://localhost:${port}${apiKubePrefix}`;
       this.initialized = true;
       logger.info(`[CLUSTER]: "${this.contextName}" init success`, {
