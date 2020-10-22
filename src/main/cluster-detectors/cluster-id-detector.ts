d396f6a44f82d9457468f9b4822c96704881e07c
Lauri Nevala <lauri.nevala@gmail.com>
2020-10-22 21:09:15 +0300

    Read cluster id from default namespace
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/cluster-detectors/cluster-id-detector.ts
+++ src/main/cluster-detectors/cluster-id-detector.ts
@@ -6,7 +6,18 @@ export class ClusterIdDetector extends BaseClusterDetector {
   key = ClusterMetadataKey.CLUSTER_ID
 
   public async detect() {
-    const id = createHash("sha256").update(this.cluster.apiUrl).digest("hex")
-    return { value: id, accuracy: 100 }
+    let id: string
+    try {
+      id = await this.getDefaultNamespaceId()
+    } catch(_) {
+      id = this.cluster.apiUrl
+    }
+    const value = createHash("sha256").update(id).digest("hex")
+    return { value: value, accuracy: 100 }
+  }
+
+  protected async getDefaultNamespaceId() {
+    const response = await this.k8sRequest("/api/v1/namespaces/default")
+    return response.metadata.uid
   }
 }
