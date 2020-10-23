3281803e1eebbdcb9005bdf7212460d1cd968684
Lauri Nevala <lauri.nevala@gmail.com>
2020-10-23 20:34:19 +0300

    Start authentication proxy when opening cluster settings (#1063) (#1120)
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/common/cluster-ipc.ts
+++ src/common/cluster-ipc.ts
@@ -5,11 +5,10 @@ import { tracker } from "./tracker";
 export const clusterIpc = {
   activate: createIpcChannel({
     channel: "cluster:activate",
-    handle: (clusterId: ClusterId, frameId?: number) => {
+    handle: (clusterId: ClusterId, force = false) => {
       const cluster = clusterStore.getById(clusterId);
       if (cluster) {
-        if (frameId) cluster.frameId = frameId; // save cluster's webFrame.routingId to be able to send push-updates
-        return cluster.activate();
+        return cluster.activate(force);
       }
     },
   }),
