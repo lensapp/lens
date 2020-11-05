21db4e39fb8df6f41a5838d8a0d971098ce36110
Sebastian Malton <sebastian@malton.name>
2020-11-05 10:12:35 -0500

    add button to expand and view whole badge contents
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/badge/badge.tsx
+++ src/renderer/components/badge/badge.tsx
@@ -3,6 +3,10 @@ import "./badge.scss"
 import React from "react";
 import { cssNames } from "../../utils/cssNames";
 import { TooltipDecoratorProps, withTooltip } from "../tooltip";
+import { observer } from "mobx-react";
+import { autobind } from "../../utils";
+import { observable } from "mobx";
+import { Icon } from "../icon";
 
 export interface BadgeProps extends React.HTMLAttributes<any>, TooltipDecoratorProps {
   small?: boolean;
@@ -10,14 +14,48 @@ export interface BadgeProps extends React.HTMLAttributes<any>, TooltipDecoratorP
 }
 
 @withTooltip
+@observer
 export class Badge extends React.Component<BadgeProps> {
+  private elem: HTMLElement;
+
+  @observable isExpanded = false
+  @observable canBeExpanded = false
+
+  componentDidMount() {
+    this.canBeExpanded = this.elem.offsetWidth < this.elem.scrollWidth
+  }
+
+  @autobind()
+  onClick() {
+    if (this.canBeExpanded) {
+      this.isExpanded = !this.isExpanded
+    }
+  }
+
+  renderExpansionIcon() {
+    if (!this.canBeExpanded) {
+      return null
+    }
+
+    if (this.isExpanded) {
+      return <Icon className="expansionIcon" size={20} material="close_fullscreen" onClick={this.onClick} />
+    }
+
+    return <Icon className="expansionIcon" size={20} material="open_in_full" onClick={this.onClick} />
+  }
+
   render() {
     const { className, label, small, children, ...elemProps } = this.props;
-    return <>
-      <span className={cssNames("Badge", { small }, className)} {...elemProps}>
-        {label}
-        {children}
-      </span>
-    </>
+    const classNames = cssNames("Badge", className, { small, expanded: this.isExpanded })
+
+    return (
+      <div className={classNames} {...elemProps}>
+        {this.renderExpansionIcon()}
+        <div className="children" ref={ref => (this.elem = ref)}>
+          {label}
+          {children}
+        </div>
+      </div>
+    )
   }
 }
