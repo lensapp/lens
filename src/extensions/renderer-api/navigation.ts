7bcf23b1705888776a44b1c45556fe91ce616ae7
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-11-11 19:54:58 +0200

    fix
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

---
+++ src/extensions/renderer-api/navigation.ts
@@ -0,0 +1,4 @@
+export { navigate } from "../../renderer/navigation";
+export { hideDetails, showDetails, getDetailsUrl } from "../../renderer/navigation"
+export { RouteProps } from "react-router"
+export { IURLParams } from "../../common/utils/buildUrl";
