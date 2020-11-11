74596b086e5410bdfed5988ff34a24534eed6782
Alex Andreev <alex.andreev.email@gmail.com>
2020-11-11 14:39:36 +0300

    Using css-in-js styling in support-page extension
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- extensions/support-page/renderer.tsx
+++ extensions/support-page/renderer.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Component, LensRendererExtension, Navigation } from "@k8slens/extensions";
 import { supportPageRoute, supportPageURL } from "./src/support.route";
 import { Support } from "./src/support";
+import styled from '@emotion/styled';
+
+const Icon = styled.div`
+  color: white;
+  display: flex;
+  align-items: center;
+  padding-right: calc(var(--padding) / 2);
+`
 
 export default class SupportPageRendererExtension extends LensRendererExtension {
   globalPages = [
@@ -18,12 +26,14 @@ export default class SupportPageRendererExtension extends LensRendererExtension 
   statusBarItems = [
     {
       item: (
-        <div
-          className="flex align-center gaps hover-highlight"
-          onClick={() => Navigation.navigate(supportPageURL())}
-        >
-          <Component.Icon material="help" smallest />
-        </div>
+        <Icon>
+          <Component.Icon
+            className="SupportIcon"
+            material="help"
+            smallest
+            onClick={() => Navigation.navigate(supportPageURL())}
+          />
+        </Icon>
       )
     }
   ]
--- extensions/support-page/src/support.tsx
+++ extensions/support-page/src/support.tsx
@@ -1,9 +1,22 @@
 // TODO: support localization / figure out how to extract / consume i18n strings
 
-import "./support.scss"
 import React from "react"
 import { observer } from "mobx-react"
 import { App, Component } from "@k8slens/extensions";
+import styled from '@emotion/styled';
+
+const SupportPage = styled.div`
+  a[target=_blank] {
+    text-decoration: none;
+
+    &:after {
+      content: "launch";
+      font: small "Material Icons";
+      vertical-align: middle;
+      margin-left: 2px;
+    }
+  }
+`
 
 @observer
 export class Support extends React.Component {
@@ -11,19 +24,21 @@ export class Support extends React.Component {
     const { PageLayout } = Component;
     const { slackUrl, issuesTrackerUrl } = App;
     return (
-      <PageLayout showOnTop className="Support" header={<h2>Support</h2>}>
-        <h2>Community Slack Channel</h2>
-        <p>
-          Ask a question, see what's being discussed, join the conversation <a href={slackUrl} target="_blank">here</a>
-        </p>
+      <SupportPage>
+        <PageLayout showOnTop header={<h2>Support</h2>}>
+          <h2>Community Slack Channel</h2>
+          <p>
+            Ask a question, see what's being discussed, join the conversation <a href={slackUrl} target="_blank">here</a>
+          </p>
 
-        <h2>Report an Issue</h2>
-        <p>
-          Review existing issues or open a new one <a href={issuesTrackerUrl} target="_blank">here</a>
-        </p>
+          <h2>Report an Issue</h2>
+          <p>
+            Review existing issues or open a new one <a href={issuesTrackerUrl} target="_blank">here</a>
+          </p>
 
-        {/*<h2><Trans>Commercial Support</Trans></h2>*/}
-      </PageLayout>
+          {/*<h2><Trans>Commercial Support</Trans></h2>*/}
+        </PageLayout>
+      </SupportPage>
     );
   }
 }
