d3979c28835fa0510f9209dfd9344d813786bc29
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-11-13 09:46:43 +0200

    add tests
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

---
+++ src/extensions/__tests__/lens-extension.test.ts
@@ -0,0 +1,23 @@
+import { LensExtension } from "../lens-extension"
+
+let ext: LensExtension = null
+
+describe("lens extension", () => {
+  beforeEach(async () => {
+    ext = new LensExtension({
+      manifest: {
+        name: "foo-bar",
+        version: "0.1.1"
+      },
+      manifestPath: "/this/is/fake/package.json",
+      isBundled: false,
+      isEnabled: true
+    })
+  })
+
+  describe("name", () => {
+    it("returns name", () => {
+      expect(ext.name).toBe("foo-bar")
+    })
+  })
+})
---
+++ src/extensions/registries/__tests__/page-registry.test.ts
@@ -0,0 +1,31 @@
+import { getPageUrl } from "../page-registry"
+import { LensExtension } from "../../lens-extension"
+
+let ext: LensExtension = null
+
+describe("getPageUrl", () => {
+  beforeEach(async () => {
+    ext = new LensExtension({
+      manifest: {
+        name: "foo-bar",
+        version: "0.1.1"
+      },
+      manifestPath: "/this/is/fake/package.json",
+      isBundled: false,
+      isEnabled: true
+    })
+  })
+
+  it("returns a page url for extension", () => {
+    expect(getPageUrl(ext)).toBe("/extension/foo-bar")
+  })
+
+  it("allows to pass base url as parameter", () => {
+    expect(getPageUrl(ext, "/test")).toBe("/extension/foo-bar/test")
+  })
+
+  it("removes @", () => {
+    ext.manifest.name = "@foo/bar"
+    expect(getPageUrl(ext)).toBe("/extension/foo-bar")
+  })
+})
