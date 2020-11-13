305843b084ac5f8377e3a4a014b04de6ef753764
Lauri Nevala <lauri.nevala@gmail.com>
2020-11-13 14:14:38 +0200

    Remove getMeta() from main class in example extension
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- extensions/example-extension/main.ts
+++ extensions/example-extension/main.ts
@@ -2,10 +2,10 @@ import { LensMainExtension } from "@k8slens/extensions";
 
 export default class ExampleExtensionMain extends LensMainExtension {
   onActivate() {
-    console.log('EXAMPLE EXTENSION MAIN: ACTIVATED', this.getMeta());
+    console.log('EXAMPLE EXTENSION MAIN: ACTIVATED');
   }
 
   onDeactivate() {
-    console.log('EXAMPLE EXTENSION MAIN: DEACTIVATED', this.getMeta());
+    console.log('EXAMPLE EXTENSION MAIN: DEACTIVATED');
   }
 }
