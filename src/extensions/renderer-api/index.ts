d559d5e62aea3b6b5d14564f37e853299055d5f6
Alex Andreev <alex.andreev.email@gmail.com>
2020-11-16 13:10:35 +0300

    Exporting active theme to extensions (#1371)
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/extensions/renderer-api/index.ts
+++ src/extensions/renderer-api/index.ts
@@ -4,9 +4,11 @@
 import * as Component from "./components"
 import * as K8sApi from "./k8s-api"
 import * as Navigation from "./navigation"
+import * as Theme from "./theming"
 
 export {
   Component,
   K8sApi,
-  Navigation
+  Navigation,
+  Theme,
 }
