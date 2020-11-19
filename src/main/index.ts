8a3613ac6f4aa9b8686a4a08abce6f351ffeed04
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-11-19 16:11:58 +0200

    fix extensionLoader error on dev environments where renderer might start early (#1447)
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/main/index.ts
+++ src/main/index.ts
@@ -78,8 +78,9 @@ app.on("ready", async () => {
     app.exit();
   }
 
+  extensionLoader.init()
   windowManager = WindowManager.getInstance<WindowManager>(proxyPort);
-  extensionLoader.init(await extensionManager.load()); // call after windowManager to see splash earlier
+  extensionLoader.initExtensions(await extensionManager.load()); // call after windowManager to see splash earlier
 
   setTimeout(() => {
     appEventBus.emit({ name: "service", action: "start" })
