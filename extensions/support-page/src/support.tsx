8d2ffda2cf3739049d82cb91a174469f5d4a62b4
leenamba <lee.namba@gmail.com>
2020-11-19 14:25:06 +0100

    Modified support page
    Signed-off-by: leenamba <lee.namba@gmail.com>

--- extensions/support-page/src/support.tsx
+++ extensions/support-page/src/support.tsx
@@ -11,16 +11,25 @@ export class SupportPage extends React.Component {
     const { PageLayout } = Component;
     const { slackUrl, issuesTrackerUrl } = App;
     return (
-      <PageLayout showOnTop className="SupportPage" header={<h2>Support</h2>}>
+      <PageLayout showOnTop className="SupportPage" header={<h2>Welcome to Lens support</h2>}>
+        <p>Here you will find different ways of getting support for Lens.</p>
         <h2>Community Slack Channel</h2>
         <p>
-          Ask a question, see what's being discussed, join the conversation <a href={slackUrl} target="_blank">here</a>
+        We have an active and growing community! Ask a question, see what's being discussed, get insights to up and coming features, help others, join the conversation on our community slack <a href={slackUrl} target="_blank">here</a>
         </p>
 
-        <h2>Report an Issue</h2>
+        <h2>Open Source Github Repository</h2>
         <p>
-          Review existing issues or open a new one <a href={issuesTrackerUrl} target="_blank">here</a>
+        Search feature requests, submit an idea, review existing issues, or open a new one at our Github repository <a href={issuesTrackerUrl} target="_blank">here</a>
         </p>
+        
+        <h2>Enterprise Support</h2>
+        <p>
+        If you are interested in paid support options designed for enterprises to cover Lens usage at scale please see the following links:
+        </p>
+        <ul>
+          <li><a href="https://www.mirantis.com/support/enterprise-support-services" target="_blank">Mirantis</a></li>
+        </ul>
 
         {/*<h2><Trans>Commercial Support</Trans></h2>*/}
       </PageLayout>
