80176a70453b810b723158a27bee47b68ecca9a6
Alex Andreev <alex.andreev.email@gmail.com>
2020-11-20 07:51:42 +0300

    Revert "Allow extension cluster menus to have a parent (#1452)"
    This reverts commit 622c45cd6d532a3e3ff3c49c7724bc0573ddb264.

--- src/extensions/registries/page-menu-registry.ts
+++ src/extensions/registries/page-menu-registry.ts
@@ -1,10 +1,9 @@
 // Extensions-api -> Register page menu items
 import type { IconProps } from "../../renderer/components/icon";
 import type React from "react";
-import { action, computed } from "mobx";
+import { action } from "mobx";
 import { BaseRegistry } from "./base-registry";
 import { LensExtension } from "../lens-extension";
-import { RegisteredPage } from "./page-registry";
 
 export interface PageMenuTarget<P extends object = any> {
   extensionId?: string;
@@ -18,16 +17,11 @@ export interface PageMenuRegistration {
   components: PageMenuComponents;
 }
 
-export interface ClusterPageMenuRegistration extends PageMenuRegistration {
-  id?: string;
-  parentId?: string;
-}
-
 export interface PageMenuComponents {
   Icon: React.ComponentType<IconProps>;
 }
 
-export class GlobalPageMenuRegistry extends BaseRegistry<PageMenuRegistration> {
+export class PageMenuRegistry extends BaseRegistry<PageMenuRegistration, Required<PageMenuRegistration>> {
   @action
   add(items: PageMenuRegistration[], ext: LensExtension) {
     const normalizedItems = items.map(menuItem => {
@@ -41,31 +35,5 @@ export class GlobalPageMenuRegistry extends BaseRegistry<PageMenuRegistration> {
   }
 }
 
-export class ClusterPageMenuRegistry extends BaseRegistry<ClusterPageMenuRegistration> {
-  @action
-  add(items: PageMenuRegistration[], ext: LensExtension) {
-    const normalizedItems = items.map(menuItem => {
-      menuItem.target = {
-        extensionId: ext.name,
-        ...(menuItem.target || {}),
-      };
-      return menuItem;
-    });
-    return super.add(normalizedItems);
-  }
-
-  getRootItems() {
-    return this.getItems().filter((item) => !item.parentId);
-  }
-
-  getSubItems(parent: ClusterPageMenuRegistration) {
-    return this.getItems().filter((item) => item.parentId === parent.id && item.target.extensionId === parent.target.extensionId);
-  }
-
-  getByPage(page: RegisteredPage) {
-    return this.getItems().find((item) => item.target?.pageId == page.id && item.target?.extensionId === page.extensionId);
-  }
-}
-
-export const globalPageMenuRegistry = new GlobalPageMenuRegistry();
-export const clusterPageMenuRegistry = new ClusterPageMenuRegistry();
+export const globalPageMenuRegistry = new PageMenuRegistry();
+export const clusterPageMenuRegistry = new PageMenuRegistry();
