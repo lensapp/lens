971d320532931abe36664db562fdc905a7691c75
Sebastian Malton <sebastian@malton.name>
2020-11-23 09:11:13 -0500

    fix typing
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ src/common/utils/rectify-array.ts
@@ -0,0 +1,8 @@
+/**
+ * rectify condences the single item or array of T type, to an array.
+ * @param items either one item or an array of items
+ * @returns a list of items
+ */
+export function recitfy<T>(items: T | T[]): T[] {
+  return Array.isArray(items) ? items : [items];
+}
