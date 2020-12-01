deb9078ff643b736030fc3e389efbd4eac5001e3
Sebastian Malton <sebastian@malton.name>
2020-12-01 15:11:35 -0500

    persist extension installation state for the duration of the window's life
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ src/renderer/components/+extensions/extension-install.store.ts
@@ -0,0 +1,13 @@
+import { observable } from "mobx";
+import { autobind, Singleton } from "../../utils";
+
+interface ExtensionState {
+  displayName: string;
+  // Possible states the extension can be
+  state: "installing" | "uninstalling";
+}
+
+@autobind()
+export class ExtensionStateStore extends Singleton {
+  extensionState = observable.map<string, ExtensionState>();
+}
