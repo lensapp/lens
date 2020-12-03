432b00b8cce7797318406e858d9041f387aa9503
Panu Horsmalahti <phorsmalahti@mirantis.com>
2020-12-03 11:43:33 +0200

    Refactor ExtensionsStore (#1620)
    Signed-off-by: Panu Horsmalahti <phorsmalahti@mirantis.com>

--- src/extensions/extensions-store.ts
+++ src/extensions/extensions-store.ts
@@ -1,7 +1,6 @@
 import type { LensExtensionId } from "./lens-extension";
-import type { ExtensionLoader } from "./extension-loader";
 import { BaseStore } from "../common/base-store";
-import { action, computed, observable, reaction, toJS } from "mobx";
+import { action, computed, observable, toJS } from "mobx";
 
 export interface LensExtensionsStoreModel {
   extensions: Record<LensExtensionId, LensExtensionState>;
@@ -28,35 +27,17 @@ export class ExtensionsStore extends BaseStore<LensExtensionsStoreModel> {
 
   protected state = observable.map<LensExtensionId, LensExtensionState>();
 
-  protected getState(extensionLoader: ExtensionLoader) {
-    const state: Record<LensExtensionId, LensExtensionState> = {};
-
-    return Array.from(extensionLoader.userExtensions).reduce((state, [extId, ext]) => {
-      state[extId] = {
-        enabled: ext.isEnabled,
-        name: ext.manifest.name,
-      };
-
-      return state;
-    }, state);
-  }
-
-  async manageState(extensionLoader: ExtensionLoader) {
-    await extensionLoader.whenLoaded;
-    await this.whenLoaded;
-
-    // save state on change `extension.isEnabled`
-    reaction(() => this.getState(extensionLoader), extensionsState => {
-      this.state.merge(extensionsState);
-    });
-  }
-
   isEnabled(extId: LensExtensionId) {
     const state = this.state.get(extId);
 
     // By default false, so that copied extensions are disabled by default.
     // If user installs the extension from the UI, the Extensions component will specifically enable it.
     return Boolean(state?.enabled);
+  }
+
+  @action
+  mergeState(extensionsState: Record<LensExtensionId, LensExtensionState>) {
+    this.state.merge(extensionsState);
   }
 
   @action
