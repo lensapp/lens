cb954e6575bec205d622e952223b54e47c4845ba
Sebastian Malton <sebastian@malton.name>
2020-12-04 09:25:37 -0500

    persist extension installation state for the duration of the window's life (#1602)
    * persist extension installation state for the duration of the window's life
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+extensions/__tests__/extensions.test.tsx
+++ src/renderer/components/+extensions/__tests__/extensions.test.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { extensionDiscovery } from "../../../../extensions/extension-discovery";
 import { ConfirmDialog } from "../../confirm-dialog";
 import { Notifications } from "../../notifications";
+import { ExtensionStateStore } from "../extension-install.store";
 import { Extensions } from "../extensions";
 
 jest.mock("fs-extra");
@@ -51,6 +52,10 @@ jest.mock("../../notifications", () => ({
 }));
 
 describe("Extensions", () => {
+  beforeEach(() => {
+    ExtensionStateStore.resetInstance();
+  });
+
   it("disables uninstall and disable buttons while uninstalling", async () => {
     render(<><Extensions /><ConfirmDialog/></>);
 
@@ -61,14 +66,14 @@ describe("Extensions", () => {
 
     // Approve confirm dialog
     fireEvent.click(screen.getByText("Yes"));
-    
+
     expect(extensionDiscovery.uninstallExtension).toHaveBeenCalledWith("/absolute/path");
     expect(screen.getByText("Disable").closest("button")).toBeDisabled();
     expect(screen.getByText("Uninstall").closest("button")).toBeDisabled();
   });
 
   it("displays error notification on uninstall error", () => {
-    (extensionDiscovery.uninstallExtension as any).mockImplementationOnce(() => 
+    (extensionDiscovery.uninstallExtension as any).mockImplementationOnce(() =>
       Promise.reject()
     );
     render(<><Extensions /><ConfirmDialog/></>);
