8f275590536e974c80c4bb6b3c1151d313685a82
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-12-07 13:38:40 +0200

    Tag cluster & workspace as beta features in extension api (#1589)
    * mark cluster & workspace as beta
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    * back to public
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/extensions/core-api/stores.ts
+++ src/extensions/core-api/stores.ts
@@ -1,8 +1,8 @@
 export { ExtensionStore } from "../extension-store";
 
-export { clusterStore, Cluster } from "../stores/cluster-store";
+export { clusterStore, Cluster, ClusterStore } from "../stores/cluster-store";
 export type { ClusterModel, ClusterId } from "../stores/cluster-store";
 
-export { workspaceStore, Workspace } from "../stores/workspace-store";
+export { workspaceStore, Workspace, WorkspaceStore } from "../stores/workspace-store";
 export type { WorkspaceId, WorkspaceModel } from "../stores/workspace-store";
 
