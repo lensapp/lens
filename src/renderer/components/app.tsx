dc9f5fe64c57b06d56814ec9826174a41790807c
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2020-12-08 08:51:16 +0200

    remove observer from app class
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/renderer/components/app.tsx
+++ src/renderer/components/app.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { observer } from "mobx-react";
 import { Redirect, Route, Router, Switch } from "react-router";
 import { I18nProvider } from "@lingui/react";
 import { _i18n } from "../i18n";
@@ -49,7 +48,6 @@ import { nodesStore } from "./+nodes/nodes.store";
 import { podsStore } from "./+workloads-pods/pods.store";
 import { sum } from "lodash";
 
-@observer
 export class App extends React.Component {
   static async init() {
     const frameId = webFrame.routingId;
