af3c43203b353612123e65adeab732f251f4c160
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-12-15 12:07:33 +0800

    Revert back to redirecting /sockjs/ to webpack-dev-server
    Signed-off-by: Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>

--- src/main/router.ts
+++ src/main/router.ts
@@ -106,8 +106,8 @@ export class Router {
 
     try {
       const filename = path.basename(req.url);
-      // redirect requests to [appName].js, [appName].html to webpack-dev-server (for hot-reload support)
-      const toWebpackDevServer = filename.includes(appName) || filename.includes("hot-update");
+      // redirect requests to [appName].js, [appName].html /sockjs-node/ to webpack-dev-server (for hot-reload support)
+      const toWebpackDevServer = filename.includes(appName) || filename.includes("hot-update") || req.url.includes("sockjs-node");
 
       if (isDevelopment && toWebpackDevServer) {
         const redirectLocation = `http://localhost:${webpackDevServerPort}${req.url}`;
