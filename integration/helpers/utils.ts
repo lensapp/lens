aaff71e0b03f34a4b00d366fd2a3d79cd39bdd5a
Lauri Nevala <lauri.nevala@gmail.com>
2021-01-25 15:10:21 +0200

    Fix Helm repository and pod logs integration tests
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- integration/helpers/utils.ts
+++ integration/helpers/utils.ts
@@ -1,4 +1,6 @@
 import { Application } from "spectron";
+import * as util from "util";
+import { exec } from "child_process";
 
 const AppPaths: Partial<Record<NodeJS.Platform, string>> = {
   "win32": "./dist/win-unpacked/Lens.exe",
@@ -27,6 +29,7 @@ export function setup(): Application {
 }
 
 type AsyncPidGetter = () => Promise<number>;
+export const promiseExec = util.promisify(exec);
 
 export async function tearDown(app: Application) {
   const pid = await (app.mainProcess.pid as any as AsyncPidGetter)();
@@ -38,4 +41,20 @@ export async function tearDown(app: Application) {
   } catch (e) {
     console.error(e);
   }
+}
+
+export async function listHelmRepositories(retries = 0): Promise<string> {
+  if (retries < 5) {
+    try {
+      const { stdout: reposJson } = await promiseExec("helm repo list -o json");
+
+      return reposJson;
+    } catch {
+      await new Promise(r => setTimeout(r, 2000)); // if no repositories, wait for Lens adding bitnami repository
+
+      return await listHelmRepositories((retries + 1));
+    }
+  }
+
+  return "[]";
 }
