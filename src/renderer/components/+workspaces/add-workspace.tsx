8a34184c46c9256ca4bfea6e586258f0d0dad4d5
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-02-02 10:45:39 +0200

    cleanup
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/renderer/components/+workspaces/add-workspace.tsx
+++ src/renderer/components/+workspaces/add-workspace.tsx
@@ -10,7 +10,7 @@ import { CommandOverlay } from "../command-palette/command-container";
 const uniqueWorkspaceName: InputValidator = {
   condition: ({ required }) => required,
   message: () => `Workspace with this name already exists`,
-  validate: value => !workspaceStore.workspacesList.find((workspace) => workspace.name === value),
+  validate: value => !workspaceStore.getByName(value),
 };
 
 @observer
