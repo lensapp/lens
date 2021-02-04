ad6de71826322498a2c13627a758f2c97652c2aa
Lauri Nevala <lauri.nevala@gmail.com>
2021-02-04 19:19:11 +0200

    Add more log entries on start up (#1955)
    * Add more log entries on start up
    
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    
    * And log entry about shell sync
    
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    
    * Refactoring
    
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    
    * Add new line
    
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastian Malton <sebastian@malton.name>
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>
    
    Co-authored-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    Co-authored-by: Sebastian Malton <sebastian@malton.name>

--- src/main/developer-tools.ts
+++ src/main/developer-tools.ts
@@ -1,9 +1,12 @@
+import logger from "./logger";
+
 /**
  * Installs Electron developer tools in the development build.
  * The dependency is not bundled to the production build.
  */
 export const installDeveloperTools = async () => {
   if (process.env.NODE_ENV === "development") {
+    logger.info("🤓 Installing developer tools");
     const { default: devToolsInstaller, REACT_DEVELOPER_TOOLS } = await import("electron-devtools-installer");
 
     return devToolsInstaller([REACT_DEVELOPER_TOOLS]);
--- src/main/router.ts
+++ src/main/router.ts
@@ -5,7 +5,7 @@ import path from "path";
 import { readFile } from "fs-extra";
 import { Cluster } from "./cluster";
 import { apiPrefix, appName, publicPath, isDevelopment, webpackDevServerPort } from "../common/vars";
-import { helmRoute, kubeconfigRoute, metricsRoute, portForwardRoute, resourceApplierRoute, watchRoute } from "./routes";
+import { helmRoute, kubeconfigRoute, metricsRoute, portForwardRoute, resourceApplierRoute, watchRoute, versionRoute } from "./routes";
 import logger from "./logger";
 
 export interface RouterRequestOpts {
@@ -143,6 +143,7 @@ export class Router {
         this.handleStaticFile(params.path, response, req);
       });
 
+    this.router.add({ method: "get", path: "/version"}, versionRoute.getVersion.bind(versionRoute));
     this.router.add({ method: "get", path: `${apiPrefix}/kubeconfig/service-account/{namespace}/{account}` }, kubeconfigRoute.routeServiceAccountRoute.bind(kubeconfigRoute));
 
     // Watch API
