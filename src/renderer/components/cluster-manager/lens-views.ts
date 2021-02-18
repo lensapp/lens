9b82e35a3ea540da050894996d8a06146b319906
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-02-18 17:43:56 +0200

    fix duplicate cluster iframes
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/renderer/components/cluster-manager/lens-views.ts
+++ src/renderer/components/cluster-manager/lens-views.ts
@@ -25,8 +25,6 @@ export async function initView(clusterId: ClusterId) {
     return;
   }
 
-  logger.info(`[LENS-VIEW]: waiting cluster to be ready, clusterId=${clusterId}`);
-  await cluster.whenReady;
   logger.info(`[LENS-VIEW]: init dashboard, clusterId=${clusterId}`);
   const parentElem = document.getElementById("lens-views");
   const iframe = document.createElement("iframe");
@@ -39,6 +37,8 @@ export async function initView(clusterId: ClusterId) {
   }, { once: true });
   lensViews.set(clusterId, { clusterId, view: iframe });
   parentElem.appendChild(iframe);
+  logger.info(`[LENS-VIEW]: waiting cluster to be ready, clusterId=${clusterId}`);
+  await cluster.whenReady;
   await autoCleanOnRemove(clusterId, iframe);
 }
 
