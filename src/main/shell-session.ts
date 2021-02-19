6dcf3ec6688d6792eea1a86fede01605ae3bf3d4
Lauri Nevala <lauri.nevala@gmail.com>
2021-02-19 18:46:27 +0200

    Update src/main/shell-session.ts
    Co-authored-by: Sebastian Malton <sebastian@malton.name>
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/shell-session.ts
+++ src/main/shell-session.ts
@@ -105,7 +105,7 @@ export class ShellSession extends EventEmitter {
   protected async getShellEnv() {
     const env = JSON.parse(JSON.stringify(await shellEnv()));
     const pathStr = [this.kubectlBinDir, this.helmBinDir, process.env.PATH].join(path.delimiter);
-    const shell = userStore.preferences.shell && userStore.preferences.shell !== "" ? userStore.preferences.shell : process.env.SHELL;
+    const shell = userStore.preferences.shell || process.env.SHELL;
 
     if(isWindows) {
       env["SystemRoot"] = process.env.SystemRoot;
