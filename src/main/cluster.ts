ed6e00798dae83e9902ddae30a14ace5a6849b42
Lauri Nevala <lauri.nevala@gmail.com>
2021-03-01 11:56:30 +0200

    Remove isDead property
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/main/cluster.ts
+++ src/main/cluster.ts
@@ -77,7 +77,6 @@ export class Cluster implements ClusterModel, ClusterState {
    * If extension sets this it needs to also mark cluster as enabled on activate (or when added to a store)
    */
   public ownerRef: string;
-  public isDead = false;
   protected kubeconfigManager: KubeconfigManager;
   protected eventDisposers: Function[] = [];
   protected activated = false;
@@ -267,7 +266,6 @@ export class Cluster implements ClusterModel, ClusterState {
     } catch(err) {
       logger.error(err);
       logger.error(`[CLUSTER] Failed to load kubeconfig for the cluster '${this.name || this.contextName}' (context: ${this.contextName}, kubeconfig: ${this.kubeConfigPath}).`);
-      this.isDead = true;
     }
   }
 
