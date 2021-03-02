576d9c7b682e98c2a405a2e3efd980a613354c4f
Lauri Nevala <lauri.nevala@gmail.com>
2021-03-02 09:12:52 +0200

    Refactor secrets to observable map
    Signed-off-by: Lauri Nevala <lauri.nevala@gmail.com>

--- src/renderer/components/+workloads-pods/pod-details-secrets.tsx
+++ src/renderer/components/+workloads-pods/pod-details-secrets.tsx
@@ -13,18 +13,20 @@ interface Props {
 
 @observer
 export class PodDetailsSecrets extends Component<Props> {
-  @observable secrets: Secret[] = [];
+  @observable secrets: Map<string, Secret> = observable.map<string, Secret>();
 
   @disposeOnUnmount
   secretsLoader = autorun(async () => {
     const { pod } = this.props;
 
-    this.secrets = (await Promise.all(
+    const secrets = await Promise.all(
       pod.getSecrets().map(secretName => secretsApi.get({
         name: secretName,
         namespace: pod.getNs(),
       }))
-    )).filter(Boolean);
+    );
+
+    secrets.forEach(secret => secret && this.secrets.set(secret.getName(), secret));
   });
 
   render() {
@@ -34,13 +36,13 @@ export class PodDetailsSecrets extends Component<Props> {
       <div className="PodDetailsSecrets">
         {
           pod.getSecrets().map(secretName => {
-            const secret = this.secrets.find(secret => secret.getName() === secretName);
+            const secret = this.secrets.get(secretName);
 
             if (secret) {
               return this.renderSecretLink(secret);
             } else {
               return (
-                <span>{secretName}</span>
+                <span key={secretName}>{secretName}</span>
               );
             }
           })
