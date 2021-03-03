3870f07eca9cc74ba3a8046e34eaa5a0db4b811b
Alex Andreev <alex.andreev.email@gmail.com>
2021-03-03 14:50:18 +0300

    A bit of TreeView styling
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/tree-view/tree-view.tsx
+++ src/renderer/components/tree-view/tree-view.tsx
@@ -1,6 +1,7 @@
+import "./tree-view.scss";
+
 import React from "react";
 import { Icon } from "../icon";
-import { makeStyles } from "@material-ui/core/styles";
 import TreeView from "@material-ui/lab/TreeView";
 import TreeItem from "@material-ui/lab/TreeItem";
 
@@ -14,17 +15,7 @@ interface Props {
   data: NavigationTree[]
 }
 
-const useStyles = makeStyles({
-  root: {
-    height: 110,
-    flexGrow: 1,
-    maxWidth: 400,
-  },
-});
-
 export function RecursiveTreeView({ data }: Props) {
-  const classes = useStyles();
-
   const renderTree = (nodes: NavigationTree[]) => {
     return nodes.map(node => (
       <TreeItem key={node.id} nodeId={node.id} label={node.name}>
@@ -35,7 +26,7 @@ export function RecursiveTreeView({ data }: Props) {
 
   return (
     <TreeView
-      className={classes.root}
+      className="TreeView"
       defaultCollapseIcon={<Icon material="expand_more"/>}
       defaultExpandIcon={<Icon material="chevron_right" />}
     >
