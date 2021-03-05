662cb2578747c48bdbef899919822b0233ba5c50
Alex Andreev <alex.andreev.email@gmail.com>
2021-03-05 17:30:34 +0300

    Expand all links by default
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/tree-view/tree-view.tsx
+++ src/renderer/components/tree-view/tree-view.tsx
@@ -5,6 +5,7 @@ import { Icon } from "../icon";
 import TreeView from "@material-ui/lab/TreeView";
 import TreeItem from "@material-ui/lab/TreeItem";
 import { cssNames } from "../../utils";
+import flattenDeep from "lodash/flattenDeep";
 
 export interface NavigationTree {
   id: string;
@@ -31,9 +32,20 @@ export function RecursiveTreeView({ data }: Props) {
     ));
   };
 
+  const getAllNodeIds = (nodes: NavigationTree[]): string[] => {
+    return flattenDeep(nodes.map(node => {
+      return [node.id, ...node.children.map(item => getAllNodeIds([item]))];
+    }));
+  };
+
+  if (!data.length) {
+    return null;
+  }
+
   return (
     <TreeView
       className="TreeView"
+      defaultExpanded={getAllNodeIds(data)}
       defaultCollapseIcon={<Icon material="expand_more"/>}
       defaultExpandIcon={<Icon material="chevron_right" />}
     >
