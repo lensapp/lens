74d8ff212ff65fc48c136181fce675047bb9028b
Jim Ehrismann <jehrismann@mirantis.com>
2021-03-05 19:30:41 -0500

    added original landing page back only for default workspace with no clusters
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- src/renderer/components/+landing-page/landing-page.tsx
+++ src/renderer/components/+landing-page/landing-page.tsx
@@ -14,7 +14,18 @@ export class LandingPage extends React.Component {
   get workspace(): Workspace {
     return workspaceStore.currentWorkspace;
   }
-  
+
+  renderStartupHint() {
+    return (
+      <div className="startup-hint flex column gaps" onMouseEnter={() => this.showHint = false}>
+        <p>This is the quick launch menu.</p>
+        <p>
+          Associate clusters and choose the ones you want to access via quick launch menu by clicking the + button.
+        </p>
+      </div>
+    )
+  }
+
   render() {
     const clusters = clusterStore.getByWorkspaceId(this.workspace.id);
     const noClustersInScope = !clusters.length;
@@ -22,16 +33,24 @@ export class LandingPage extends React.Component {
 
     const header = <h2>Workspace: {this.workspace.name}</h2>;
 
-    return (
-      <PageLayout className="LandingPage flex column gaps" header={header} provideBackButtonNavigation={false} contentGaps={false}>
-        {showStartupHint && (
-          <div className="startup-hint flex column gaps" onMouseEnter={() => this.showHint = false}>
-            <p>This is the quick launch menu.</p>
+    if (this.workspace.name === "default" && noClustersInScope) {
+      return (
+        <div className="LandingPage flex">
+          {showStartupHint && this.renderStartupHint()}
+          <div className="no-clusters flex column gaps box center">
+            <h1>
+              Welcome!
+            </h1>
             <p>
-              Associate clusters and choose the ones you want to access via quick launch menu by clicking the + button.
+              Get started by associating one or more clusters to Lens.
             </p>
           </div>
-        )}
+        </div>
+      )
+    }
+
+    return (
+      <PageLayout className="LandingOverview flex column gaps" header={header} provideBackButtonNavigation={false} contentGaps={false}>
         <WorkspaceOverview workspace={this.workspace}/>
       </PageLayout>
     );
