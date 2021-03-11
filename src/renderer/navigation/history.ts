f099489244bad172b76ecf5969130e67132f0cb9
Sebastian Malton <sebastian@malton.name>
2021-03-11 13:38:20 -0500

    add debug prints for actions against the navigation object
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/navigation/history.ts
+++ src/renderer/navigation/history.ts
@@ -1,6 +1,15 @@
 import { ipcRenderer } from "electron";
 import { createBrowserHistory, createMemoryHistory } from "history";
 import { createObservableHistory } from "mobx-observable-history";
+import { getHostedClusterId } from "../../common/cluster-store";
+import logger from "../../main/logger";
 
 export const history = ipcRenderer ? createBrowserHistory() : createMemoryHistory();
 export const navigation = createObservableHistory(history);
+
+navigation.listen((location, action) => {
+  const clusterId = getHostedClusterId();
+  const frame = clusterId ?? "root";
+
+  logger.debug(`[NAVIGATION]: ${action}`, { location, frame });
+});
