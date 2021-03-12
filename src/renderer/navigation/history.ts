373d2d2a47ebf54aa3abb91bbb9be7e35cfc1db6
Sebastian Malton <sebastian@malton.name>
2021-03-12 16:03:34 -0500

    add debug prints for actions against the navigation object (#2321)

--- src/renderer/navigation/history.ts
+++ src/renderer/navigation/history.ts
@@ -1,6 +1,14 @@
 import { ipcRenderer } from "electron";
 import { createBrowserHistory, createMemoryHistory } from "history";
 import { createObservableHistory } from "mobx-observable-history";
+import logger from "../../main/logger";
 
 export const history = ipcRenderer ? createBrowserHistory() : createMemoryHistory();
 export const navigation = createObservableHistory(history);
+
+navigation.listen((location, action) => {
+  const isClusterView = !process.isMainFrame;
+  const domain = global.location.href;
+
+  logger.debug(`[NAVIGATION]: ${action}`, { isClusterView, domain, location });
+});
