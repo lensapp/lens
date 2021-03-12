ed2e9736353996eddd22161ffbaf333e815451d4
Sebastian Malton <sebastian@malton.name>
2021-03-12 12:49:41 -0500

    change the information
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/navigation/history.ts
+++ src/renderer/navigation/history.ts
@@ -1,15 +1,14 @@
 import { ipcRenderer } from "electron";
 import { createBrowserHistory, createMemoryHistory } from "history";
 import { createObservableHistory } from "mobx-observable-history";
-import { getHostedClusterId } from "../../common/cluster-store";
 import logger from "../../main/logger";
 
 export const history = ipcRenderer ? createBrowserHistory() : createMemoryHistory();
 export const navigation = createObservableHistory(history);
 
 navigation.listen((location, action) => {
-  const clusterId = getHostedClusterId();
-  const frame = clusterId ?? "root";
+  const isClusterView = !process.isMainFrame;
+  const domain = global.location.href;
 
-  logger.debug(`[NAVIGATION]: ${action}`, { location, frame });
+  logger.debug(`[NAVIGATION]: ${action}`, { isClusterView, domain, location });
 });
