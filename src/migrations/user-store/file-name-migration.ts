86815ae9f51bf25a1fdd0506c460fb899c45ca14
Sebastian Malton <sebastian@malton.name>
2021-03-16 08:52:39 -0400

    fix timing issue with fileNameMigration
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/migrations/user-store/file-name-migration.ts
+++ src/migrations/user-store/file-name-migration.ts
@@ -6,12 +6,17 @@ export async function fileNameMigration() {
   const userDataPath = (app || remote.app).getPath("userData");
   const configJsonPath = path.join(userDataPath, "config.json");
   const lensUserStoreJsonPath = path.join(userDataPath, "lens-user-store.json");
-  const [configJsonExists, lensUserStoreJsonExists] = await Promise.all([
-    fse.pathExists(configJsonPath),
-    fse.pathExists(lensUserStoreJsonPath),
-  ]);
 
-  if (configJsonExists && !lensUserStoreJsonExists) {
+  try {
     await fse.move(configJsonPath, lensUserStoreJsonPath);
+  } catch (error) {
+    if (error.code === "ENOENT" && error.path === configJsonPath) { // (No such file or directory)
+      return; // file already moved
+    } else if (error.message === "dest already exists.") {
+      await fse.remove(configJsonPath);
+    } else {
+      // pass other errors along
+      throw error;
+    }
   }
 }
