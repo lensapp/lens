b173d758f2e74f848fff907d4038fbf1bfd93e65
Sebastian Malton <sebastian@malton.name>
2021-03-23 09:14:45 -0400

    change over cluster actions and loaded events
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/navigation/events.ts
+++ src/renderer/navigation/events.ts
@@ -9,6 +9,7 @@ export const enum IpcRendererNavigationEvents {
   CLUSTER_VIEW_CURRENT_ID = "renderer:cluster-id-of-active-view",
   NAVIGATE_IN_APP = "renderer:navigate",
   NAVIGATE_IN_CLUSTER = "renderer:navigate-in-cluster",
+  LOADED = "renderer:loaded",
 }
 
 export function bindEvents() {
