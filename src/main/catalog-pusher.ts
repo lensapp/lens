a884bb03e1b9dcdb0ed932a2312cecfd3404ec18
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-04-01 13:49:02 +0300

    cleanup
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/main/catalog-pusher.ts
+++ src/main/catalog-pusher.ts
@@ -21,7 +21,6 @@ export class CatalogPusher {
   }
 
   broadcast() {
-    console.log("BROADCAST", toJS(this.catalog.items, { recurseEverything: true }));
     broadcastMessage("catalog:items", toJS(this.catalog.items, { recurseEverything: true }));
   }
 }
