42f8016312a33fc0ae438338ee8138fd87553e16
Jim Ehrismann <jehrismann@mirantis.com>
2021-04-12 19:35:02 -0400

    fix lint
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- src/main/__test__/kubeconfig-manager.test.ts
+++ src/main/__test__/kubeconfig-manager.test.ts
@@ -84,8 +84,8 @@ describe("kubeconfig manager tests", () => {
 
     expect(logger.error).not.toBeCalled();
     expect(await kubeConfManager.getPath()).toBe(`tmp${path.sep}kubeconfig-foo`);
-// this causes an intermittent "ENXIO: no such device or address, read" error
-//    const file = await fse.readFile(await kubeConfManager.getPath());
+    // this causes an intermittent "ENXIO: no such device or address, read" error
+    //    const file = await fse.readFile(await kubeConfManager.getPath());
     const file = fse.readFileSync(await kubeConfManager.getPath());
     const yml = loadYaml<any>(file.toString());
 
