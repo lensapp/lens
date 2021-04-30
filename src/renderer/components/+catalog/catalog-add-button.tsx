f125e12b89fe9eafd3682ab27261850651444941
Alex Andreev <alex.andreev.email@gmail.com>
2021-04-30 15:12:13 +0300

    Removing material Tooltip global styles
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/+catalog/catalog-add-button.tsx
+++ src/renderer/components/+catalog/catalog-add-button.tsx
@@ -74,6 +74,9 @@ export class CatalogAddButton extends React.Component<CatalogAddButtonProps> {
             icon={<Icon material={menuItem.icon} />}
             tooltipTitle={menuItem.title}
             onClick={() => menuItem.onClick()}
+            TooltipClasses={{
+              popper: "catalogSpeedDialPopper"
+            }}
           />;
         })}
       </SpeedDial>
