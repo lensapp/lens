b9cd305746fc21ad6929955a9a5fa7d1b7a5297e
Sebastian Malton <sebastian@malton.name>
2021-05-06 11:44:20 -0400

    Fix refreshViews in ClusterView to use props value
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/cluster-manager/cluster-view.tsx
+++ src/renderer/components/cluster-manager/cluster-view.tsx
@@ -70,7 +70,7 @@ export class ClusterView extends React.Component<Props> {
     const { cluster } = this;
     const showStatus = cluster && (!cluster.available || !hasLoadedView(cluster.id) || !cluster.ready);
 
-    refreshViews(cluster.id);
+    refreshViews(this.clusterId);
 
     return (
       <div className="ClusterView flex align-center">
