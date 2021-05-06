f134a701251967b1c02202e4cedb18bac8ca1da8
Sebastian Malton <sebastian@malton.name>
2021-05-06 08:58:15 -0400

    Switch main's route handlers to be static functions
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/lens-api.ts
+++ src/main/lens-api.ts
@@ -1,17 +1,15 @@
 import http from "http";
 
-export abstract class LensApi {
-  protected respondJson(res: http.ServerResponse, content: {}, status = 200) {
-    this.respond(res, JSON.stringify(content), "application/json", status);
-  }
+export function respondJson(res: http.ServerResponse, content: any, status = 200) {
+  respond(res, JSON.stringify(content), "application/json", status);
+}
 
-  protected respondText(res: http.ServerResponse, content: string, status = 200) {
-    this.respond(res, content, "text/plain", status);
-  }
+export function respondText(res: http.ServerResponse, content: string, status = 200) {
+  respond(res, content, "text/plain", status);
+}
 
-  protected respond(res: http.ServerResponse, content: string, contentType: string, status = 200) {
-    res.setHeader("Content-Type", contentType);
-    res.statusCode = status;
-    res.end(content);
-  }
+export function respond(res: http.ServerResponse, content: string, contentType: string, status = 200) {
+  res.setHeader("Content-Type", contentType);
+  res.statusCode = status;
+  res.end(content);
 }
