e2220f95de50acc25fd5e35b92a6467d0bc6a8e2
Alex Andreev <alex.andreev.email@gmail.com>
2021-05-06 11:49:57 +0300

    Move HotbarCell to separate component
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

---
+++ src/renderer/components/hotbar/hotbar-cell.tsx
@@ -0,0 +1,36 @@
+import "./hotbar-menu.scss";
+import "./hotbar.commands";
+
+import React, { HTMLAttributes, ReactNode, useState } from "react";
+
+import { cssNames } from "../../utils";
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: ReactNode;
+  index: number;
+  innerRef?: React.LegacyRef<HTMLDivElement>;
+}
+
+export function HotbarCell({ innerRef, children, className, ...rest }: Props) {
+  const [animating, setAnimating] = useState(false);
+  const onAnimationEnd = () => { setAnimating(false); };
+  const onClick = () => {
+    if (className.includes("isDraggingOver")) {
+      return;
+    }
+
+    setAnimating(true);
+  };
+
+  return (
+    <div
+      className={cssNames("HotbarCell", { animating }, className)}
+      onAnimationEnd={onAnimationEnd}
+      onClick={onClick}
+      ref={innerRef}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
