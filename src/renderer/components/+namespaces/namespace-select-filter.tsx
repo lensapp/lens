93814ae684d0a1e83b9129fd94951d4764598414
Alex Andreev <alex.andreev.email@gmail.com>
2021-05-12 16:20:47 +0300

    Rewrite gradient color for light theme
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/+namespaces/namespace-select-filter.tsx
+++ src/renderer/components/+namespaces/namespace-select-filter.tsx
@@ -7,7 +7,7 @@ import { components, PlaceholderProps } from "react-select";
 import { Icon } from "../icon";
 import { FilterIcon } from "../item-object-list/filter-icon";
 import { FilterType } from "../item-object-list/page-filters.store";
-import { SelectOption } from "../select";
+import { SelectOption, SelectProps } from "../select";
 import { NamespaceSelect } from "./namespace-select";
 import { namespaceStore } from "./namespace.store";
 
@@ -33,9 +33,8 @@ const Placeholder = observer((props: PlaceholderProps<any>) => {
   );
 });
 
-
 @observer
-export class NamespaceSelectFilter extends React.Component {
+export class NamespaceSelectFilter extends React.Component<SelectProps> {
   formatOptionLabel({ value: namespace, label }: SelectOption) {
     if (namespace) {
       const isSelected = namespaceStore.hasContext(namespace);
