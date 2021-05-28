c4f177fe58d335201b4c576c073330561ee68049
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-05-28 07:59:41 +0300

    cleanup
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/extensions/common-api/catalog.ts
+++ src/extensions/common-api/catalog.ts
@@ -19,15 +19,5 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import type { CatalogEntity } from "../../common/catalog";
-import { catalogEntityRegistry as registry } from "../../main/catalog";
-
-export { catalogCategoryRegistry as catalogCategories } from "../../common/catalog/catalog-category-registry";
-
-export class CatalogEntityRegistry {
-  getItemsForApiKind<T extends CatalogEntity>(apiVersion: string, kind: string): T[] {
-    return registry.getItemsForApiKind<T>(apiVersion, kind);
-  }
-}
-
-export const catalogEntities = new CatalogEntityRegistry();
+export * from "../../common/catalog-entities";
+export * from "../../common/catalog/catalog-entity";
