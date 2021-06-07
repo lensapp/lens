60f9ea5cd735803f426a98df3310228023f38e01
Sebastian Malton <sebastian@malton.name>
2021-06-07 12:01:15 -0400

    Revert "Switch to I, remove type parameter default"
    This reverts commit a3073cc99b1cdd85eb693d65bae99b2f7df4842d.

--- src/renderer/components/item-object-list/item-list-layout.tsx
+++ src/renderer/components/item-object-list/item-list-layout.tsx
@@ -46,10 +46,10 @@ import { NamespaceSelectFilter } from "../+namespaces/namespace-select-filter";
 
 // todo: refactor, split to small re-usable components
 
-export type SearchFilter<I extends ItemObject> = (item: I) => string | number | (string | number)[];
-export type SearchFilters<I extends ItemObject> = Record<string, SearchFilter<I>>;
-export type ItemsFilter<I extends ItemObject> = (items: I[]) => I[];
-export type ItemsFilters<I extends ItemObject> = Record<string, ItemsFilter<I>>;
+export type SearchFilter<Item extends ItemObject> = (item: Item) => string | number | (string | number)[];
+export type SearchFilters<Item extends ItemObject> = Record<string, SearchFilter<Item>>;
+export type ItemsFilter<Item extends ItemObject> = (items: Item[]) => Item[];
+export type ItemsFilters<Item extends ItemObject> = Record<string, ItemsFilter<Item>>;
 
 export interface IHeaderPlaceholders {
   title: ReactNode;
@@ -58,22 +58,22 @@ export interface IHeaderPlaceholders {
   info: ReactNode;
 }
 
-export interface ItemListLayoutProps<I extends ItemObject> {
+export interface ItemListLayoutProps<Item extends ItemObject> {
   tableId?: string;
   className: IClassName;
-  items?: I[];
-  store: ItemStore<I>;
+  items?: Item[];
+  store: ItemStore<Item>;
   dependentStores?: ItemStore<ItemObject>[];
   preloadStores?: boolean;
   hideFilters?: boolean;
-  searchFilters?: SearchFilter<I>[];
+  searchFilters?: SearchFilter<Item>[];
   /** @deprecated */
-  filterItems?: ItemsFilter<I>[];
+  filterItems?: ItemsFilter<Item>[];
 
   // header (title, filtering, searching, etc.)
   showHeader?: boolean;
   headerClassName?: IClassName;
-  renderHeaderTitle?: ReactNode | ((parent: ItemListLayout<I>) => ReactNode);
+  renderHeaderTitle?: ReactNode | ((parent: ItemListLayout<Item>) => ReactNode);
   customizeHeader?: (placeholders: IHeaderPlaceholders, content: ReactNode) => Partial<IHeaderPlaceholders> | ReactNode;
 
   // items list configuration
@@ -82,25 +82,25 @@ export interface ItemListLayoutProps<I extends ItemObject> {
   isSearchable?: boolean; // apply search-filter & add search-input
   isConfigurable?: boolean;
   copyClassNameFromHeadCells?: boolean;
-  sortingCallbacks?: TableSortCallbacks<I>;
-  tableProps?: Partial<TableProps<I>>; // low-level table configuration
+  sortingCallbacks?: TableSortCallbacks<Item>;
+  tableProps?: Partial<TableProps<Item>>; // low-level table configuration
   renderTableHeader: TableCellProps[] | null;
-  renderTableContents: (item: I) => (ReactNode | TableCellProps)[];
-  renderItemMenu?: (item: I, store: ItemStore<I>) => ReactNode;
-  customizeTableRowProps?: (item: I) => Partial<TableRowProps>;
+  renderTableContents: (item: Item) => (ReactNode | TableCellProps)[];
+  renderItemMenu?: (item: Item, store: ItemStore<Item>) => ReactNode;
+  customizeTableRowProps?: (item: Item) => Partial<TableRowProps>;
   addRemoveButtons?: Partial<AddRemoveButtonsProps>;
   virtual?: boolean;
 
   // item details view
   hasDetailsView?: boolean;
-  detailsItem?: I;
-  onDetails?: (item: I) => void;
+  detailsItem?: Item;
+  onDetails?: (item: Item) => void;
 
   // other
-  customizeRemoveDialog?: (selectedItems: I[]) => Partial<ConfirmDialogParams>;
-  renderFooter?: (parent: ItemListLayout<I>) => React.ReactNode;
+  customizeRemoveDialog?: (selectedItems: Item[]) => Partial<ConfirmDialogParams>;
+  renderFooter?: (parent: ItemListLayout<Item>) => React.ReactNode;
 
-  filterCallbacks?: ItemsFilters<I>;
+  filterCallbacks?: ItemsFilters<Item>;
 }
 
 const defaultProps: Partial<ItemListLayoutProps<ItemObject>> = {
@@ -119,14 +119,14 @@ const defaultProps: Partial<ItemListLayoutProps<ItemObject>> = {
 };
 
 @observer
-export class ItemListLayout<I extends ItemObject> extends React.Component<ItemListLayoutProps<I>> {
+export class ItemListLayout<Item extends ItemObject> extends React.Component<ItemListLayoutProps<Item>> {
   static defaultProps = defaultProps as object;
 
   private storage = createStorage("item_list_layout", {
     showFilters: false, // setup defaults
   });
 
-  constructor(props: ItemListLayoutProps<I>) {
+  constructor(props: ItemListLayoutProps<Item>) {
     super(props);
     makeObservable(this);
   }
@@ -162,7 +162,7 @@ export class ItemListLayout<I extends ItemObject> extends React.Component<ItemLi
     stores.forEach(store => store.loadAll(namespaceStore.contextNamespaces));
   }
 
-  private filterCallbacks: ItemsFilters<I> = {
+  private filterCallbacks: ItemsFilters<Item> = {
     [FilterType.SEARCH]: items => {
       const { searchFilters, isSearchable } = this.props;
       const search = pageFilters.getValues(FilterType.SEARCH)[0] || "";
@@ -203,7 +203,7 @@ export class ItemListLayout<I extends ItemObject> extends React.Component<ItemLi
     return activeFilters;
   }
 
-  applyFilters(filters: ItemsFilter<I>[], items: I[]): I[] {
+  applyFilters(filters: ItemsFilter<Item>[], items: Item[]): Item[] {
     if (!filters || !filters.length) return items;
 
     return filters.reduce((items, filter) => filter(items), items);
@@ -213,7 +213,7 @@ export class ItemListLayout<I extends ItemObject> extends React.Component<ItemLi
     const { filters, filterCallbacks, props } = this;
     const filterGroups = groupBy<Filter>(filters, ({ type }) => type);
 
-    const filterItems: ItemsFilter<I>[] = [];
+    const filterItems: ItemsFilter<Item>[] = [];
 
     Object.entries(filterGroups).forEach(([type, filtersGroup]) => {
       const filterCallback = filterCallbacks[type] ?? props.filterCallbacks?.[type];
