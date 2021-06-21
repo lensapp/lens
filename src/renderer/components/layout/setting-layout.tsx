8a09eb5c131b0b4885cd38881395f850a64b83c9
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-06-21 08:41:54 +0300

    fix
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/renderer/components/layout/setting-layout.tsx
+++ src/renderer/components/layout/setting-layout.tsx
@@ -19,7 +19,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import "./page-layout.scss";
+import "./setting-layout.scss";
 
 import React from "react";
 import { observer } from "mobx-react";
