428a473ba451741fa09bfceb034c26ac269cf804
Mario Sarcher <mario@sarcher.de>
2021-06-25 11:01:26 +0200

    update button text on welcome screen

--- src/renderer/initializers/welcome-menu-registry.ts
+++ src/renderer/initializers/welcome-menu-registry.ts
@@ -27,7 +27,7 @@ export function initWelcomeMenuRegistry() {
   WelcomeMenuRegistry.getInstance()
     .add([
       {
-        title: "Browse Clusters",
+        title: "Browse Clusters in Catalog",
         icon: "view_list",
         click: () => navigate(catalogURL({ params: { group: "entity.k8slens.dev", kind: "KubernetesCluster" } } )),
       }
