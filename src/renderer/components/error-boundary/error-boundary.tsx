f7bcad0717bd93c960dd4e797c5f54a9ba5d7670
Alex Andreev <alex.andreev.email@gmail.com>
2021-07-05 12:36:00 +0300

    Usign Sentry.ErrorBoundary
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/error-boundary/error-boundary.tsx
+++ src/renderer/components/error-boundary/error-boundary.tsx
@@ -21,126 +21,54 @@
 
 import "./error-boundary.scss";
 
-import React, { ErrorInfo } from "react";
-import { reaction } from "mobx";
-import { disposeOnUnmount, observer } from "mobx-react";
-import {
-  captureEvent,
-  captureException,
-  eventFromException,
-} from "@sentry/browser";
-import type { Event } from "@sentry/types";
-import { parseSemver } from "@sentry/utils";
+import React from "react";
 import { Button } from "../button";
 import { navigation } from "../../navigation";
 import { issuesTrackerUrl, slackUrl } from "../../../common/vars";
-
-interface Props {
-}
-
-interface State {
-  error?: Error;
-  errorInfo?: ErrorInfo;
-}
-
-const reactVersion = parseSemver(React.version);
-
-/**
- * Logs react error boundary errors to Sentry.
- * 
- * @param error An error captured by React Error Boundary
- * @param componentStack The component stacktrace
- * 
- * edited from https://github.com/getsentry/sentry-javascript/blob/master/packages/react/src/errorboundary.tsx
- */
-function captureReactErrorBoundaryError(error: Error, componentStack: string) {
-  const errorBoundaryError = new Error(error.message);
-
-  errorBoundaryError.name = `React ErrorBoundary ${errorBoundaryError.name}`;
-  errorBoundaryError.stack = componentStack;
-
-  let errorBoundaryEvent: Event = {};
-
-  void eventFromException({}, errorBoundaryError).then(e => {
-    errorBoundaryEvent = e;
-  });
-
-  if (
-    errorBoundaryEvent.exception &&
-    Array.isArray(errorBoundaryEvent.exception.values) &&
-    reactVersion.major &&
-    reactVersion.major >= 17
-  ) {
-    let originalEvent: Event = {};
-
-    void eventFromException({}, error).then(e => {
-      originalEvent = e;
-    });
-
-    if (originalEvent.exception && Array.isArray(originalEvent.exception.values)) {
-      originalEvent.exception.values = [...errorBoundaryEvent.exception.values, ...originalEvent.exception.values];
-    }
-
-    return captureEvent(originalEvent);
-  }
-
-  return captureException(error, { contexts: { react: { componentStack } } });
-}
+import * as Sentry from "@sentry/react";
+import { observer } from "mobx-react";
 
 @observer
-export class ErrorBoundary extends React.Component<Props, State> {
-  public state: State = {};
-
-  @disposeOnUnmount
-  resetOnNavigate = reaction(
-    () => navigation.toString(),
-    () => this.setState({ error: null, errorInfo: null })
-  );
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    captureReactErrorBoundaryError(error, errorInfo.componentStack);
-    this.setState({ error, errorInfo });
-  }
-
-  back = () => {
-    navigation.goBack();
-  };
-
+export class ErrorBoundary extends React.Component {
   render() {
-    const { error, errorInfo } = this.state;
+    return (
+      <Sentry.ErrorBoundary
+        fallback={({ error, componentStack, resetError }) => {
+          const slackLink = <a href={slackUrl} rel="noreferrer" target="_blank">Slack</a>;
+          const githubLink = <a href={issuesTrackerUrl} rel="noreferrer" target="_blank">Github</a>;
+          const pageUrl = location.pathname;
 
-    if (error) {
-      const slackLink = <a href={slackUrl} rel="noreferrer" target="_blank">Slack</a>;
-      const githubLink = <a href={issuesTrackerUrl} rel="noreferrer" target="_blank">Github</a>;
-      const pageUrl = location.pathname;
-
-      return (
-        <div className="flex ErrorBoundary column gaps">
-          <h5>
-            App crash at <span className="contrast">{pageUrl}</span>
-          </h5>
-          <p>
-            To help us improve the product please report bugs to {slackLink} community or {githubLink} issues tracker.
-          </p>
-          <div className="wrapper">
-            <code className="block">
-              <p className="contrast">Component stack:</p>
-              {errorInfo.componentStack}
-            </code>
-            <code className="box grow">
-              <p className="contrast">Error stack:</p> <br/>
-              {error.stack}
-            </code>
-          </div>
-          <Button
-            className="box self-flex-start"
-            primary label="Back"
-            onClick={this.back}
-          />
-        </div>
-      );
-    }
-
-    return this.props.children;
+          return (
+            <div className="flex ErrorBoundary column gaps">
+              <h5>
+                App crash at <span className="contrast">{pageUrl}</span>
+              </h5>
+              <p>
+                To help us improve the product please report bugs to {slackLink} community or {githubLink} issues tracker.
+              </p>
+              <div className="wrapper">
+                <code className="block">
+                  <p className="contrast">Component stack:</p>
+                  {componentStack}
+                </code>
+                <code className="box grow">
+                  <p className="contrast">Error stack:</p> <br/>
+                  {error.stack}
+                </code>
+              </div>
+              <Button
+                className="box self-flex-start"
+                primary label="Back"
+                onClick={() => {
+                  resetError();
+                  navigation.goBack();
+                }}
+              />
+            </div>
+          );
+        }}>
+        {this.props.children}
+      </Sentry.ErrorBoundary>
+    );
   }
 }
