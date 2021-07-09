lensapp/lens

commit 77f8ea67bd78da6721114d40e285dc3296087ada

Cleanup sentry integration (#3315)

src/common/utils/isValidSentryDsn.ts
@@ -1,39 +1,0 @@
-/**
- * Copyright (c) 2021 OpenLens Authors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-
-// Regular expression used to parse a Sentry Dsn.
-// from https://github.com/getsentry/sentry-javascript/blob/f624f442fd76bf655de6def9fa4d7631735ebba0/packages/utils/src/dsn.ts#L6
-const DSN_REGEX = /^(?:(\w+):)\/\/(?:(\w+)(?::(\w+))?@)([\w.-]+)(?::(\d+))?\/(.+)/;
-
-/**
- * check if string is a valid Sentry dsn
- *
- * @param {(string | undefined | null)} string
- * @return {boolean} 
- */
-export const isValidSentryDsn = (string: string | undefined | null) => {
-
-  const valid = DSN_REGEX.exec(string);
-
-  return valid;
-
-};

src/common/vars.ts
@@ -24,8 +24,6 @@ import path from "path";
 import { SemVer } from "semver";
 import packageInfo from "../../package.json";
 import { defineGlobal } from "./utils/defineGlobal";
-import { isValidURL } from "./utils/isValidURL";
-import { isValidSentryDsn } from "./utils/isValidSentryDsn";
 
 export const isMac = process.platform === "darwin";
 export const isWindows = process.platform === "win32";
@@ -71,8 +69,6 @@ export const slackUrl = "https://join.slack.com/t/k8slens/shared_invite/enQtOTc5
 export const supportUrl = "https://docs.k8slens.dev/latest/support/" as string;
 
 export const appSemVer = new SemVer(packageInfo.version);
-export const docsUrl = `https://docs.k8slens.dev/main/` as string;
+export const docsUrl = "https://docs.k8slens.dev/main/" as string;
 
 export const sentryDsn = packageInfo.config?.sentryDsn;
-export const sentryDsnIsValid = isValidURL(sentryDsn) && isValidSentryDsn(sentryDsn);
-

src/renderer/components/+preferences/preferences.tsx
@@ -41,7 +41,7 @@ import { FormSwitch, Switcher } from "../switch";
 import { KubeconfigSyncs } from "./kubeconfig-syncs";
 import { SettingLayout } from "../layout/setting-layout";
 import { Checkbox } from "../checkbox";
-import { sentryDsnIsValid } from "../../../common/vars";
+import { sentryIsInitialized } from "../../../common/sentry";
 
 enum Pages {
   Application = "application",
@@ -259,7 +259,7 @@ export class Preferences extends React.Component {
           <section id="telemetry">
             <h2 data-testid="telemetry-header">Telemetry</h2>
             {telemetryExtensions.map(this.renderExtension)}
-            {sentryDsnIsValid ? (
+            {sentryIsInitialized ? (
               <React.Fragment key='sentry'>
                 <section id='sentry' className="small">
                   <SubTitle title='Automatic Error Reporting' />

src/common/sentry.ts
@@ -20,96 +20,73 @@
  */
 
 import { CaptureConsole, Dedupe, Offline } from "@sentry/integrations";
-import type { Event as SentryEvent } from "@sentry/types";
-import { sentryDsn, sentryDsnIsValid, isProduction } from "./vars";
+import { sentryDsn, isProduction } from "./vars";
 import { UserStore } from "./user-store";
 import logger from "../main/logger";
 
-// https://www.electronjs.org/docs/api/process#processtype-readonly
-const electronProcessType = ["browser", "renderer", "worker"] as const;
+export let sentryIsInitialized = false;
 
-export const integrations = [
-  new CaptureConsole({ levels: ["error"] }),
-  new Dedupe(),
-  new Offline()
-];
-
-const initialScope = (processType: typeof electronProcessType[number]) => {
-  return {
-    tags: {
-      // "translate" browser to 'main' as Lens developer more familiar with the term 'main'
-      "process": processType === "browser" ? "main" : "renderer"
-    }
-  };
-};
-
-export const environment = isProduction ? "production" : "development";
-
-const logInitError = (reason: string) => {
-  logger.warn(`⚠️  [SENTRY-INIT]: ${reason}, Sentry is not initialized.`);
-};
-
-export const beforeSend = (event: SentryEvent) => {
-  const allowErrorReporting = UserStore.getInstance().allowErrorReporting;
-
-  if (allowErrorReporting) return event;
-
-  logger.info(`🔒  [SENTRY-BEFORE-SEND-HOOK]: allowErrorReporting: ${allowErrorReporting}. Sentry event is caught but not sent to server.`);
-  logger.info("🔒  [SENTRY-BEFORE-SEND-HOOK]: === START OF SENTRY EVENT ===");
-  logger.info(event);
-  logger.info("🔒  [SENTRY-BEFORE-SEND-HOOK]: ===  END OF SENTRY EVENT  ===");
-
-  // if return null, the event won't be sent
-  // ref https://github.com/getsentry/sentry-javascript/issues/2039
-  return null;
-};
-
-export const SentryInit = () => {
-  if (!sentryDsnIsValid) {
-    logInitError("Sentry DSN is not valid");
-
-    return;
+/**
+ * This function bypasses webpack issues.
+ *
+ * See: https://docs.sentry.io/platforms/javascript/guides/electron/#webpack-configuration
+ */
+async function requireSentry() {
+  switch (process.type) {
+    case "browser":
+      return import("@sentry/electron/dist/main");
+    case "renderer":
+      return import("@sentry/electron/dist/renderer");
+    default:
+      throw new Error(`Unsupported process type ${process.type}`);
   }
+}
 
-  const processType = process.type as typeof electronProcessType[number];
-
-  let Sentry;
-
+/**
+ * Initialize Sentry for the current process so to send errors for debugging.
+ */
+export async function SentryInit(): Promise<void> {
   try {
-    // if in main process 
-    if (processType === "browser") {
-      Sentry = require("@sentry/electron/dist/main");
-    }
+    const Sentry = await requireSentry();
 
-    // if in renderer process
-    if (processType === "renderer") {
-      Sentry = require("@sentry/electron/dist/renderer");
+    try {
+      Sentry.init({
+        beforeSend: event => {
+          if (UserStore.getInstance().allowErrorReporting) {
+            return event;
+          }
+
+          logger.info(`🔒  [SENTRY-BEFORE-SEND-HOOK]: allowErrorReporting: false. Sentry event is caught but not sent to server.`);
+          logger.info("🔒  [SENTRY-BEFORE-SEND-HOOK]: === START OF SENTRY EVENT ===");
+          logger.info(event);
+          logger.info("🔒  [SENTRY-BEFORE-SEND-HOOK]: ===  END OF SENTRY EVENT  ===");
+
+          // if return null, the event won't be sent
+          // ref https://github.com/getsentry/sentry-javascript/issues/2039
+          return null;
+        },
+        dsn: sentryDsn,
+        integrations: [
+          new CaptureConsole({ levels: ["error"] }),
+          new Dedupe(),
+          new Offline()
+        ],
+        initialScope: {
+          tags: {
+            // "translate" browser to 'main' as Lens developer more familiar with the term 'main'
+            "process": process.type === "browser" ? "main" : "renderer"
+          }
+        },
+        environment: isProduction ? "production" : "development",
+      });
+
+      sentryIsInitialized = true;
+
+      logger.info(`✔️  [SENTRY-INIT]: Sentry for ${process.type} is initialized.`);
+    } catch (error) {
+      logger.warn(`⚠️  [SENTRY-INIT]: Sentry.init() error: ${error?.message ?? error}.`);
     }
   } catch (error) {
-    logInitError(`Error loading Sentry module ${error?.message}`);
-
-    return;
+    logger.warn(`⚠️  [SENTRY-INIT]: Error loading Sentry module ${error?.message ?? error}.`);
   }
-
-
-  if (!Sentry) {
-    logInitError(`Unsupported process type ${processType}`);
-
-    return;
-  }
-
-  try {
-    Sentry.init({
-      beforeSend,
-      dsn: sentryDsn,
-      integrations,
-      initialScope: initialScope(processType),
-      environment
-    });
-    logger.info(`✔️  [SENTRY-INIT]: Sentry for ${processType} is initialized.`);
-  } catch (error) {
-    logInitError(`Sentry.init() error ${error?.message}`);
-
-    return;
-  }
-};
+}

src/common/utils/index.ts
@@ -51,8 +51,6 @@ export * from "./tar";
 export * from "./toggle-set";
 export * from "./toJS";
 export * from "./type-narrowing";
-export * from "./isValidURL";
-export * from "./isValidSentryDsn";
 
 import * as iter from "./iter";
 

src/common/utils/isValidURL.ts
@@ -1,39 +1,0 @@
-/**
- * Copyright (c) 2021 OpenLens Authors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
-
-/**
- * check if string is valid URL
- *
- * @param {(string | undefined | null)} url
- * @return {boolean} 
- */
-export const isValidURL = (url: string | undefined | null) => {
-  try {
-    new URL(url);
-
-    return true;
-  } catch {
-    // ignore TypeError and return false
-  }
-
-  return false;
-};

src/main/index.ts
@@ -61,8 +61,6 @@ import { ExtensionsStore } from "../extensions/extensions-store";
 import { FilesystemProvisionerStore } from "./extension-filesystem";
 import { SentryInit } from "../common/sentry";
 
-SentryInit();
-
 const workingDir = path.join(app.getPath("appData"), appName);
 const cleanup = disposer();
 
@@ -141,8 +139,15 @@ app.on("ready", async () => {
 
   logger.info("💾 Loading stores");
 
-  ClusterStore.createInstance().provideInitialFromMain();
   UserStore.createInstance().startMainReactions();
+
+  /**
+   * There is no point setting up sentry before UserStore is initialized as
+   * `allowErrorReporting` will always be falsy.
+   */
+  await SentryInit();
+
+  ClusterStore.createInstance().provideInitialFromMain();
   HotbarStore.createInstance();
   ExtensionsStore.createInstance();
   FilesystemProvisionerStore.createInstance();

src/renderer/bootstrap.tsx
@@ -47,6 +47,7 @@ import { WeblinkStore } from "../common/weblink-store";
 import { ExtensionsStore } from "../extensions/extensions-store";
 import { FilesystemProvisionerStore } from "../main/extension-filesystem";
 import { ThemeStore } from "./theme.store";
+import { SentryInit } from "../common/sentry";
 
 configurePackages();
 
@@ -85,6 +86,13 @@ export async function bootstrap(App: AppComponent) {
   ExtensionDiscovery.createInstance().init();
 
   UserStore.createInstance();
+
+  /**
+   * There is no point setting up sentry before UserStore is initialized as
+   * `allowErrorReporting` will always be falsy.
+   */
+  await SentryInit();
+
   await ClusterStore.createInstance().loadInitialOnRenderer();
   HotbarStore.createInstance();
   ExtensionsStore.createInstance();

commit b68288e8e42ef81360c73d91684c285c0ab82134

Adding back isValidSentryDsn and sentryDsnIsValid, and use sentryDsnIsValid to determine the UI should be shown, regression introduced in https://github.com/lensapp/lens/pull/3315
Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

src/common/utils/isValidSentryDsn.ts
@@ -19,40 +19,20 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+
+// Regular expression used to parse a Sentry Dsn.
+// from https://github.com/getsentry/sentry-javascript/blob/f624f442fd76bf655de6def9fa4d7631735ebba0/packages/utils/src/dsn.ts#L6
+const DSN_REGEX = /^(?:(\w+):)\/\/(?:(\w+)(?::(\w+))?@)([\w.-]+)(?::(\d+))?\/(.+)/;
+
 /**
- * A function that does nothing
+ * check if string is a valid Sentry dsn
+ *
+ * @param {(string | undefined | null)} string
+ * @return {boolean} 
  */
-export function noop<T extends any[]>(...args: T): void {
-  return void args;
-}
+export const isValidSentryDsn = (string: string | undefined | null) => {
 
-export * from "./app-version";
-export * from "./autobind";
-export * from "./base64";
-export * from "./camelCase";
-export * from "./cloneJson";
-export * from "./debouncePromise";
-export * from "./defineGlobal";
-export * from "./delay";
-export * from "./disposer";
-export * from "./downloadFile";
-export * from "./escapeRegExp";
-export * from "./extended-map";
-export * from "./getRandId";
-export * from "./hash-set";
-export * from "./n-fircate";
-export * from "./openExternal";
-export * from "./paths";
-export * from "./reject-promise";
-export * from "./singleton";
-export * from "./sort-compare";
-export * from "./splitArray";
-export * from "./tar";
-export * from "./toggle-set";
-export * from "./toJS";
-export * from "./type-narrowing";
-export * from "./isValidSentryDsn";
+  const valid = DSN_REGEX.exec(string);
 
-import * as iter from "./iter";
-
-export { iter };
+  return valid;
+};

src/common/vars.ts
@@ -24,6 +24,7 @@ import path from "path";
 import { SemVer } from "semver";
 import packageInfo from "../../package.json";
 import { defineGlobal } from "./utils/defineGlobal";
+import { isValidSentryDsn } from "./utils/isValidSentryDsn";
 
 export const isMac = process.platform === "darwin";
 export const isWindows = process.platform === "win32";
@@ -71,4 +72,5 @@ export const supportUrl = "https://docs.k8slens.dev/latest/support/" as string;
 export const appSemVer = new SemVer(packageInfo.version);
 export const docsUrl = "https://docs.k8slens.dev/main/" as string;
 
-export const sentryDsn = packageInfo.config?.sentryDsn;
+export const sentryDsn = packageInfo.config?.sentryDsn ?? "";
+export const sentryDsnIsValid = isValidSentryDsn(sentryDsn);

src/renderer/components/+preferences/preferences.tsx
@@ -41,7 +41,7 @@ import { FormSwitch, Switcher } from "../switch";
 import { KubeconfigSyncs } from "./kubeconfig-syncs";
 import { SettingLayout } from "../layout/setting-layout";
 import { Checkbox } from "../checkbox";
-import { sentryIsInitialized } from "../../../common/sentry";
+import { sentryDsnIsValid } from "../../../common/vars";
 
 enum Pages {
   Application = "application",
@@ -259,7 +259,7 @@ export class Preferences extends React.Component {
           <section id="telemetry">
             <h2 data-testid="telemetry-header">Telemetry</h2>
             {telemetryExtensions.map(this.renderExtension)}
-            {sentryIsInitialized ? (
+            {sentryDsnIsValid ? (
               <React.Fragment key='sentry'>
                 <section id='sentry' className="small">
                   <SubTitle title='Automatic Error Reporting' />