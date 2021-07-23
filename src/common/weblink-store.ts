da53aa0d79d7df21aaf4ab52ec83eeea53c79305
Sebastian Malton <sebastian@malton.name>
2021-07-22 20:28:01 -0400

    Treat default weblinks same as any other weblinks (#3385)
    - Use seenWeblink and check for non-arrays
    
    - Spread out link validation
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/weblink-store.ts
+++ src/common/weblink-store.ts
@@ -21,7 +21,7 @@
 
 import { action, comparer, observable, makeObservable } from "mobx";
 import { BaseStore } from "./base-store";
-import migrations from "../migrations/hotbar-store";
+import migrations from "../migrations/weblinks-store";
 import * as uuid from "uuid";
 import { toJS } from "./utils";
 
