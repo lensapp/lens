4e52f661fa9ab8143f2bbf6f3af91df999a7ccc7
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2021-09-06 08:32:23 +0300

    cleanup
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

--- src/main/utils/__test__/update-channel.test.ts
+++ src/main/utils/__test__/update-channel.test.ts
@@ -19,7 +19,6 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-jest.mock("electron-updater");
 import { nextUpdateChannel } from "../update-channel";
 
 describe("nextUpdateChannel", () => {
