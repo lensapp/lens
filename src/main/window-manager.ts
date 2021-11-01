1df9051172a89ccaade4734b3e02935c9fc20a38
Jim Ehrismann <jehrismann@mirantis.com>
2021-11-01 17:10:51 -0400

    Revert "Electron 13.4.0 (#3820)"
    This reverts commit 65d7fca691c0f5a6d01a0f324b4209ce092f9888.

--- src/main/window-manager.ts
+++ src/main/window-manager.ts
@@ -109,6 +109,10 @@ export class WindowManager extends Singleton {
           app.dock?.hide(); // hide icon in dock (mac-os)
         })
         .webContents
+        .on("new-window", (event, url) => {
+          event.preventDefault();
+          shell.openExternal(url);
+        })
         .on("dom-ready", () => {
           appEventBus.emit({ name: "app", action: "dom-ready" });
         })
@@ -146,10 +150,6 @@ export class WindowManager extends Singleton {
 
           // Always disable Node.js integration for all webviews
           webPreferences.nodeIntegration = false;
-        }).setWindowOpenHandler((details) => {
-          shell.openExternal(details.url);
-
-          return { action: "deny" };
         });
     }
 
