34ffdb9c5b6e1013938e344d02de5f1297936a0c
Panu Horsmalahti <phorsmalahti@mirantis.com>
2021-11-15 15:23:50 +0200

    Export KubeStatus to extensions api.
    Signed-off-by: Panu Horsmalahti <phorsmalahti@mirantis.com>

--- src/extensions/main-api/k8s-api.ts
+++ src/extensions/main-api/k8s-api.ts
@@ -23,7 +23,7 @@ export { isAllowedResource } from "../../common/utils/allowed-resource";
 export { ResourceStack } from "../../common/k8s/resource-stack";
 export { apiManager } from "../../common/k8s-api/api-manager";
 export { KubeApi, forCluster, forRemoteCluster } from "../../common/k8s-api/kube-api";
-export { KubeObject } from "../../common/k8s-api/kube-object";
+export { KubeObject, KubeStatus } from "../../common/k8s-api/kube-object";
 export { KubeObjectStore } from "../../common/k8s-api/kube-object.store";
 export { Pod, podsApi, PodsApi } from "../../common/k8s-api/endpoints/pods.api";
 export { Node, nodesApi, NodesApi } from "../../common/k8s-api/endpoints/nodes.api";
