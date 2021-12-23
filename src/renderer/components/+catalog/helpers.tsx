17c29ad161db9c366671adb713124ce1a6fd62c3
Alex Andreev <alex.andreev.email@gmail.com>
2021-12-23 15:21:52 +0300

    Fix styles file name
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/+catalog/helpers.tsx
+++ src/renderer/components/+catalog/helpers.tsx
@@ -19,7 +19,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import styles from "./catalog.module.css";
+import styles from "./catalog.module.scss";
 import React from "react";
 import { KubeObject } from "../../../common/k8s-api/kube-object";
 import type { CatalogEntity } from "../../api/catalog-entity";
