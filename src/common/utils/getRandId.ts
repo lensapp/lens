b48f237a66749696a17fcffcdc077353ac3f3376
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-01-13 21:29:49 +0530

    replacing the deprecated substr() with slice()
    Signed-off-by: Gaurav Agrawal <gauravag.1005@gmail.com>

--- src/common/utils/getRandId.ts
+++ src/common/utils/getRandId.ts
@@ -22,7 +22,7 @@
 // Create random system name
 
 export function getRandId({ prefix = "", suffix = "", sep = "_" } = {}) {
-  const randId = () => Math.random().toString(16).substr(2);
+  const randId = () => Math.random().toString(16).slice(2);
 
   return [prefix, randId(), suffix].filter(s => s).join(sep);
 }
