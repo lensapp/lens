c82fa922227c08014e3716b5a9108505a6a7caf4
Sebastian Malton <sebastian@malton.name>
2022-01-17 13:55:06 -0500

    Revert "Add auto-detect support for Victoria Metrics Single (#4693)"
    This reverts commit ae10c4d87a26bc42ebd981733d6c1590266e7a7f.

--- src/main/initializers/metrics-providers.ts
+++ src/main/initializers/metrics-providers.ts
@@ -24,13 +24,11 @@ import { PrometheusHelm } from "../prometheus/helm";
 import { PrometheusLens } from "../prometheus/lens";
 import { PrometheusOperator } from "../prometheus/operator";
 import { PrometheusStacklight } from "../prometheus/stacklight";
-import { PrometheusVictoriaMetricsSingle } from "../prometheus/victoria-metrics-single";
 
 export function initPrometheusProviderRegistry() {
   PrometheusProviderRegistry
     .getInstance()
     .registerProvider(new PrometheusLens())
-    .registerProvider(new PrometheusVictoriaMetricsSingle())
     .registerProvider(new PrometheusHelm())
     .registerProvider(new PrometheusOperator())
     .registerProvider(new PrometheusStacklight());
