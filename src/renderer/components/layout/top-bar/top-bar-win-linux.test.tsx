2665cfc594e65820b973ff7a446f6079ab6acf18
Sebastian Malton <sebastian@malton.name>
2022-01-24 13:12:57 -0500

    Fix test
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/layout/top-bar/top-bar-win-linux.test.tsx
+++ src/renderer/components/layout/top-bar/top-bar-win-linux.test.tsx
@@ -7,14 +7,12 @@ import React from "react";
 import { fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { TopBar } from "./top-bar";
-import { broadcastMessage } from "../../../../common/ipc";
 import * as vars from "../../../../common/vars";
 import { getDiForUnitTesting } from "../../../getDiForUnitTesting";
 import { DiRender, renderFor } from "../../test-utils/renderFor";
 import directoryForUserDataInjectable from "../../../../common/app-paths/directory-for-user-data/directory-for-user-data.injectable";
 import mockFs from "mock-fs";
-import { requestWindowAction } from "../../../ipc";
-import { windowOpenAppMenuAsContextMenuChannel } from "../../../../common/ipc/window";
+import { emitOpenAppMenuAsContextMenu, requestWindowAction } from "../../../ipc";
 
 const mockConfig = vars as { isWindows: boolean; isLinux: boolean };
 
@@ -88,7 +86,7 @@ describe("<TopBar/> in Windows and Linux", () => {
     const close = getByTestId("window-close");
 
     fireEvent.click(menu);
-    expect(broadcastMessage).toHaveBeenCalledWith(windowOpenAppMenuAsContextMenuChannel);
+    expect(emitOpenAppMenuAsContextMenu).toHaveBeenCalledWith();
 
     fireEvent.click(minimize);
     expect(requestWindowAction).toHaveBeenCalledWith("minimize");
