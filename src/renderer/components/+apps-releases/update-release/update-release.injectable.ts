c6f42d29486b0d6978da31cf3f8b8c9c20ab9437
Janne Savolainen <janne.savolainen@live.fi>
2022-02-01 15:28:18 +0200

    Fix helm releases not updating after upgrade
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/components/+apps-releases/update-release/update-release.injectable.ts
+++ src/renderer/components/+apps-releases/update-release/update-release.injectable.ts
@@ -3,10 +3,29 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable, lifecycleEnum } from "@ogre-tools/injectable";
-import { updateRelease } from "../../../../common/k8s-api/endpoints/helm-releases.api";
+import {
+  IReleaseUpdatePayload,
+  updateRelease,
+} from "../../../../common/k8s-api/endpoints/helm-releases.api";
+import releasesInjectable from "../releases.injectable";
 
 const updateReleaseInjectable = getInjectable({
-  instantiate: () => updateRelease,
+  instantiate: (di) => {
+    const releases = di.inject(releasesInjectable);
+
+    return async (
+      name: string,
+      namespace: string,
+      payload: IReleaseUpdatePayload,
+    ) => {
+      const result = await updateRelease(name, namespace, payload);
+
+      releases.invalidate();
+
+      return result;
+    };
+  },
+
   lifecycle: lifecycleEnum.singleton,
 });
 
