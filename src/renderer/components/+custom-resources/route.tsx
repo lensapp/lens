79685370e369c8a1b4f05d0a835fbeb6e1229171
Sebastian Malton <sebastian@malton.name>
2022-02-07 16:54:20 -0500

    Fix custom-resource navigation
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+custom-resources/route.tsx
+++ src/renderer/components/+custom-resources/route.tsx
@@ -4,40 +4,18 @@
  */
 
 import React from "react";
-import { observer } from "mobx-react";
 import { Redirect, Route, Switch } from "react-router";
 import { TabLayout } from "../layout/tab-layout";
-import { crdURL } from "../../../common/routes";
-import type { IComputedValue } from "mobx";
-import type { CustomResourceGroupTabLayoutRoute } from "./route-tabs.injectable";
-import { withInjectables } from "@ogre-tools/injectable-react";
-import customResourcesRouteTabsInjectable from "./route-tabs.injectable";
+import { crdDefinitionsRoute, crdResourcesRoute, crdURL } from "../../../common/routes";
+import { CrdList, CrdResources } from ".";
 
-interface Dependencies {
-  routes: IComputedValue<CustomResourceGroupTabLayoutRoute[]>;
-}
-
-const NonInjectedCustomResourcesRoute = observer(({ routes }: Dependencies) => (
+export const CustomResourcesRoute = () => (
   <TabLayout>
     <Switch>
-      {
-        routes.get().map(({ id, component, routePath, exact }) => (
-          <Route
-            key={id}
-            component={component}
-            path={routePath}
-            exact={exact}
-          />
-        ))
-      }
+      <Route component={CrdList} {...crdDefinitionsRoute} exact/>
+      <Route component={CrdResources} {...crdResourcesRoute}/>
       <Redirect to={crdURL()}/>
     </Switch>
   </TabLayout>
-));
+);
 
-export const CustomResourcesRoute = withInjectables<Dependencies>(NonInjectedCustomResourcesRoute, {
-  getProps: (di, props) => ({
-    routes: di.inject(customResourcesRouteTabsInjectable),
-    ...props,
-  }),
-});
