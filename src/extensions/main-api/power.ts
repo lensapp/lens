0a7fbc162648c20c9f34a58797d27bfc1e68fa38
Juho Heikka <juho.heikka@gmail.com>
2022-02-10 18:59:20 +0200

    Add documentation for power monitor functions
    Signed-off-by: Juho Heikka <juho.heikka@gmail.com>

--- src/extensions/main-api/power.ts
+++ src/extensions/main-api/power.ts
@@ -4,8 +4,17 @@
  */
 import { powerMonitor } from "electron";
 import type { Disposer } from "../../common/utils/disposer";
+
+/**
+ * Event listener for system power events
+ */
 export type PowerEventListener = () => void;
 
+/**
+ * Adds event listener to system suspend events
+ * @param listener function which will be called on system suspend
+ * @returns function to remove event listener
+ */
 export const onSuspend = (listener: PowerEventListener): Disposer => {
   powerMonitor.on("suspend", listener);
 
@@ -14,6 +23,11 @@ export const onSuspend = (listener: PowerEventListener): Disposer => {
   };
 };
 
+/**
+ * Adds event listener to system resume event
+ * @param listener function which will be called on system resume
+ * @returns function to remove event listener
+ */
 export const onResume = (listener: PowerEventListener): Disposer => {
   powerMonitor.on("resume", listener);
 
@@ -22,6 +36,12 @@ export const onResume = (listener: PowerEventListener): Disposer => {
   };
 };
 
+/**
+ * Adds event listener to the event which is emitted when
+ * the system is about to reboot or shut down
+ * @param listener function which will be called on system shutdown
+ * @returns function to remove event listener
+ */
 export const onShutdown = (listener: PowerEventListener): Disposer => {
   powerMonitor.on("shutdown", listener);
 
