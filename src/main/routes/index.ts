f15dd3b3b1d9cb45081b79fc016142071f9ebf8e
Janne Savolainen <janne.savolainen@live.fi>
2022-02-11 10:52:12 +0200

    Remove HelmRoute for not being used anymore
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -6,6 +6,5 @@
 export * from "./kubeconfig-route";
 export * from "./metrics-route";
 export * from "./port-forward-route";
-export * from "./helm-route";
 export * from "./resource-applier-route";
 export * from "./version-route";
