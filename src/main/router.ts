3d803ff1c2496a9fa7098bdac29b5bf412ef4560
Janne Savolainen <janne.savolainen@live.fi>
2022-02-11 10:42:26 +0200

    Switch to using injectable to setup route for installing chart
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/router.ts
+++ src/main/router.ts
@@ -183,7 +183,6 @@ export class Router {
     this.router.add({ method: "delete", path: `${apiPrefix}/pods/port-forward/{namespace}/{resourceType}/{resourceName}` }, PortForwardRoute.routeCurrentPortForwardStop);
 
     // Helm API
-    this.router.add({ method: "post", path: `${apiPrefix}/v2/releases` }, HelmApiRoute.installChart);
     this.router.add({ method: `put`, path: `${apiPrefix}/v2/releases/{namespace}/{release}` }, HelmApiRoute.updateRelease);
     this.router.add({ method: `put`, path: `${apiPrefix}/v2/releases/{namespace}/{release}/rollback` }, HelmApiRoute.rollbackRelease);
     this.router.add({ method: "get", path: `${apiPrefix}/v2/releases/{namespace?}` }, HelmApiRoute.listReleases);
