dd819bb534994a4ac76938c057c39c988cf9fa6c
Sebastian Malton <sebastian@malton.name>
2022-02-16 14:43:19 -0500

    Refine UserStore from/to usage (#3292)

--- src/common/utils/objects.ts
+++ src/common/utils/objects.ts
@@ -7,6 +7,10 @@
  * A better typed version of `Object.fromEntries` where the keys are known to
  * be a specific subset
  */
-export function fromEntries<T, Key extends string>(entries: Iterable<readonly [Key, T]>): { [k in Key]: T } {
+export function fromEntries<T, Key extends string>(entries: Iterable<readonly [Key, T]>): Record<Key, T> {
   return Object.fromEntries(entries) as { [k in Key]: T };
+}
+
+export function entries<T extends Record<string, any>>(obj: T): [keyof T, T[keyof T]][] {
+  return Object.entries(obj);
 }
