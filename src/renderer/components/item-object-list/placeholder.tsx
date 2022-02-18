ed17e064ce086ce08652e515a722b11776f6ec52
Alex Andreev <alex.andreev.email@gmail.com>
2022-02-18 15:05:10 +0300

    Respect hidden columns
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/item-object-list/placeholder.tsx
+++ src/renderer/components/item-object-list/placeholder.tsx
@@ -7,13 +7,15 @@ import styles from "./placeholder.module.scss";
 
 import React from "react";
 import type { TableCellProps } from "../table";
+import { UserStore } from "../../../common/user-store";
 
 interface Props {
   renderTableHeader: TableCellProps[];
   showExtraColumn?: boolean;
+  tableId: string;
 }
 
-export function Placeholder({ renderTableHeader, showExtraColumn = true }: Props) {
+export function Placeholder({ renderTableHeader, showExtraColumn = true, tableId }: Props) {
   const linesNumber = 3;
 
   function renderLines() {
@@ -28,9 +30,15 @@ export function Placeholder({ renderTableHeader, showExtraColumn = true }: Props
     return React.Children.toArray(lines);
   }
 
+  function showColumn({ id: columnId, showWithColumn }: TableCellProps): boolean {
+    return !UserStore.getInstance().isTableColumnHidden(tableId, columnId, showWithColumn);
+  }
+
+  const filteredColumns = renderTableHeader.filter(showColumn);
+
   return (
-    <div className={styles.placeholder}>
-      {renderTableHeader.map((cellProps) => {
+    <div className={styles.placeholder} aria-hidden="true">
+      {filteredColumns.map((cellProps) => {
         return (
           <div key={cellProps.id} className={cellProps.className}>
             {renderLines()}
