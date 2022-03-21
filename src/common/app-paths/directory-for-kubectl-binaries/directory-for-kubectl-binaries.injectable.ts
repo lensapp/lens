b947568b1409b7d4602651a83279c08d126faf92
Iku-turso <mikko.aspiala@gmail.com>
2022-03-21 16:14:17 +0200

    Make a directory commonly available
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- src/common/app-paths/directory-for-kubectl-binaries/directory-for-kubectl-binaries.injectable.ts
+++ src/common/app-paths/directory-for-kubectl-binaries/directory-for-kubectl-binaries.injectable.ts
@@ -3,7 +3,7 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import directoryForBinariesInjectable from "../../../common/app-paths/directory-for-binaries/directory-for-binaries.injectable";
+import directoryForBinariesInjectable from "../directory-for-binaries/directory-for-binaries.injectable";
 import path from "path";
 
 const directoryForKubectlBinariesInjectable = getInjectable({
