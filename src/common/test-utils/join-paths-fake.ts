6a9f77757fb114d85c0be99cd74615a92ffa45e3
Janne Savolainen <janne.savolainen@live.fi>
2022-03-30 12:28:20 +0300

    Fix fake implementation for join paths
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/common/test-utils/join-paths-fake.ts
+++ src/common/test-utils/join-paths-fake.ts
@@ -2,7 +2,6 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { join } from "lodash/fp";
 import type { JoinPaths } from "../path/join-paths.injectable";
 
-export const joinPathsFake: JoinPaths = join("/");
+export const joinPathsFake: JoinPaths = (...args) => args.join("/");
