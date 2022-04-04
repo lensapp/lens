4fdb46f30ccdcebf4b55a83efe7e3ff00804f7d4
Alex Andreev <alex.andreev.email@gmail.com>
2022-04-04 15:00:27 +0300

    Introducing VerticalBar
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

---
+++ src/renderer/components/VerticalBar/vertical-bar.tsx
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import styles from "./vertical-bar.module.scss";
+
+import React, { HTMLAttributes } from "react";
+import { cssNames } from "../../utils";
+
+interface BarProps extends HTMLAttributes<HTMLDivElement> {
+  color: string;
+  value: number;
+}
+
+export function VerticalBar({ color, className, value }: BarProps) {
+  return (
+    <div className={styles.verticalBar}>
+      <div className={cssNames(styles.value, className)} style={{ backgroundColor: color, height: `${value}%` }}></div>
+    </div>
+  );
+}
