86195eb12533bea52da34981b7d74da16666720f
DmitriyNoa <dmytro.zharkov@gmail.com>
2022-04-08 14:35:21 +0200

    Use Number to cast
    Signed-off-by: DmitriyNoa <dmytro.zharkov@gmail.com>

--- src/common/vars.ts
+++ src/common/vars.ts
@@ -136,4 +136,4 @@ export const docsUrl = "https://docs.k8slens.dev/main/" as string;
 
 export const sentryDsn = packageInfo.config?.sentryDsn ?? "";
 
-export const webpackDevServerPort: number = +process.env.WEBPACK_DEV_SERVER_PORT || 9191;
+export const webpackDevServerPort: number = Number(process.env.WEBPACK_DEV_SERVER_PORT) || 9191;
