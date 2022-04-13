4eb54a9296a7ee0c0c05b587aac3cc16e6c888a6
Alex Andreev <alex.andreev.email@gmail.com>
2022-04-13 15:48:37 +0300

    Add Open Cluster Settings link
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/+cluster/cluster-no-metrics.tsx
+++ src/renderer/components/+cluster/cluster-no-metrics.tsx
@@ -3,20 +3,33 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
+import styles from "./cluster-no-metrics.module.scss";
+
 import React from "react";
 import { Icon } from "../icon";
 import { cssNames } from "../../utils";
+import { broadcastMessage } from "../../../common/ipc";
+import { IpcRendererNavigationEvents } from "../../navigation/events";
+import { catalogEntityRegistry } from "../../api/catalog-entity-registry";
 
 export interface ClusterNoMetricsProps {
   className: string;
 }
 
 export function ClusterNoMetrics({ className }: ClusterNoMetricsProps) {
+  function getClusterId() {
+    return catalogEntityRegistry.activeEntity;
+  }
+
+  function openSettingsPage() {
+    broadcastMessage(IpcRendererNavigationEvents.NAVIGATE_IN_APP, `/entity/${getClusterId()?.getId()}/settings`);
+  }
+
   return (
-    <div className={cssNames("ClusterNoMetrics flex column box grow justify-center align-center", className)}>
+    <div className={cssNames(styles.ClusterNoMetrics, className)}>
       <Icon material="info"/>
       <p>Metrics are not available due to missing or invalid Prometheus configuration.</p>
-      <p>Right click cluster icon to open cluster settings.</p>
+      <p><span className={styles.link} onClick={openSettingsPage}>Open cluster settings</span></p>
     </div>
   );
 }
