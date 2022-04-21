52488b7188e084bac2d715044e93e42d389e1f17
Sebastian Malton <sebastian@malton.name>
2022-04-21 13:12:27 -0400

    fix navigation-to-kubernetes-preferences.test.ts
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/behaviours/preferences/navigation-to-kubernetes-preferences.test.ts
+++ src/behaviours/preferences/navigation-to-kubernetes-preferences.test.ts
@@ -7,9 +7,8 @@ import type { ApplicationBuilder } from "../../renderer/components/test-utils/ge
 import { getApplicationBuilder } from "../../renderer/components/test-utils/get-application-builder";
 import userStoreInjectable from "../../common/user-store/user-store.injectable";
 import type { UserStore } from "../../common/user-store";
-import themeStoreInjectable from "../../renderer/themes/store.injectable";
-import type { ThemeStore } from "../../renderer/themes/store";
 import { observable } from "mobx";
+import ipcRendererInjectable from "../../renderer/app-paths/get-value-from-registered-channel/ipc-renderer/ipc-renderer.injectable";
 
 describe("preferences - navigation to kubernetes preferences", () => {
   let applicationBuilder: ApplicationBuilder;
@@ -24,10 +23,10 @@ describe("preferences - navigation to kubernetes preferences", () => {
       } as unknown as UserStore;
 
       rendererDi.override(userStoreInjectable, () => userStoreStub);
-
-      const themeStoreStub = ({ themeOptions: [] }) as unknown as ThemeStore;
-
-      rendererDi.override(themeStoreInjectable, () => themeStoreStub);
+      rendererDi.override(ipcRendererInjectable, () => ({
+        on: jest.fn(),
+        invoke: jest.fn(), // TODO: replace with proper mocking via the IPC bridge
+      } as never));
     });
   });
 
