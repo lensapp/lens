2e6d54e3f9d9e86468569ac50ab1412c22ccdc6d
Janne Savolainen <janne.savolainen@live.fi>
2022-04-27 11:17:43 +0300

    Reimplement setup for system certifications using runnables instead of non-OCP in index.ts
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ src/main/start-main-application/before-application-is-ready/implementations/setup-system-ca.injectable.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { injectSystemCAs } from "../../../../common/system-ca";
+import { getInjectable } from "@ogre-tools/injectable";
+import { beforeApplicationIsReadyInjectionToken } from "../before-application-is-ready-injection-token";
+
+const setupSystemCaInjectable = getInjectable({
+  id: "setup-system-ca",
+
+  instantiate: () => ({
+    run: async () => {
+      await injectSystemCAs();
+    },
+  }),
+
+  injectionToken: beforeApplicationIsReadyInjectionToken,
+});
+
+export default setupSystemCaInjectable;
