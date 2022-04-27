74f8d7462e813da66b43ea40bdb4cde0b4c90efc
Janne Savolainen <janne.savolainen@live.fi>
2022-04-27 11:14:43 +0300

    Reimplement setup for electron application name using runnables instead of non-OCP in index.ts
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/app-paths/app-name/app-name.injectable.ts
+++ src/main/app-paths/app-name/app-name.injectable.ts
@@ -3,11 +3,13 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import electronAppInjectable from "../get-electron-app-path/electron-app/electron-app.injectable";
+import { isDevelopment } from "../../../common/vars";
+import packageInfo from "../../../../package.json";
 
 const appNameInjectable = getInjectable({
   id: "app-name",
-  instantiate: (di) => di.inject(electronAppInjectable).name,
+  instantiate: () => `${packageInfo.productName}${isDevelopment ? "Dev" : ""}`,
+  causesSideEffects: true,
 });
 
 export default appNameInjectable;
---
+++ src/main/start-main-application/before-application-is-ready/implementations/setup-application-name.injectable.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import electronAppInjectable from "../../../app-paths/get-electron-app-path/electron-app/electron-app.injectable";
+import appNameInjectable from "../../../app-paths/app-name/app-name.injectable";
+import { beforeApplicationIsReadyInjectionToken } from "../before-application-is-ready-injection-token";
+
+const setupApplicationNameInjectable = getInjectable({
+  id: "setup-application-name",
+
+  instantiate: (di) => ({
+    run: () => {
+      const app = di.inject(electronAppInjectable);
+      const appName = di.inject(appNameInjectable);
+
+      app.setName(appName);
+    },
+  }),
+
+  injectionToken: beforeApplicationIsReadyInjectionToken,
+});
+
+export default setupApplicationNameInjectable;
