ff119dabf722c31667c613c4d26aac1c22c9a65e
Jim Ehrismann <40840436+jim-docker@users.noreply.github.com>
2022-04-27 11:55:23 -0400

    adding https_proxy to execHelm env (#5273)
    * adding https_proxy to execHelm env
    
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>
    
    * lint plus simplification
    
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- src/main/helm/exec.ts
+++ src/main/helm/exec.ts
@@ -7,6 +7,7 @@ import { promiseExecFile } from "../../common/utils/promise-exec";
 import type { BaseEncodingOptions } from "fs";
 import type { ExecFileOptions } from "child_process";
 import { helmBinaryPath } from "../../common/vars";
+import { UserStore } from "../../common/user-store";
 
 /**
  * ExecFile the bundled helm CLI
@@ -14,7 +15,15 @@ import { helmBinaryPath } from "../../common/vars";
  */
 export async function execHelm(args: string[], options?: BaseEncodingOptions & ExecFileOptions): Promise<string> {
   try {
-    const { stdout } = await promiseExecFile(helmBinaryPath.get(), args, options);
+    const opts = { ...options };
+
+    opts.env ??= process.env;
+
+    if (!opts.env.HTTPS_PROXY && UserStore.getInstance().httpsProxy) {
+      opts.env.HTTPS_PROXY = UserStore.getInstance().httpsProxy;
+    }
+
+    const { stdout } = await promiseExecFile(helmBinaryPath.get(), args, opts);
 
     return stdout;
   } catch (error) {
