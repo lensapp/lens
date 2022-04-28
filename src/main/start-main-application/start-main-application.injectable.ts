25a48f3d7d47f757ee73aee39a88e0cb17dd6a18
Janne Savolainen <janne.savolainen@live.fi>
2022-04-28 12:42:25 +0300

    Make runnables for before application is ready synchronous as this is technical limitation of Electron
    See: https://github.com/electron/electron/issues/21370
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/start-main-application/start-main-application.injectable.ts
+++ src/main/start-main-application/start-main-application.injectable.ts
@@ -8,16 +8,20 @@ import electronAppInjectable from "../electron-app/electron-app.injectable";
 import { beforeApplicationIsReadyInjectionToken } from "./before-application-is-ready/before-application-is-ready-injection-token";
 import { afterApplicationIsReadyInjectionToken } from "./after-application-is-ready/after-application-is-ready-injection-token";
 import { runManyFor } from "./run-many-for";
+import { runManySyncFor } from "./run-many-sync-for";
 
 const startMainApplicationInjectable = getInjectable({
   id: "start-main-application",
 
   instantiate: (di) => {
     const runMany = runManyFor(di);
+    const runManySync = runManySyncFor(di);
     const app = di.inject(electronAppInjectable);
 
     return async () => {
-      await runMany(beforeApplicationIsReadyInjectionToken)();
+      // Stuff happening before application is ready needs to be synchronous because of
+      // https://github.com/electron/electron/issues/21370
+      runManySync(beforeApplicationIsReadyInjectionToken)();
 
       await app.whenReady();
 
