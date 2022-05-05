d18b84743dc7671978503033e491bec21f0aaa87
Sebastian Malton <sebastian@malton.name>
2022-05-05 08:45:54 -0400

    remove duplicate license header
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/utils/wait.ts
+++ src/common/utils/wait.ts
@@ -6,10 +6,6 @@ import type { IComputedValue } from "mobx";
 import { runInAction, when } from "mobx";
 import type { Disposer } from "./disposer";
 
-/**
- * Copyright (c) OpenLens Authors. All rights reserved.
- * Licensed under MIT License. See LICENSE in root directory for more information.
- */
 export async function waitUntilDefinied<T>(getter: (() => T | null | undefined) | IComputedValue<T | null | undefined>, opts?: { timeout?: number }): Promise<T> {
   return new Promise<T>((resolve, reject) => {
     let res: T | null | undefined;
