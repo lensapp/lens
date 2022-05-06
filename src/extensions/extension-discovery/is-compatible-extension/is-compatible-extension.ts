8f356b86352e582442117a7934a9419d11cfc6ca
Roman <ixrock@gmail.com>
2022-05-06 14:33:12 +0300

    fix: versions matching check (app <-> extensions)
    Signed-off-by: Roman <ixrock@gmail.com>

--- src/extensions/extension-discovery/is-compatible-extension/is-compatible-extension.ts
+++ src/extensions/extension-discovery/is-compatible-extension/is-compatible-extension.ts
@@ -2,21 +2,21 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import semver, { SemVer } from "semver";
+import semver, { type SemVer } from "semver";
 import type { LensExtensionManifest } from "../../lens-extension";
 
 interface Dependencies {
   appSemVer: SemVer;
 }
 
-export const isCompatibleExtension = (deps: Dependencies): ((manifest: LensExtensionManifest) => boolean) => {
-  const { major: appMajor, minor: appMinor } = deps.appSemVer;
-
+export const isCompatibleExtension = ({ appSemVer }: Dependencies): ((manifest: LensExtensionManifest) => boolean) => {
   return (manifest: LensExtensionManifest): boolean => {
-    const { major: extMajor, minor: extMinor } = new SemVer(manifest.engines.lens, {
-      loose: true,
-    });
+    const { raw: appVersion } = appSemVer;
+    const supportedVersionsByExtension: string = semver.validRange(manifest.engines.lens);
 
-    return semver.intersects(`>=${appMajor}.${appMinor}`, `<${extMajor}.${extMinor + 1}`);
+    return semver.satisfies(appVersion, supportedVersionsByExtension, {
+      loose: true,
+      includePrerelease: false,
+    });
   };
 };
