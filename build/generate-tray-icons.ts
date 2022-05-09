f7a567f07c26b831751987cd53563fa409940f88
Sebastian Malton <sebastian@malton.name>
2022-05-09 09:55:03 -0400

    generate more pngs
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- build/generate-tray-icons.ts
+++ build/generate-tray-icons.ts
@@ -17,18 +17,31 @@ const svgData = readFileSync(inputFile, { encoding: "utf-8" });
 const svgDom = new JSDOM(`<body>${svgData}</body>`);
 const svgRoot = svgDom.window.document.body.getElementsByTagName("svg")[0];
 
+svgRoot.innerHTML += `<style>* {fill: white !important;}</style>`;
+const lightTemplate = svgRoot.outerHTML;
+
 svgRoot.innerHTML += `<style>* {fill: black !important;}</style>`;
+
+const darkTemplate = svgRoot.outerHTML;
 
 console.log("Generating tray icon pngs");
 
 ensureDirSync(outputFolder);
 
 Promise.allSettled([
-  sharp(Buffer.from(svgRoot.outerHTML))
+  sharp(Buffer.from(darkTemplate))
+    .resize({ width: size, height: size })
+    .png()
+    .toFile(path.join(outputFolder, "trayIconDarkTemplate.png")),
+  sharp(Buffer.from(darkTemplate))
+    .resize({ width: size*2, height: size*2 })
+    .png()
+    .toFile(path.join(outputFolder, "trayIconDarkTemplate@2x.png")),
+  sharp(Buffer.from(lightTemplate))
     .resize({ width: size, height: size })
     .png()
     .toFile(path.join(outputFolder, "trayIconTemplate.png")),
-  sharp(Buffer.from(svgRoot.outerHTML))
+  sharp(Buffer.from(lightTemplate))
     .resize({ width: size*2, height: size*2 })
     .png()
     .toFile(path.join(outputFolder, "trayIconTemplate@2x.png")),
