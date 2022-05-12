3b4f8779b753d8b0026fb2f7e1ac148153ee9ade
Alex Andreev <alex.andreev.email@gmail.com>
2022-05-12 13:48:37 +0300

    Default button styles
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/update-button/update-button.tsx
+++ src/renderer/components/update-button/update-button.tsx
@@ -6,7 +6,6 @@
 import styles from "./styles.module.scss";
 
 import React, { useState } from "react";
-import { Icon } from "../icon";
 import { Menu, MenuItem } from "../menu";
 import uniqueId from "lodash/uniqueId";
 import { noop } from "lodash";
@@ -48,7 +47,11 @@ export function UpdateButton({ warningLevel, update }: UpdateButtonProps) {
         onKeyDown={onKeyDown}
       >
         Update
-        <Icon material="update"/>
+        <svg width="12" height="12" viewBox="0 0 12 12" shapeRendering="crispEdges">
+          <path fill="currentColor" d="M0,8.5h12v1H0V8.5z"/>
+          <path fill="currentColor" d="M0,5.5h12v1H0V5.5z"/>
+          <path fill="currentColor" d="M0,2.5h12v1H0V2.5z"/>
+        </svg>
       </button>
       <Menu
         usePortal
