03f3d0933a240b9d32e6104091b3488d75fa8167
Janne Savolainen <janne.savolainen@live.fi>
2022-05-16 13:55:25 +0300

    Introduce abstraction for a state that is shared between environments
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ src/common/sync-box/create-sync-box.injectable.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import type { IComputedValue } from "mobx";
+import { computed } from "mobx";
+import syncBoxChannelInjectable from "./sync-box-channel.injectable";
+import { sendToAgnosticChannelInjectionToken } from "./channel/send-to-agnostic-channel-injection-token";
+import syncBoxStateInjectable from "./sync-box-state.injectable";
+
+const createSyncBoxInjectable = getInjectable({
+  id: "create-sync-box",
+
+  instantiate: (di) => {
+    const syncBoxChannel = di.inject(syncBoxChannelInjectable);
+    const sendToAgnosticChannel = di.inject(sendToAgnosticChannelInjectionToken);
+    const getSyncBoxState = (id: string) => di.inject(syncBoxStateInjectable, id);
+
+    return (id: string): SyncBox<any> => {
+      const state = getSyncBoxState(id);
+
+      return {
+        id,
+
+        value: computed(() => state.get()),
+
+        set: (value) => {
+          state.set(value);
+
+          sendToAgnosticChannel(syncBoxChannel, { id, value });
+        },
+      };
+    };
+  },
+});
+
+export default createSyncBoxInjectable;
+
+
+export interface SyncBox<TValue> {
+  id: string;
+  value: IComputedValue<TValue>;
+  set: (value: TValue) => void;
+}
---
+++ src/common/sync-box/sync-box-channel-listener.injectable.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import syncBoxChannelInjectable from "./sync-box-channel.injectable";
+import { channelListenerInjectionToken } from "./channel/channel-listener-injection-token";
+import syncBoxStateInjectable from "./sync-box-state.injectable";
+
+const syncBoxChannelListenerInjectable = getInjectable({
+  id: "sync-box-channel-listener",
+
+  instantiate: (di) => {
+    const getSyncBoxState = (id: string) => di.inject(syncBoxStateInjectable, id);
+
+    return {
+      channel: di.inject(syncBoxChannelInjectable),
+
+      handler: ({ id, value }) => {
+        const target = getSyncBoxState(id);
+
+        if (target) {
+          target.set(value);
+        }
+      },
+    };
+  },
+
+  injectionToken: channelListenerInjectionToken,
+});
+
+export default syncBoxChannelListenerInjectable;
