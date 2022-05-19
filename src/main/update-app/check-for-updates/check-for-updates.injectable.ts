8af19d8c4b0d74494ece9d865a9a2ab38541085c
Janne Savolainen <janne.savolainen@live.fi>
2022-05-19 13:25:50 +0300

    Reorganize responsibilities for checking updates
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/update-app/check-for-updates/check-for-updates.injectable.ts
+++ src/main/update-app/check-for-updates/check-for-updates.injectable.ts
@@ -3,103 +3,91 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import type { CheckForPlatformUpdates } from "../check-for-platform-updates/check-for-platform-updates.injectable";
-import checkForPlatformUpdatesInjectable from "../check-for-platform-updates/check-for-platform-updates.injectable";
-import type { UpdateChannel } from "../update-channels";
 import selectedUpdateChannelInjectable from "../selected-update-channel.injectable";
 import updatesAreBeingDiscoveredInjectable from "../../../common/application-update/updates-are-being-discovered/updates-are-being-discovered.injectable";
 import discoveredUpdateVersionInjectable from "../../../common/application-update/discovered-update-version/discovered-update-version.injectable";
 import { runInAction } from "mobx";
 import assert from "assert";
-import applicationUpdateStatusChannelInjectable from "../../../common/application-update/application-update-status-channel.injectable";
-import { sendToAgnosticChannelInjectionToken } from "../../../common/channel/send-to-agnostic-channel-injection-token";
+import askBooleanInjectable from "../../ask-boolean/ask-boolean.injectable";
+import quitAndInstallUpdateInjectable from "../../electron-app/features/quit-and-install-update.injectable";
+import downloadUpdateInjectable from "../download-update/download-update.injectable";
+import broadcastChangeInUpdatingStatusInjectable from "./broadcast-change-in-updating-status.injectable";
+import checkForUpdatesStartingFromChannelInjectable from "./check-for-updates-starting-from-channel.injectable";
 
 const checkForUpdatesInjectable = getInjectable({
   id: "check-for-updates",
 
   instantiate: (di) => {
+    const askBoolean = di.inject(askBooleanInjectable);
+    const quitAndInstallUpdate = di.inject(quitAndInstallUpdateInjectable);
+    const downloadUpdate = di.inject(downloadUpdateInjectable);
     const selectedUpdateChannel = di.inject(selectedUpdateChannelInjectable);
-    const sendToAgnosticChannel = di.inject(sendToAgnosticChannelInjectionToken);
-    const applicationUpdateStatusChannel = di.inject(applicationUpdateStatusChannelInjectable);
-
-    const checkForPlatformUpdates = di.inject(
-      checkForPlatformUpdatesInjectable,
-    );
-
-    const checkingForUpdatesState = di.inject(
-      updatesAreBeingDiscoveredInjectable,
-    );
-
+    const broadcastChangeInUpdatingStatus = di.inject(broadcastChangeInUpdatingStatusInjectable);
+    const checkingForUpdatesState = di.inject(updatesAreBeingDiscoveredInjectable);
     const discoveredVersionState = di.inject(discoveredUpdateVersionInjectable);
+    const checkForUpdatesStartingFromChannel = di.inject(checkForUpdatesStartingFromChannelInjectable);
 
     return async () => {
+      broadcastChangeInUpdatingStatus({ eventId: "checking-for-updates" });
+
       runInAction(() => {
         checkingForUpdatesState.set(true);
       });
-
-      const checkForUpdatesStartingFromChannel =
-        checkForUpdatesStartingFromChannelFor(checkForPlatformUpdates);
-
-      sendToAgnosticChannel(applicationUpdateStatusChannel, { eventId: "checking-for-updates" });
 
       const { updateWasDiscovered, version, actualUpdateChannel } =
         await checkForUpdatesStartingFromChannel(selectedUpdateChannel.value.get());
 
       if (!updateWasDiscovered) {
-        sendToAgnosticChannel(applicationUpdateStatusChannel, { eventId: "no-updates-available" });
+        broadcastChangeInUpdatingStatus({ eventId: "no-updates-available" });
+
+        runInAction(() => {
+          discoveredVersionState.set(null);
+          checkingForUpdatesState.set(false);
+        });
+
+        return;
       }
 
+      broadcastChangeInUpdatingStatus({
+        eventId: "download-for-update-started",
+        version,
+      });
+
       runInAction(() => {
-        if (!updateWasDiscovered) {
-          discoveredVersionState.set(null);
-        } else {
+        // TODO: Unacceptable damage caused by strict mode
+        assert(version);
+        assert(actualUpdateChannel);
 
-          // TODO: Unacceptable damage caused by strict mode
-          assert(version);
-          assert(actualUpdateChannel);
-
-          discoveredVersionState.set({
-            version,
-            updateChannel: actualUpdateChannel,
-          });
-        }
+        discoveredVersionState.set({
+          version,
+          updateChannel: actualUpdateChannel,
+        });
 
         checkingForUpdatesState.set(false);
       });
 
-      return { updateWasDiscovered, version };
+      // Note: intentional orphan promise to make download happen in the background
+      downloadUpdate().then(async ({ downloadWasSuccessful }) => {
+        if (!downloadWasSuccessful) {
+          broadcastChangeInUpdatingStatus({
+            eventId: "download-for-update-failed",
+          });
+
+          return;
+        }
+
+        const userWantsToInstallUpdate = await askBoolean({
+          id: "install-update",
+          title: "Update Available",
+          question: `Version ${version} of Lens IDE is available and ready to be installed. Would you like to update now?`,
+        });
+
+        if (userWantsToInstallUpdate) {
+          quitAndInstallUpdate();
+        }
+      });
     };
   },
 });
 
 export default checkForUpdatesInjectable;
-
-const checkForUpdatesStartingFromChannelFor = (
-  checkForPlatformUpdates: CheckForPlatformUpdates,
-) => {
-  const _recursiveCheck = async (
-    updateChannel: UpdateChannel,
-  ): Promise<{
-    updateWasDiscovered: boolean;
-    version?: string;
-    actualUpdateChannel?: UpdateChannel;
-  }> => {
-    const result = await checkForPlatformUpdates(updateChannel);
-
-    if (result.updateWasDiscovered) {
-      return {
-        updateWasDiscovered: true,
-        version: result.version,
-        actualUpdateChannel: updateChannel,
-      };
-    }
-
-    if (updateChannel.moreStableUpdateChannel) {
-      return await _recursiveCheck(updateChannel.moreStableUpdateChannel);
-    }
-
-    return { updateWasDiscovered: false };
-  };
-
-  return _recursiveCheck;
-};
