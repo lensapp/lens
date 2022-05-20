eb8d316eeaa5286bc146b7d83ae2d9517f53839b
Janne Savolainen <janne.savolainen@live.fi>
2022-05-20 09:56:11 +0300

    Switch to using release channel of installed application version as default value for selected update channel
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/common/application-update/selected-update-channel/selected-update-channel.injectable.ts
+++ src/common/application-update/selected-update-channel/selected-update-channel.injectable.ts
@@ -7,40 +7,28 @@ import type { IComputedValue } from "mobx";
 import { action, computed, observable } from "mobx";
 import type { UpdateChannel, UpdateChannelId } from "../update-channels";
 import { updateChannels } from "../update-channels";
+import defaultUpdateChannelInjectable from "./default-update-channel.injectable";
 
 export interface SelectedUpdateChannel {
   value: IComputedValue<UpdateChannel>;
   setValue: (channelId?: UpdateChannelId) => void;
 }
 
-// export const defaultUpdateChannel = new SemVer(getAppVersion()).prerelease[0]?.toString() || "latest";
-
-// const updateChannel: PreferenceDescription<string> = {
-//   fromStore(val) {
-//     return !val || !updateChannels.has(val)
-//       ? defaultUpdateChannel
-//       : val;
-//   },
-//   toStore(val) {
-//     if (!updateChannels.has(val) || val === defaultUpdateChannel) {
-//       return undefined;
-//     }
-//
-//     return val;
-//   },
-// };
-
 const selectedUpdateChannelInjectable = getInjectable({
   id: "selected-update-channel",
 
-  instantiate: (): SelectedUpdateChannel => {
-    const state = observable.box(updateChannels.latest);
+  instantiate: (di): SelectedUpdateChannel => {
+    const defaultUpdateChannel = di.inject(defaultUpdateChannelInjectable);
+    const state = observable.box(defaultUpdateChannel);
 
     return {
       value: computed(() => state.get()),
 
       setValue: action((channelId) => {
-        const targetUpdateChannel = channelId ? updateChannels[channelId] : updateChannels.latest;
+        const targetUpdateChannel =
+          channelId && updateChannels[channelId]
+            ? updateChannels[channelId]
+            : defaultUpdateChannel;
 
         state.set(targetUpdateChannel);
       }),
