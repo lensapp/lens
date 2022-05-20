9110f9b114b824525aaaa46da29ab4c9f053130b
Sebastian Malton <sebastian@malton.name>
2022-05-20 15:21:27 -0400

    Introduce more complete typings for Containers
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ src/common/k8s-api/endpoints/types/env-var-key-selector.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+export interface EnvVarKeySelector {
+  key: string;
+  name?: string;
+  optional?: boolean;
+}
