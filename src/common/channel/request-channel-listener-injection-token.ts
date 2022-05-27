1e80cf0b12ef32b420d01ab07f8f347618c23ec0
Janne Savolainen <janne.savolainen@live.fi>
2022-05-27 14:56:33 +0300

    Tweak typing of request channel listeners to get rid of unexpected undefined
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/common/channel/request-channel-listener-injection-token.ts
+++ src/common/channel/request-channel-listener-injection-token.ts
@@ -3,11 +3,19 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectionToken } from "@ogre-tools/injectable";
+import type { SetRequired } from "type-fest";
 import type { RequestChannel } from "./request-channel-injection-token";
 
 export interface RequestChannelListener<TChannel extends RequestChannel<any, any>> {
   channel: TChannel;
-  handler: (request: TChannel["_requestSignature"]) => TChannel["_responseSignature"];
+
+  handler: (
+    request: SetRequired<TChannel, "_requestSignature">["_requestSignature"]
+  ) =>
+    | SetRequired<TChannel, "_responseSignature">["_responseSignature"]
+    | Promise<
+        SetRequired<TChannel, "_responseSignature">["_responseSignature"]
+      >;
 }
 
 export const requestChannelListenerInjectionToken = getInjectionToken<RequestChannelListener<RequestChannel<any, any>>>(
