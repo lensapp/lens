6103f2a9ef3bbc5ab596d310649e2c654898b0b0
Janne Savolainen <janne.savolainen@live.fi>
2022-05-27 14:56:33 +0300

    Tweak typing of request channel listeners to get rid of unexpected undefined
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/main/app-paths/app-paths-request-channel-listener.injectable.ts
+++ src/main/app-paths/app-paths-request-channel-listener.injectable.ts
@@ -3,21 +3,23 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
+import type { RequestChannelListener } from "../../common/channel/request-channel-listener-injection-token";
 import { requestChannelListenerInjectionToken } from "../../common/channel/request-channel-listener-injection-token";
+import type { AppPathsChannel } from "../../common/app-paths/app-paths-channel.injectable";
 import appPathsChannelInjectable from "../../common/app-paths/app-paths-channel.injectable";
 import appPathsInjectable from "../../common/app-paths/app-paths.injectable";
 
 const appPathsRequestChannelListenerInjectable = getInjectable({
   id: "app-paths-request-channel-listener",
 
-  instantiate: (di) => {
+  instantiate: (di): RequestChannelListener<AppPathsChannel> => {
     const channel = di.inject(appPathsChannelInjectable);
     const appPaths = di.inject(appPathsInjectable);
 
-    return ({
+    return {
       channel,
       handler: () => appPaths,
-    });
+    };
   },
   injectionToken: requestChannelListenerInjectionToken,
 });
