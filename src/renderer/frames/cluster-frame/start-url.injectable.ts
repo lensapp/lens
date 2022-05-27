8ef7030306dc3fdaaef9da01da46e6e29cfeade4
Jim Ehrismann <jehrismann@mirantis.com>
2022-05-27 16:57:05 -0400

    ensure the start url is the workloads overview when the cluster overview is not available due to rbac restrictions
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- src/renderer/frames/cluster-frame/start-url.injectable.ts
+++ src/renderer/frames/cluster-frame/start-url.injectable.ts
@@ -4,8 +4,6 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import { computed } from "mobx";
-import type { KubeResource } from "../../../common/rbac";
-import isAllowedResourceInjectable from "../../../common/utils/is-allowed-resource.injectable";
 import clusterOverviewRouteInjectable from "../../../common/front-end-routing/routes/cluster/overview/cluster-overview-route.injectable";
 import workloadsOverviewRouteInjectable from "../../../common/front-end-routing/routes/cluster/workloads/overview/workloads-overview-route.injectable";
 import { buildURL } from "../../../common/utils/buildUrl";
@@ -14,17 +12,13 @@ const startUrlInjectable = getInjectable({
   id: "start-url",
 
   instantiate: (di) => {
-    const isAllowedResource = (resourceName: any) => di.inject(isAllowedResourceInjectable, resourceName);
-
     const clusterOverviewRoute = di.inject(clusterOverviewRouteInjectable);
     const workloadOverviewRoute = di.inject(workloadsOverviewRouteInjectable);
     const clusterOverviewUrl = buildURL(clusterOverviewRoute.path);
     const workloadOverviewUrl = buildURL(workloadOverviewRoute.path);
 
     return computed(() => {
-      const resources: KubeResource[] = ["events", "nodes", "pods"];
-
-      return resources.every((resourceName) => isAllowedResource(resourceName))
+      return clusterOverviewRoute.isEnabled.get()
         ? clusterOverviewUrl
         : workloadOverviewUrl;
     });
