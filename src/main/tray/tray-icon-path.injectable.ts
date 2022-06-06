9f3140db773a9185dc65eb5bfe9b127003de5ff0
Sebastian Malton <sebastian@malton.name>
2022-06-06 17:46:55 -0400

    Use alternative icons on non macos
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/tray/tray-icon-path.injectable.ts
+++ src/main/tray/tray-icon-path.injectable.ts
@@ -6,6 +6,7 @@ import { getInjectable } from "@ogre-tools/injectable";
 import getAbsolutePathInjectable from "../../common/path/get-absolute-path.injectable";
 import staticFilesDirectoryInjectable from "../../common/vars/static-files-directory.injectable";
 import isDevelopmentInjectable from "../../common/vars/is-development.injectable";
+import isMacInjectable from "../../common/vars/is-mac.injectable";
 
 const trayIconPathsInjectable = getInjectable({
   id: "tray-icon-paths",
@@ -14,14 +15,16 @@ const trayIconPathsInjectable = getInjectable({
     const getAbsolutePath = di.inject(getAbsolutePathInjectable);
     const staticFilesDirectory = di.inject(staticFilesDirectoryInjectable);
     const isDevelopment = di.inject(isDevelopmentInjectable);
+    const isMac = di.inject(isMacInjectable);
     const baseIconDirectory = getAbsolutePath(
       staticFilesDirectory,
       isDevelopment ? "../build/tray" : "icons", // copied within electron-builder extras
     );
+    const fileSuffix = isMac ? "Template.png" : ".png";
 
     return {
-      normal: getAbsolutePath(baseIconDirectory, "trayIconTemplate.png"),
-      updateAvailable: getAbsolutePath(baseIconDirectory, "trayIconUpdateAvailableTemplate.png"),
+      normal: getAbsolutePath(baseIconDirectory, `trayIcon${fileSuffix}`),
+      updateAvailable: getAbsolutePath(baseIconDirectory, `trayIconUpdateAvailable${fileSuffix}`),
     };
   },
 });
