cab1793eb57075c43bfe830801a729612c9c7210
Janne Savolainen <janne.savolainen@live.fi>
2022-06-07 08:33:58 +0300

    Introduce way to get single active helm repository
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ src/main/helm/repositories/get-active-helm-repository.injectable.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import getActiveHelmRepositoriesInjectable from "./get-active-helm-repositories/get-active-helm-repositories.injectable";
+
+const getActiveHelmRepositoryInjectable = getInjectable({
+  id: "get-active-helm-repository",
+
+  instantiate: (di) => {
+    const getActiveHelmRepositories = di.inject(getActiveHelmRepositoriesInjectable);
+
+    return async (name: string) => {
+      const activeHelmRepositories = await getActiveHelmRepositories();
+
+      return activeHelmRepositories.find(
+        (repository) => repository.name === name,
+      );
+    };
+  },
+});
+
+export default getActiveHelmRepositoryInjectable;
