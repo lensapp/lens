321913ee5708ee4f4c9badca898c7e1b8eb26faa
Sebastian Malton <sebastian@malton.name>
2022-06-22 15:41:39 -0400

    Fix tests
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/utils/resolve-system-proxy/resolve-system-proxy-channel-responder.injectable.ts
+++ src/main/utils/resolve-system-proxy/resolve-system-proxy-channel-responder.injectable.ts
@@ -5,7 +5,7 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import resolveSystemProxyChannelInjectable from "../../../common/utils/resolve-system-proxy/resolve-system-proxy-channel.injectable";
 import resolveSystemProxyInjectable from "./resolve-system-proxy.injectable";
-import { requestChannelListenerInjectionToken } from "../../../common/utils/channel/request-channel-listener-injection-token";
+import { requestChannelHandlerInjectionToken } from "../../../common/utils/channel/request-channel-listener-injection-token";
 
 const resolveSystemProxyChannelResponderInjectable = getInjectable({
   id: "resolve-system-proxy-channel-responder",
@@ -15,7 +15,7 @@ const resolveSystemProxyChannelResponderInjectable = getInjectable({
     handler: di.inject(resolveSystemProxyInjectable),
   }),
 
-  injectionToken: requestChannelListenerInjectionToken,
+  injectionToken: requestChannelHandlerInjectionToken,
 });
 
 export default resolveSystemProxyChannelResponderInjectable;
