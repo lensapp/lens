e98b4fbadfe8d74dc92f5ea90002486d54ae01e1
Iku-turso <mikko.aspiala@gmail.com>
2022-06-22 14:55:50 +0300

    Add tsdoc for "resolveSystemProxy" exposed in Extension-API
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- src/extensions/common-api/proxy.ts
+++ src/extensions/common-api/proxy.ts
@@ -6,4 +6,9 @@
 import { asLegacyGlobalFunctionForExtensionApi } from "../as-legacy-globals-for-extension-api/as-legacy-global-function-for-extension-api";
 import { resolveSystemProxyInjectionToken } from "../../common/utils/resolve-system-proxy/resolve-system-proxy-injection-token";
 
+/**
+ * Resolves URL-specific proxy information from system. See more here: https://www.electronjs.org/docs/latest/api/session#sesresolveproxyurl
+ * @param url - The URL for proxy information
+ * @returns Promise for proxy information as string
+ */
 export const resolveSystemProxy = asLegacyGlobalFunctionForExtensionApi(resolveSystemProxyInjectionToken);
