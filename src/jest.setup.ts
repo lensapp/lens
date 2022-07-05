c843d1b7bda6d29a56456f82b6f5517b47631ef6
Sebastian Malton <sebastian@malton.name>
2022-07-05 14:52:00 -0400

    Fix error with jose package upgrade
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -7,6 +7,7 @@ import fetchMock from "jest-fetch-mock";
 import configurePackages from "./common/configure-packages";
 import { configure } from "mobx";
 import { setImmediate } from "timers";
+import { TextEncoder, TextDecoder as TextDecoderNode } from "util";
 
 // setup default configuration for external npm-packages
 configurePackages();
@@ -32,3 +33,6 @@ global.fail = ((error = "Test failed without explicit error") => {
 process.on("unhandledRejection", (err: any) => {
   global.fail(err);
 });
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoderNode as unknown as typeof TextDecoder;
