ebddd8953bc429d9c398edd61a778a6a2f582db2
Sebastian Malton <sebastian@malton.name>
2022-07-05 11:16:52 -0400

    Add first keyframe for downloading spinner
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- build/generate-tray-icons.ts
+++ build/generate-tray-icons.ts
@@ -12,7 +12,7 @@ const size = Number(process.env.OUTPUT_SIZE || "16");
 const outputFolder = process.env.OUTPUT_DIR || "./build/tray";
 const inputFile = process.env.INPUT_SVG_PATH || "./src/renderer/components/icon/logo-lens.svg";
 const noticeFile = process.env.NOTICE_SVG_PATH || "./src/renderer/components/icon/notice.svg";
-const spinnerFile = process.env.SPINNER_SVG_PATH || "./src/renderer/components/icon/ssh.svg";
+const spinnerFile = process.env.SPINNER_SVG_PATH || "./src/renderer/components/icon/arrow-spinner.svg";
 
 async function ensureOutputFoler() {
   await ensureDir(outputFolder);
