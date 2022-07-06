16b87062a78d3cb49b896a02e3ce37bcacf35573
Janne Savolainen <janne.savolainen@live.fi>
2022-07-06 09:00:46 +0300

    Make enabling and disabling extensions in behavioural unit tests more realistic by enabling both main and renderer at same time
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/components/test-utils/get-renderer-extension-fake.ts
+++ src/renderer/components/test-utils/get-renderer-extension-fake.ts
@@ -2,16 +2,10 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import type { Mutable, SetRequired } from "type-fest";
-import catalogCategoryRegistryInjectable from "../../../common/catalog/category-registry.injectable";
-import fileSystemProvisionerStoreInjectable from "../../../extensions/extension-loader/file-system-provisioner-store/file-system-provisioner-store.injectable";
-import { lensExtensionDependencies } from "../../../extensions/lens-extension";
+import type { SetRequired } from "type-fest";
 import { LensRendererExtension } from "../../../extensions/lens-renderer-extension";
-import catalogEntityRegistryInjectable from "../../api/catalog/entity/registry.injectable";
-import getExtensionPageParametersInjectable from "../../routes/get-extension-page-parameters.injectable";
-import navigateToRouteInjectable from "../../routes/navigate-to-route.injectable";
-import routesInjectable from "../../routes/routes.injectable";
 import type { ApplicationBuilder } from "./get-application-builder";
+import { getExtensionFakeFor } from "./get-extension-fake";
 
 export class TestExtension extends LensRendererExtension {}
 
@@ -19,36 +13,16 @@ export type FakeExtensionData = SetRequired<Partial<LensRendererExtension>, "id"
 
 export type GetRendererExtensionFake = (fakeExtensionData: FakeExtensionData) => TestExtension;
 
-export const getRendererExtensionFakeFor = (builder: ApplicationBuilder): GetRendererExtensionFake => (
-  function getRendererExtensionFake({ id, name, ...rest }) {
-    const instance = new TestExtension({
+export const getRendererExtensionFakeFor = (
+  builder: ApplicationBuilder,
+): GetRendererExtensionFake => {
+  const getExtensionFake = getExtensionFakeFor(builder);
+
+  return ({ id, name, ...rest }) =>
+    getExtensionFake({
       id,
-      absolutePath: "irrelevant",
-      isBundled: false,
-      isCompatible: false,
-      isEnabled: false,
-      manifest: {
-        name,
-        version: "1.0.0",
-        engines: {
-          lens: "^5.5.0",
-        },
-      },
-      manifestPath: "irrelevant",
-    });
-
-    Object.assign(instance, rest);
-
-    (instance as Mutable<LensRendererExtension>)[lensExtensionDependencies] = {
-      categoryRegistry: builder.dis.rendererDi.inject(catalogCategoryRegistryInjectable),
-      entityRegistry: builder.dis.rendererDi.inject(catalogEntityRegistryInjectable),
-      fileSystemProvisionerStore: builder.dis.rendererDi.inject(fileSystemProvisionerStoreInjectable),
-      getExtensionPageParameters: builder.dis.rendererDi.inject(getExtensionPageParametersInjectable),
-      navigateToRoute: builder.dis.rendererDi.inject(navigateToRouteInjectable),
-      routes: builder.dis.rendererDi.inject(routesInjectable),
-    };
-
-    return instance;
-  }
-);
+      name,
+      rendererOptions: rest,
+    }).renderer;
+};
 
