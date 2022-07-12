3ec61133a38a08a852f795ff2285ace9fa392d4a
Janne Savolainen <janne.savolainen@live.fi>
2022-07-12 21:27:39 +0300

    Introduce kludge to make "isEnabledForCluster" work again for cluster page menus  PART 2 (#5801)
    * Kludge "isEnabledForCluster" work again for cluster page menus (sidebar items)
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Introduce kludge to make "isEnabledForCluster" work again for kube object status texts PART 3 (#5802)
    
    * Kludge "isEnabledForCluster" work again for kube object status texts
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Kludge "isEnabledForCluster" work again for kube object menu items (#5803)
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ src/renderer/components/kube-object-status-icon/kube-object-status-text-injection-token.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectionToken } from "@ogre-tools/injectable";
+import type { KubeObject } from "../../../common/k8s-api/kube-object";
+import type { KubeObjectStatus } from "../../../common/k8s-api/kube-object-status";
+import type { IComputedValue } from "mobx";
+
+export interface KubeObjectStatusText {
+  kind: string;
+  apiVersions: string[];
+  resolve: (object: KubeObject) => KubeObjectStatus;
+  enabled: IComputedValue<boolean>;
+}
+
+export const kubeObjectStatusTextInjectionToken =
+  getInjectionToken<KubeObjectStatusText>({
+    id: "kube-object-status-text-injection-token",
+  });
