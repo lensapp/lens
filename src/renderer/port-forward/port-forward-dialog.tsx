eff26ab03d78c1305e7ddf6bb85544833c5561c3
Stefan Woehrer <stefan.woehrer@dedalus.eu>
2022-07-26 10:56:09 +0200

    Autofocus port field in portf orwarding dialog

--- src/renderer/port-forward/port-forward-dialog.tsx
+++ src/renderer/port-forward/port-forward-dialog.tsx
@@ -127,6 +127,7 @@ class NonInjectedPortForwardDialog extends Component<PortForwardDialogProps & De
                 value={this.desiredPort === 0 ? "" : String(this.desiredPort)}
                 placeholder={"Random"}
                 onChange={this.changePort}
+                autoFocus
               />
             </div>
             <Checkbox
