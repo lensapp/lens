cc2745f8c6ba1c5cffa4882016a403ebf43451fa
Janne Savolainen <janne.savolainen@live.fi>
2022-08-08 19:26:48 +0300

    Fix incorrect links in workload overview page (#6002)
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    (cherry picked from commit 688ef40e929d66facdd1b794b571d6f09f494171)

--- src/renderer/components/+workloads-overview/workloads/implementations/statefulsets-workload.injectable.ts
+++ src/renderer/components/+workloads-overview/workloads/implementations/statefulsets-workload.injectable.ts
@@ -5,16 +5,16 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import { workloadInjectionToken } from "../workload-injection-token";
 import { ResourceNames } from "../../../../utils/rbac";
-import navigateToPodsInjectable from "../../../../../common/front-end-routing/routes/cluster/workloads/deployments/navigate-to-deployments.injectable";
 import namespaceStoreInjectable from "../../../+namespaces/store.injectable";
 import statefulsetsStoreInjectable from "../../../+workloads-statefulsets/store.injectable";
 import { computed } from "mobx";
+import navigateToStatefulsetsInjectable from "../../../../../common/front-end-routing/routes/cluster/workloads/statefulsets/navigate-to-statefulsets.injectable";
 
 const statefulsetsWorkloadInjectable = getInjectable({
   id: "statefulsets-workload",
 
   instantiate: (di) => {
-    const navigate = di.inject(navigateToPodsInjectable);
+    const navigate = di.inject(navigateToStatefulsetsInjectable);
     const namespaceStore = di.inject(namespaceStoreInjectable);
     const store = di.inject(statefulsetsStoreInjectable);
 
