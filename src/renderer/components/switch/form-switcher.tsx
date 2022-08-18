8d9c4743964ef1a8af76a945437eabe58ba29794
Alex Andreev <alex.andreev.email@gmail.com>
2022-08-18 16:55:00 +0300

    Clean up material UI (except types) from FormSwitch
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/components/switch/form-switcher.tsx
+++ src/renderer/components/switch/form-switcher.tsx
@@ -5,28 +5,11 @@
 
 import React from "react";
 import type { FormControlLabelProps } from "@material-ui/core/FormControlLabel";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { makeStyles } from "@material-ui/styles";
-import { Switch } from "./switch";
-
-const useStyles = makeStyles({
-  root: {
-    margin: 0,
-    "& .MuiTypography-root": {
-      fontSize: 14,
-      fontWeight: 500,
-      flex: 1,
-      color: "var(--textColorAccent)",
-    },
-  },
-});
 
 /**
  * @deprecated Use <Switch/> instead from "../switch.tsx".
  */
 export function FormSwitch(props: FormControlLabelProps & { children?: React.ReactNode }) {
-  const classes = useStyles();
-
   const ClonedElement = React.cloneElement(props.control, {
     children: props.label,
   });
