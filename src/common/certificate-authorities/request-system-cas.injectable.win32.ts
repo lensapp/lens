2702ed7468e150d1e0bc20b9cca4099722b5b666
Sebastian Malton <sebastian@malton.name>
2022-09-08 08:07:16 -0400

    Fix win-ca failing on electron renderer on windows
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/certificate-authorities/request-system-cas.injectable.win32.ts
+++ src/common/certificate-authorities/request-system-cas.injectable.win32.ts
@@ -3,26 +3,40 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import wincaAPI from "win-ca/api";
+import execFileInjectable from "../fs/exec-file.injectable";
 import { requestSystemCAsInjectionToken } from "./request-system-cas-token";
+
+const pemEncoding = (hexEncodedCert: String) => {
+  const certData = Buffer.from(hexEncodedCert, "hex").toString('base64');
+  const lines = ['-----BEGIN CERTIFICATE-----'];
+
+  for (let i = 0; i < certData.length; i += 64) {
+    lines.push(certData.substring(i, i + 64));
+  }
+  
+  lines.push('-----END CERTIFICATE-----', '');
+  
+  return lines.join("\r\n");
+}
 
 const requestSystemCAsInjectable = getInjectable({
   id: "request-system-cas",
-  instantiate: () => {
-    return () => new Promise<string[]>((resolve) => {
-      const CAs: string[] = [];
+  instantiate: (di) => {
+    const wincaRootsExePath: string = __non_webpack_require__.resolve("win-ca/lib/roots.exe");
+    const execFile = di.inject(execFileInjectable);
 
-      wincaAPI({
-        format: wincaAPI.der2.pem,
-        inject: false,
-        ondata: (ca: string) => {
-          CAs.push(ca);
-        },
-        onend: () => {
-          resolve(CAs);
-        },
-      });
-    });
+    return async () => {
+      /**
+       * This needs to be done manually because for some reason calling the api from "win-ca"
+       * directly fails to load "child_process" correctly on renderer
+       */
+      const output = await execFile(wincaRootsExePath);
+      
+      return output
+        .split("\r\n")
+        .filter(Boolean)
+        .map(pemEncoding);
+    };
   },
   causesSideEffects: true,
   injectionToken: requestSystemCAsInjectionToken,
