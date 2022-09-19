e385e8bbc1fb0db51b017c0e63bd53d4021f5f1d
Sebastian Malton <sebastian@malton.name>
2022-09-19 10:52:48 -0400

    Convert ReleaseChannel into an InitializableState
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ src/main/vars/release-channel/init.injectable.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import releaseChannelInjectable from "../../../common/vars/release-channel.injectable";
+import { beforeApplicationIsLoadingInjectionToken } from "../../start-main-application/runnable-tokens/before-application-is-loading-injection-token";
+import initSemanticBuildVersionInjectable from "../semantic-build-version/init.injectable";
+
+const initReleaseChannelInjectable = getInjectable({
+  id: "init-release-channel",
+  instantiate: (di) => {
+    const releaseChannel = di.inject(releaseChannelInjectable);
+
+    return {
+      run: () => releaseChannel.init(),
+      runAfter: di.inject(initSemanticBuildVersionInjectable),
+    };
+  },
+  injectionToken: beforeApplicationIsLoadingInjectionToken,
+});
+
+export default initReleaseChannelInjectable;
