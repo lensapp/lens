bbb85ec763a86f150c3685f669409814955aabda
Billy Tobon <billy.tobon@gmail.com>
2022-09-30 15:44:25 -0400

    adding asc provider
    Signed-off-by: Billy Tobon <billy.tobon@gmail.com>

--- build/notarize.js
+++ build/notarize.js
@@ -22,5 +22,6 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
+    ascProvider:process.env.ASCPROVIDER,
   });
 };
