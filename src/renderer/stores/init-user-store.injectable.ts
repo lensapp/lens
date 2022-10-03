00ad23a7eaa164f74a4b79f99f82fb1821d0b780
Sebastian Malton <sebastian@malton.name>
2022-10-03 08:28:11 -0400

    Add IDs to runnables
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/stores/init-user-store.injectable.ts
+++ src/renderer/stores/init-user-store.injectable.ts
@@ -5,7 +5,7 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import userStoreInjectable from "../../common/user-store/user-store.injectable";
 import { beforeFrameStartsInjectionToken } from "../before-frame-starts/before-frame-starts-injection-token";
-import initDefaultUpdateChannelInjectableInjectable from "../vars/default-update-channel/init.injectable";
+import initDefaultUpdateChannelInjectable from "../vars/default-update-channel/init.injectable";
 
 const initUserStoreInjectable = getInjectable({
   id: "init-user-store",
@@ -13,8 +13,9 @@ const initUserStoreInjectable = getInjectable({
     const userStore = di.inject(userStoreInjectable);
 
     return {
+      id: "init-user-store",
       run: () => userStore.load(),
-      runAfter: di.inject(initDefaultUpdateChannelInjectableInjectable),
+      runAfter: di.inject(initDefaultUpdateChannelInjectable),
     };
   },
   injectionToken: beforeFrameStartsInjectionToken,
