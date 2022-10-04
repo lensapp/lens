3d35ba5c2433e80dc96862d21e06f71dcfcb6911
Iku-turso <mikko.aspiala@gmail.com>
2022-10-04 16:19:51 +0300

    Make global override less strict to simplify setup of many unit tests
    There's a better solution for this in the horizon, as this overridden thing is better faked than stubbed.
    
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- src/features/application-menu/main/populate-application-menu.global-override-for-injectable.ts
+++ src/features/application-menu/main/populate-application-menu.global-override-for-injectable.ts
@@ -2,7 +2,7 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { getGlobalOverrideForFunction } from "../../../common/test-utils/get-global-override-for-function";
 import populateApplicationMenuInjectable from "./populate-application-menu.injectable";
+import { getGlobalOverride } from "../../../common/test-utils/get-global-override";
 
-export default getGlobalOverrideForFunction(populateApplicationMenuInjectable);
+export default getGlobalOverride(populateApplicationMenuInjectable, () => () => {});
