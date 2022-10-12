9f19fdceb26bdc4c6fdcc986f9c94ebf0e0bd5d2
Iku-turso <mikko.aspiala@gmail.com>
2022-10-12 09:17:37 +0300

    Simplify getting of composite paths
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- src/features/application-menu/main/menu-items/get-composite/get-composite-paths/get-composite-paths.ts
+++ src/features/application-menu/main/menu-items/get-composite/get-composite-paths/get-composite-paths.ts
@@ -3,26 +3,10 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { pipeline } from "@ogre-tools/fp";
-import { flatMap } from "lodash/fp";
+import { map } from "lodash/fp";
 import type { Composite } from "../get-composite";
+import { normalizeComposite } from "../normalize-composite/normalize-composite";
 
 export const getCompositePaths = (
-  composite: Composite<any>,
-  previousPath: string[] = [],
-): string[] => {
-  const currentPath = [...previousPath, composite.id];
-
-  const currentPathString = currentPath.join(".");
-
-  return [
-    currentPathString,
-
-    ...pipeline(
-      composite.children,
-
-      flatMap((childComposite) =>
-        getCompositePaths(childComposite, currentPath),
-      ),
-    ),
-  ];
-};
+  composite: Composite<unknown>,
+): string[] => pipeline(composite, normalizeComposite, map(([path]) => path));
