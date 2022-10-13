dfaf90a39f199028240073b2e74b07e1ab5980f8
Janne Savolainen <janne.savolainen@live.fi>
2022-10-13 12:07:54 +0300

    Introduce reusable horizontal line
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ src/renderer/components/+preferences/horizontal-line/horizontal-line.tsx
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import React from "react";
+
+import styles from "./horizontal-line.module.scss";
+
+export const HorizontalLine = () => <hr className={styles.horizontalLine} />;
