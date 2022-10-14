ffd4b13dcdf779d511ab81e726b8ba0cc1939532
Janne Savolainen <janne.savolainen@live.fi>
2022-10-14 08:21:52 +0300

    Revert "Introduce reusable horizontal line"
    This reverts commit 4d8c147fe0f1a14bd884f73cf345e7d3a28b954a.
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/components/+preferences/horizontal-line/horizontal-line.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) OpenLens Authors. All rights reserved.
- * Licensed under MIT License. See LICENSE in root directory for more information.
- */
-import React from "react";
-
-import styles from "./horizontal-line.module.scss";
-
-export const HorizontalLine = () => <hr className={styles.horizontalLine} />;
