ed14cdc2048778265fbb3fba9639af7ef6857e12
Janne Savolainen <janne.savolainen@live.fi>
2022-10-14 12:06:07 +0300

    Remove usage of old code
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/renderer/components/+preferences/extensions.tsx
+++ src/renderer/components/+preferences/extensions.tsx
@@ -10,7 +10,6 @@ import React from "react";
 import type { RegisteredAppPreference } from "./app-preferences/app-preference-registration";
 import extensionPreferencesModelInjectable from "./extension-preference-model.injectable";
 import { ExtensionSettings } from "./extension-settings";
-import { Preferences } from "./preferences";
 
 interface Dependencies {
   model: IComputedValue<{
@@ -24,29 +23,27 @@ const NonInjectedExtensions = ({ model }: Dependencies) => {
   const { extensionName, preferenceItems, preferencePageTitle } = model.get();
 
   return (
-    <Preferences data-testid="extension-preferences-page">
-      <section id="extensions">
-        <h2 data-testid="extension-preferences-page-title">
-          {preferencePageTitle}
-        </h2>
-        {!extensionName && (
-          <div
-            className="flex items-center"
-            data-testid="error-for-extension-not-being-present"
-          >
-            No extension found
-          </div>
-        )}
-        {preferenceItems.map((preferenceItem, index) => (
-          <ExtensionSettings
-            key={`${preferenceItem.id}-${index}`}
-            setting={preferenceItem}
-            size="small"
-            data-testid={`extension-preference-item-for-${preferenceItem.id}`}
-          />
-        ))}
-      </section>
-    </Preferences>
+    <section id="extensions">
+      <h2 data-testid="extension-preferences-page-title">
+        {preferencePageTitle}
+      </h2>
+      {!extensionName && (
+        <div
+          className="flex items-center"
+          data-testid="error-for-extension-not-being-present"
+        >
+          No extension found
+        </div>
+      )}
+      {preferenceItems.map((preferenceItem, index) => (
+        <ExtensionSettings
+          key={`${preferenceItem.id}-${index}`}
+          setting={preferenceItem}
+          size="small"
+          data-testid={`extension-preference-item-for-${preferenceItem.id}`}
+        />
+      ))}
+    </section>
   );
 };
 
