6f4a6c95fccb2963e08f4821d0947cdbffa4f0b6
Iku-turso <mikko.aspiala@gmail.com>
2022-10-17 12:21:31 +0300

    Remove code made redundant with hiding of preference tabs without content
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- src/features/preferences/renderer/preference-items/telemetry/telemetry-preference-page.injectable.tsx
+++ src/features/preferences/renderer/preference-items/telemetry/telemetry-preference-page.injectable.tsx
@@ -6,24 +6,18 @@ import { getInjectable } from "@ogre-tools/injectable";
 import { preferenceItemInjectionToken } from "../preference-item-injection-token";
 import { TelemetryPage } from "./telemetry-page";
 import React from "react";
-import sentryDataSourceNameInjectable from "../../../../../common/vars/sentry-dsn-url.injectable";
 
 const telemetryPreferencePageInjectable = getInjectable({
   id: "telemetry-preference-page",
 
-  instantiate: (di) => {
-    const sentryDnsUrl = di.inject(sentryDataSourceNameInjectable);
-
-    return {
-      kind: "page" as const,
-      id: "telemetry-page",
-      parentId: "telemetry-tab",
-      orderNumber: 0,
-      Component: TelemetryPage,
-      childrenSeparator: () => <hr className="small" />,
-      isShown: !!sentryDnsUrl,
-    };
-  },
+  instantiate: () => ({
+    kind: "page" as const,
+    id: "telemetry-page",
+    parentId: "telemetry-tab",
+    orderNumber: 0,
+    Component: TelemetryPage,
+    childrenSeparator: () => <hr className="small" />,
+  }),
 
   injectionToken: preferenceItemInjectionToken,
 });
--- src/features/preferences/renderer/preference-items/telemetry/telemetry-preference-tab.injectable.ts
+++ src/features/preferences/renderer/preference-items/telemetry/telemetry-preference-tab.injectable.ts
@@ -4,25 +4,19 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import { preferenceItemInjectionToken } from "../preference-item-injection-token";
-import sentryDataSourceNameInjectable from "../../../../../common/vars/sentry-dsn-url.injectable";
 
 const telemetryPreferenceTabInjectable = getInjectable({
   id: "telemetry-preference-tab",
 
-  instantiate: (di) => {
-    const sentryDnsUrl = di.inject(sentryDataSourceNameInjectable);
-
-    return {
-      kind: "tab" as const,
-      id: "telemetry-tab",
-      parentId: "general-tab-group" as const,
-      pathId: "telemetry",
-      testId: "terminal-preferences-page",
-      label: "Telemetry",
-      orderNumber: 60,
-      isShown: !!sentryDnsUrl,
-    };
-  },
+  instantiate: () => ({
+    kind: "tab" as const,
+    id: "telemetry-tab",
+    parentId: "general-tab-group" as const,
+    pathId: "telemetry",
+    testId: "terminal-preferences-page",
+    label: "Telemetry",
+    orderNumber: 60,
+  }),
 
   injectionToken: preferenceItemInjectionToken,
 });
