071ba8ac2489df9c0339e94a431b247ead311fb4
Sebastian Malton <sebastian@malton.name>
2022-10-18 15:46:13 -0400

    Fix installing canvas on all platforms
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- scripts/fix-canvas-deps.ts
+++ scripts/fix-canvas-deps.ts
@@ -4,21 +4,13 @@
  */
 
 import child_process from "child_process";
-import { remove } from "fs-extra";
+import { ensureDir } from "fs-extra";
 import fetch from "node-fetch";
 import { platform } from "process";
 import { SemVer } from "semver";
 import stream from "stream";
 import { promisify } from "util";
 import { extract } from "tar";
-
-// From "Node Major Version" to NODE_MODULE_VERSION
-const NODE_MODULE_VERSIONS: Partial<Record<string, string>> = {
-  "16": "93",
-  "17": "102",
-  "18": "108",
-  "19": "111",
-};
 
 function canvasPrebuiltUrlBuilder(canvasVersion: SemVer, nodeVersion: string) {
   const compiler = platform === "linux"
@@ -31,27 +23,23 @@ function canvasPrebuiltUrlBuilder(canvasVersion: SemVer, nodeVersion: string) {
 const exec = promisify(child_process.exec);
 const pipeline = promisify(stream.pipeline);
 
-const electronVersionCommand = "npm list --depth=0 --json";
-const { stdout } = await exec(electronVersionCommand);
+// This is done so that we can skip the scripts for only this package
+await exec("npm install canvas@2 --no-save --no-package-lock --ignore-scripts");
+
+const { stdout } = await exec("npm list --depth=0 --json");
 const output = JSON.parse(stdout);
 
-const nodeVersion = new SemVer(output.dependencies["@types/node"].version);
-const nodeModuleVersion = NODE_MODULE_VERSIONS[`${nodeVersion.major}`];
-
-if (!nodeModuleVersion) {
-  throw new Error(`Unknown node major version "${nodeVersion.major}". You need to update the NODE_MODULE_VERSIONS table above from https://nodejs.org/en/download/releases/`);
-}
-
+const nodeModuleVersion = process.versions.modules;
 const canvasVersion = new SemVer(output.dependencies["canvas"].version);
 const canvasPrebuildUrl = canvasPrebuiltUrlBuilder(canvasVersion, nodeModuleVersion);
-
-await remove("./node_modules/canvas/build/Release");
 
 const canvasPrebuilt = await fetch(canvasPrebuildUrl);
 
 if (canvasPrebuilt.status !== 200) {
   throw new Error(`Failed to download prebuilt from ${canvasPrebuildUrl}: ${canvasPrebuilt.statusText}`);
 }
+
+await ensureDir("./node_modules/canvas/build");
 
 const dest = extract({
   cwd: "./node_modules/canvas/build",
