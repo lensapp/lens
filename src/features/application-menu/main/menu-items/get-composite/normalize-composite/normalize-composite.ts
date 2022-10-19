638ea2e12ef2aed575a43d44704b35a9b9fd5569
Janne Savolainen <janne.savolainen@live.fi>
2022-10-19 13:51:36 +0300

    Clean public interface of a normalize composite
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- src/features/application-menu/main/menu-items/get-composite/normalize-composite/normalize-composite.ts
+++ src/features/application-menu/main/menu-items/get-composite/normalize-composite/normalize-composite.ts
@@ -4,17 +4,21 @@
  */
 import type { Composite } from "../get-composite";
 
-export const normalizeComposite = <T>(
-  composite: Composite<T>,
-  previousPath: string[] = [],
-): (readonly [path: string, composite: Composite<T>])[] => {
-  const currentPath = [...previousPath, composite.id];
+export const normalizeComposite = <T>(composite: Composite<T>) => {
+  const _normalizeComposite = <T>(
+    composite: Composite<T>,
+    previousPath: string[] = [],
+  ): (readonly [path: string, composite: Composite<T>])[] => {
+    const currentPath = [...previousPath, composite.id];
 
-  const pathAndCompositeTuple = [currentPath.join("."), composite] as const;
+    const pathAndCompositeTuple = [currentPath.join("."), composite] as const;
 
-  return [
-    pathAndCompositeTuple,
+    return [
+      pathAndCompositeTuple,
 
-    ...composite.children.flatMap((x) => normalizeComposite(x, currentPath)),
-  ];
+      ...composite.children.flatMap((x) => _normalizeComposite(x, currentPath)),
+    ];
+  };
+
+  return _normalizeComposite(composite);
 };
