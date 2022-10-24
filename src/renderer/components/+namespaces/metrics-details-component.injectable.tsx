84dbb5f86ad2ab01f08be33031b65a5692a4459d
Sebastian Malton <sebastian@malton.name>
2022-10-24 15:08:39 -0400

    Change namespace metrics details to use more injectables
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+namespaces/metrics-details-component.injectable.tsx
+++ src/renderer/components/+namespaces/metrics-details-component.injectable.tsx
@@ -3,43 +3,31 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import { asyncComputed } from "@ogre-tools/injectable-react";
-import { computed } from "mobx";
-import { now } from "mobx-utils";
 import React from "react";
 import { PodCharts, podMetricTabs } from "../+workloads-pods/pod-charts";
 import { ClusterMetricsResourceType } from "../../../common/cluster-types";
 import type { Namespace } from "../../../common/k8s-api/endpoints";
-import requestPodMetricsInNamespaceInjectable from "../../../common/k8s-api/endpoints/metrics.api/request-pod-metrics-in-namespace.injectable";
-import getActiveClusterEntityInjectable from "../../api/catalog/entity/get-active-cluster-entity.injectable";
+import enabledMetricsInjectable from "../../api/catalog/entity/metrics-enabled.injectable";
 import type { KubeObjectDetailsProps } from "../kube-object-details";
 import { kubeObjectDetailItemInjectionToken } from "../kube-object-details/kube-object-detail-items/kube-object-detail-item-injection-token";
 import { ResourceMetrics } from "../resource-metrics";
+import namespaceMetricsInjectable from "./metrics.injectable";
 
 const namespaceMetricsDetailsComponentInjectable = getInjectable({
   id: "namespace-metrics-details-component",
-  instantiate: (di) => {
-    const getActiveClusterEntity = di.inject(getActiveClusterEntityInjectable);
-    const requestPodMetricsInNamespace = di.inject(requestPodMetricsInNamespaceInjectable);
-
-    return {
-      Component: (props: KubeObjectDetailsProps<Namespace>) => (
-        <ResourceMetrics
-          tabs={podMetricTabs}
-          object={props.object}
-          metrics={asyncComputed(async () => {
-            now(60 * 1000); // Update every minute
-
-            return requestPodMetricsInNamespace(props.object.getName());
-          })}
-        >
-          <PodCharts />
-        </ResourceMetrics>
-      ),
-      enabled: computed(() => !getActiveClusterEntity()?.isMetricHidden(ClusterMetricsResourceType.Namespace)),
-      orderNumber: -1,
-    };
-  },
+  instantiate: (di) => ({
+    Component: ({ object }: KubeObjectDetailsProps<Namespace>) => (
+      <ResourceMetrics
+        tabs={ podMetricTabs }
+        object={ object }
+        metrics={ di.inject(namespaceMetricsInjectable, object) }
+      >
+        <PodCharts />
+      </ResourceMetrics>
+    ),
+    enabled: di.inject(enabledMetricsInjectable, ClusterMetricsResourceType.Namespace),
+    orderNumber: -1,
+  }),
   injectionToken: kubeObjectDetailItemInjectionToken,
 });
 
