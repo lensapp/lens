660dd47966022a6c302335f85e1497a224e35d08
Sebastian Malton <sebastian@malton.name>
2022-10-24 12:26:19 -0400

    Refactor ResourceMetrics to use IAsyncComputed
    - Introduce first use for namespace metrics
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/resource-metrics/resource-metrics.tsx
+++ src/renderer/components/resource-metrics/resource-metrics.tsx
@@ -5,24 +5,22 @@
 
 import "./resource-metrics.scss";
 
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { Radio, RadioGroup } from "../radio";
-import { useInterval } from "../../hooks";
 import type { KubeObject } from "../../../common/k8s-api/kube-object";
-import { cssNames, noop } from "../../utils";
+import { cssNames } from "../../utils";
 import { Spinner } from "../spinner";
 import type { MetricsTab } from "../chart/options";
 import type { MetricData } from "../../../common/k8s-api/endpoints/metrics.api";
+import type { IAsyncComputed } from "@ogre-tools/injectable-react";
 
 export type AtLeastOneMetricTab = [MetricsTab, ...MetricsTab[]];
 
 export interface ResourceMetricsProps<Keys extends string> {
   tabs: AtLeastOneMetricTab;
   object: KubeObject;
-  loader?: () => void;
-  interval?: number;
   className?: string;
-  metrics: Partial<Record<Keys, MetricData>> | null | undefined;
+  metrics: IAsyncComputed<Partial<Record<Keys, MetricData>> | null | undefined>;
   children: React.ReactChild | React.ReactChild[];
 }
 
@@ -34,48 +32,41 @@ export interface ResourceMetricsValue {
 
 export const ResourceMetricsContext = createContext<ResourceMetricsValue | null>(null);
 
-export function ResourceMetrics<Keys extends string>({ object, loader = noop, interval = 60, tabs, children, className, metrics }: ResourceMetricsProps<Keys>) {
+export function ResourceMetrics<Keys extends string>({ object, tabs, children, className, metrics }: ResourceMetricsProps<Keys>) {
   const [tab, setTab] = useState<MetricsTab>(tabs[0]);
-
-  // This is done just incase `loader` is actually something like `() => Promise<void>`
-  useEffect(() => void loader(), [object]);
-  useInterval(loader, interval * 1000);
-
-  const renderContents = () => {
-    return (
-      <>
-        <div className="switchers">
-          <RadioGroup
-            asButtons
-            className="flex box grow gaps"
-            value={tab}
-            onChange={setTab}
-          >
-            {tabs.map((tab, index) => (
-              <Radio
-                key={index}
-                className="box grow"
-                label={tab}
-                value={tab}
-              />
-            ))}
-          </RadioGroup>
-        </div>
-        <ResourceMetricsContext.Provider value={{ object, tab, metrics }}>
-          <div className="graph">
-            {children}
-          </div>
-        </ResourceMetricsContext.Provider>
-        <div className="loader">
-          <Spinner/>
-        </div>
-      </>
-    );
-  };
 
   return (
     <div className={cssNames("ResourceMetrics flex column", className)}>
-      {renderContents()}
+      <div className="switchers">
+        <RadioGroup
+          asButtons
+          className="flex box grow gaps"
+          value={tab}
+          onChange={setTab}
+        >
+          {tabs.map((tab, index) => (
+            <Radio
+              key={index}
+              className="box grow"
+              label={tab}
+              value={tab} />
+          ))}
+        </RadioGroup>
+      </div>
+      <ResourceMetricsContext.Provider
+        value={{
+          object,
+          tab,
+          metrics: metrics.value.get(),
+        }}
+      >
+        <div className="graph">
+          {children}
+        </div>
+      </ResourceMetricsContext.Provider>
+      <div className="loader">
+        <Spinner />
+      </div>
     </div>
   );
 }
