a1c9b9a22bc512c6a5509ed30ce5889b0ca5a7e3
Sebastian Malton <sebastian@malton.name>
2022-10-24 15:06:02 -0400

    Replace NodeMetrics in details with injectable
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ src/renderer/components/+nodes/metrics-details-component.injectable.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import React from "react";
+import { ClusterMetricsResourceType } from "../../../common/cluster-types";
+import type { Node } from "../../../common/k8s-api/endpoints";
+import enabledMetricsInjectable from "../../api/catalog/entity/metrics-enabled.injectable";
+import type { KubeObjectDetailsProps } from "../kube-object-details";
+import { kubeObjectDetailItemInjectionToken } from "../kube-object-details/kube-object-detail-items/kube-object-detail-item-injection-token";
+import { ResourceMetrics } from "../resource-metrics";
+import nodeMetricsInjectable from "./metrics.injectable";
+import { NodeCharts } from "./node-charts";
+
+const nodeMetricsDetailsComponentInjectable = getInjectable({
+  id: "node-metrics-details-component",
+  instantiate: (di) => ({
+    Component: ({ object }: KubeObjectDetailsProps<Node>) => (
+      <ResourceMetrics
+        tabs={ [
+          "CPU",
+          "Memory",
+          "Disk",
+          "Pods",
+        ] }
+        object={ object }
+        metrics={ di.inject(nodeMetricsInjectable, object) }
+      >
+        <NodeCharts />
+      </ResourceMetrics>
+    ),
+    enabled: di.inject(enabledMetricsInjectable, ClusterMetricsResourceType.Node),
+    orderNumber: -1,
+  }),
+  injectionToken: kubeObjectDetailItemInjectionToken,
+});
+
+export default nodeMetricsDetailsComponentInjectable;
