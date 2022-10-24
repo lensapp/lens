239e3eac244dbc30c074e18951f65466617c61df
Alex Andreev <alex.andreev.email@gmail.com>
2022-10-24 12:24:06 +0300

    Fix linter again
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/renderer/cluster-modals/cluster-modals-registrator.injectable.ts
+++ src/renderer/cluster-modals/cluster-modals-registrator.injectable.ts
@@ -5,7 +5,8 @@
 import { pipeline } from "@ogre-tools/fp";
 import { getInjectable } from "@ogre-tools/injectable";
 import { map } from "lodash/fp";
-import { ExtensionRegistrator, extensionRegistratorInjectionToken } from "../../extensions/extension-loader/extension-registrator-injection-token";
+import { extensionRegistratorInjectionToken } from "../../extensions/extension-loader/extension-registrator-injection-token";
+import type { ExtensionRegistrator } from "../../extensions/extension-loader/extension-registrator-injection-token";
 import type { LensRendererExtension } from "../../extensions/lens-renderer-extension";
 import { clusterModalsInjectionToken } from "./cluster-modals-injection-token";
 
