2370928ea7b648b4e6bbc1f8b51c5b8082861fd2
Sebastian Malton <sebastian@malton.name>
2022-11-04 06:25:29 -0700

    Fix syncing shell environment when using fish (#6502)
    * Fix syncing shell environment when using fish
    
    - Add some better logging for the future
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Add some unit tests to codify assumptions
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Fix timeout
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Update tests
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Fix tests
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Fix handling of '' in env
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Add function description
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/utils/shell-env/compute-unix-shell-environment.injectable.ts
+++ src/main/utils/shell-env/compute-unix-shell-environment.injectable.ts
@@ -7,96 +7,154 @@ import { getInjectable } from "@ogre-tools/injectable";
 import getBasenameOfPathInjectable from "../../../common/path/get-basename.injectable";
 import spawnInjectable from "../../child-process/spawn.injectable";
 import randomUUIDInjectable from "../../crypto/random-uuid.injectable";
+import loggerInjectable from "../../../common/logger.injectable";
+import processExecPathInjectable from "./execPath.injectable";
+import processEnvInjectable from "./env.injectable";
+import { object } from "../../../common/utils";
+import type { AsyncResult } from "../../../common/utils/async-result";
 
 export interface UnixShellEnvOptions {
   signal: AbortSignal;
 }
 
-export type ComputeUnixShellEnvironment = (shell: string, opts: UnixShellEnvOptions) => Promise<EnvironmentVariables>;
+export type ComputeUnixShellEnvironment = (shell: string, opts: UnixShellEnvOptions) => Promise<AsyncResult<EnvironmentVariables, string>>;
+
+/**
+ * @param src The object containing the current environment variables
+ * @param overrides The environment variables that want to be overridden before passing the env to a child process
+ * @returns The combination of environment variables and a function which resets an object of environment variables to the values the keys corresponded to in `src` (rather than `overrides`)
+ */
+const getResetProcessEnv = (src: Partial<Record<string, string>>, overrides: Partial<Record<string, string>>): {
+  resetEnvPairs: (target: Partial<Record<string, string>>) => void;
+  env: Partial<Record<string, string>>;
+} => {
+  const originals = object.entries(overrides).map(([name]) => [name, src[name]] as const);
+
+  return {
+    env: {
+      ...src,
+      ...overrides,
+    },
+    resetEnvPairs: (target) => {
+      for (const [name, orginalValue] of originals) {
+        if (typeof orginalValue === "string") {
+          target[name] = orginalValue;
+        } else {
+          delete target[name];
+        }
+      }
+    },
+  };
+};
 
 const computeUnixShellEnvironmentInjectable = getInjectable({
   id: "compute-unix-shell-environment",
   instantiate: (di): ComputeUnixShellEnvironment => {
     const powerShellName = /^pwsh(-preview)?$/;
-    const nonBashLikeShellName = /^t?csh$/;
+    const cshLikeShellName = /^(t?csh)$/;
+    const fishLikeShellName = /^fish$/;
 
     const getBasenameOfPath = di.inject(getBasenameOfPathInjectable);
     const spawn = di.inject(spawnInjectable);
+    const logger = di.inject(loggerInjectable);
     const randomUUID = di.inject(randomUUIDInjectable);
+    const processExecPath = di.inject(processExecPathInjectable);
+    const processEnv = di.inject(processEnvInjectable);
 
-    const getShellSpecifices = (shellPath: string, mark: string) => {
-      const shellName = getBasenameOfPath(shellPath);
+    const getShellSpecifices = (shellName: string) => {
+      const mark = randomUUID().replace(/-/g, "");
+      const regex = new RegExp(`${mark}(\\{.*\\})${mark}`);
 
       if (powerShellName.test(shellName)) {
         // Older versions of PowerShell removes double quotes sometimes so we use "double single quotes" which is how
         // you escape single quotes inside of a single quoted string.
         return {
-          command: `Command '${process.execPath}' -p '\\"${mark}\\" + JSON.stringify(process.env) + \\"${mark}\\"'`,
+          command: `Command '${processExecPath}' -p '\\"${mark}\\" + JSON.stringify(process.env) + \\"${mark}\\"'`,
           shellArgs: ["-Login"],
+          regex,
         };
       }
 
-      return {
-        command: `'${process.execPath}' -p '"${mark}" + JSON.stringify(process.env) + "${mark}"'`,
-        shellArgs: nonBashLikeShellName.test(shellName)
-          // tcsh and csh don't support any other options when providing the -l (login) shell option
-          ? ["-l"]
-          // zsh (at least, maybe others) don't load RC files when in non-interactive mode, even when using -l (login) option
-          : ["-li"],
-      };
+      let command = `'${processExecPath}' -p '"${mark}" + JSON.stringify(process.env) + "${mark}"'`;
+      const shellArgs = ["-l"];
+
+      if (fishLikeShellName.test(shellName)) {
+        shellArgs.push("-c", command);
+        command = "";
+      } else if (!cshLikeShellName.test(shellName)) {
+        // zsh (at least, maybe others) don't load RC files when in non-interactive mode, even when using -l (login) option
+        shellArgs.push("-i");
+      } else {
+        // Some shells don't support any other options when providing the -l (login) shell option
+      }
+
+      return { command, shellArgs, regex };
     };
 
+
     return async (shellPath, opts) => {
-      const runAsNode = process.env["ELECTRON_RUN_AS_NODE"];
-      const noAttach = process.env["ELECTRON_NO_ATTACH_CONSOLE"];
-      const env = {
-        ...process.env,
+      const { resetEnvPairs, env } = getResetProcessEnv(processEnv, {
         ELECTRON_RUN_AS_NODE: "1",
         ELECTRON_NO_ATTACH_CONSOLE: "1",
-      };
-      const mark = randomUUID().replace(/-/g, "");
-      const regex = new RegExp(`${mark}(\\{.*\\})${mark}`);
-      const { command, shellArgs } = getShellSpecifices(shellPath, mark);
+        TERM: "screen-256color-bce", // required for fish
+      });
+      const shellName = getBasenameOfPath(shellPath);
+      const { command, shellArgs, regex } = getShellSpecifices(shellName);
 
-      return new Promise((resolve, reject) => {
+      logger.info(`[UNIX-SHELL-ENV]: running against ${shellPath}`, { command, shellArgs });
+
+      return new Promise((resolve) => {
         const shellProcess = spawn(shellPath, shellArgs, {
-          detached: true,
           signal: opts.signal,
           env,
         });
         const stdout: Buffer[] = [];
+        const stderr: Buffer[] = [];
 
         shellProcess.stdout.on("data", b => stdout.push(b));
+        shellProcess.stderr.on("data", b => stderr.push(b));
 
-        shellProcess.on("error", (err) => reject(err));
+        shellProcess.on("error", (err) => resolve({
+          callWasSuccessful: false,
+          error: `Failed to spawn ${shellPath}: ${err}`,
+        }));
         shellProcess.on("close", (code, signal) => {
           if (code || signal) {
-            return reject(new Error(`Unexpected return code from spawned shell (code: ${code}, signal: ${signal})`));
+            const context = {
+              code,
+              signal,
+              stdout: Buffer.concat(stdout).toString("utf-8"),
+              stderr: Buffer.concat(stderr).toString("utf-8"),
+            };
+
+            return resolve({
+              callWasSuccessful: false,
+              error: `Shell did not exit sucessfully: ${JSON.stringify(context, null, 4)}`,
+            });
           }
 
           try {
             const rawOutput = Buffer.concat(stdout).toString("utf-8");
+
+            logger.info(`[UNIX-SHELL-ENV]: got the following output`, { rawOutput });
+
             const match = regex.exec(rawOutput);
             const strippedRawOutput = match ? match[1] : "{}";
-            const resolvedEnv = JSON.parse(strippedRawOutput);
+            const resolvedEnv = JSON.parse(strippedRawOutput) as Partial<Record<string, string>>;
 
-            if (runAsNode) {
-              resolvedEnv["ELECTRON_RUN_AS_NODE"] = runAsNode;
-            } else {
-              delete resolvedEnv["ELECTRON_RUN_AS_NODE"];
-            }
-
-            if (noAttach) {
-              resolvedEnv["ELECTRON_NO_ATTACH_CONSOLE"] = noAttach;
-            } else {
-              delete resolvedEnv["ELECTRON_NO_ATTACH_CONSOLE"];
-            }
-
-            resolve(resolvedEnv);
+            resetEnvPairs(resolvedEnv);
+            resolve({
+              callWasSuccessful: true,
+              response: resolvedEnv,
+            });
           } catch (err) {
-            reject(err);
+            resolve({
+              callWasSuccessful: false,
+              error: String(err),
+            });
           }
         });
+
         shellProcess.stdin.end(command);
       });
     };
