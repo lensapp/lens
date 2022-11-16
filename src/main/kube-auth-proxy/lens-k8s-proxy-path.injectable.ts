7cbf59dc8622161b4eb4fe5d202d4927c496f644
Sebastian Malton <sebastian@malton.name>
2022-11-16 11:52:16 -0500

    Add new injectable version of binaryName
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/kube-auth-proxy/lens-k8s-proxy-path.injectable.ts
+++ src/main/kube-auth-proxy/lens-k8s-proxy-path.injectable.ts
@@ -3,20 +3,19 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import baseBundledBinariesDirectoryInjectable from "../../common/vars/base-bundled-binaries-dir.injectable";
 import joinPathsInjectable from "../../common/path/join-paths.injectable";
 import binaryNameInjectable from "../../common/utils/get-binary-name.injectable";
+import baseBundledBinariesDirectoryInjectable from "../../common/vars/base-bundled-binaries-dir.injectable";
 
-const helmBinaryPathInjectable = getInjectable({
-  id: "helm-binary-path",
-
+const lensK8sProxyPathInjectable = getInjectable({
+  id: "lens-k8s-proxy-path",
   instantiate: (di) => {
     const joinPaths = di.inject(joinPathsInjectable);
-    const binaryName = di.inject(binaryNameInjectable, "helm");
+    const binaryName = di.inject(binaryNameInjectable, "lens-k8s-proxy");
     const baseBundledBinariesDirectory = di.inject(baseBundledBinariesDirectoryInjectable);
 
     return joinPaths(baseBundledBinariesDirectory, binaryName);
   },
 });
 
-export default helmBinaryPathInjectable;
+export default lensK8sProxyPathInjectable;
