f300e6e74fbf4cfc5f23b29853560da8f18f7e9c
Sebastian Malton <sebastian@malton.name>
2022-11-30 09:40:49 -0500

    Add quick namespace filtering to StatefulSets view
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+workloads-statefulsets/statefulsets.tsx
+++ src/renderer/components/+workloads-statefulsets/statefulsets.tsx
@@ -8,13 +8,20 @@ import "./statefulsets.scss";
 import React from "react";
 import { observer } from "mobx-react";
 import type { StatefulSet } from "../../../common/k8s-api/endpoints";
-import { podStore } from "../+workloads-pods/legacy-store";
-import { statefulSetStore } from "./legacy-store";
-import { eventStore } from "../+events/legacy-store";
 import { KubeObjectListLayout } from "../kube-object-list-layout";
 import { KubeObjectStatusIcon } from "../kube-object-status-icon";
 import { SiblingsInTabLayout } from "../layout/siblings-in-tab-layout";
 import { KubeObjectAge } from "../kube-object/age";
+import type { StatefulSetStore } from "./store";
+import type { PodStore } from "../+workloads-pods/store";
+import type { EventStore } from "../+events/store";
+import type { FilterByNamespace } from "../+namespaces/namespace-select-filter-model/filter-by-namespace.injectable";
+import { withInjectables } from "@ogre-tools/injectable-react";
+import eventStoreInjectable from "../+events/store.injectable";
+import filterByNamespaceInjectable from "../+namespaces/namespace-select-filter-model/filter-by-namespace.injectable";
+import podStoreInjectable from "../+workloads-pods/store.injectable";
+import statefulSetStoreInjectable from "./store.injectable";
+import { prevDefault } from "../../utils";
 
 enum columnId {
   name = "name",
@@ -24,8 +31,15 @@ enum columnId {
   replicas = "replicas",
 }
 
+interface Dependencies {
+  statefulSetStore: StatefulSetStore;
+  podStore: PodStore;
+  eventStore: EventStore;
+  filterByNamespace: FilterByNamespace;
+}
+
 @observer
-export class StatefulSets extends React.Component {
+class NonInjectedStatefulSets extends React.Component<Dependencies> {
   renderPods(statefulSet: StatefulSet) {
     const { readyReplicas, currentReplicas } = statefulSet.status ?? {};
 
@@ -33,6 +47,13 @@ export class StatefulSets extends React.Component {
   }
 
   render() {
+    const {
+      eventStore,
+      filterByNamespace,
+      podStore,
+      statefulSetStore,
+    } = this.props;
+
     return (
       <SiblingsInTabLayout>
         <KubeObjectListLayout
@@ -61,7 +82,13 @@ export class StatefulSets extends React.Component {
           ]}
           renderTableContents={statefulSet => [
             statefulSet.getName(),
-            statefulSet.getNs(),
+            <a
+              key="namespace"
+              className="filterNamespace"
+              onClick={prevDefault(() => filterByNamespace(statefulSet.getNs()))}
+            >
+              {statefulSet.getNs()}
+            </a>,
             this.renderPods(statefulSet),
             statefulSet.getReplicas(),
             <KubeObjectStatusIcon key="icon" object={statefulSet} />,
@@ -72,3 +99,13 @@ export class StatefulSets extends React.Component {
     );
   }
 }
+
+export const StatefulSets = withInjectables<Dependencies>(NonInjectedStatefulSets, {
+  getProps: (di, props) => ({
+    ...props,
+    eventStore: di.inject(eventStoreInjectable),
+    filterByNamespace: di.inject(filterByNamespaceInjectable),
+    podStore: di.inject(podStoreInjectable),
+    statefulSetStore: di.inject(statefulSetStoreInjectable),
+  }),
+});
