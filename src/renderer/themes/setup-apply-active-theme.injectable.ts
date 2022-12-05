83bb5c37d9d2e0db0aa05d7fcfa0a313fe58ad17
Sebastian Malton <sebastian@malton.name>
2022-12-05 13:43:00 -0500

    Fix ordering of runnable and order of injection
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/themes/setup-apply-active-theme.injectable.ts
+++ src/renderer/themes/setup-apply-active-theme.injectable.ts
@@ -6,29 +6,31 @@ import { getInjectable } from "@ogre-tools/injectable";
 import { reaction } from "mobx";
 import initializeSystemThemeTypeInjectable from "../../features/theme/system-type/renderer/initialize.injectable";
 import { beforeFrameStartsInjectionToken } from "../before-frame-starts/tokens";
+import initUserStoreInjectable from "../stores/init-user-store.injectable";
 import activeThemeInjectable from "./active.injectable";
 import applyLensThemeInjectable from "./apply-lens-theme.injectable";
 
 const setupApplyActiveThemeInjectable = getInjectable({
   id: "setup-apply-active-theme",
-  instantiate: (di) => {
-    const activeTheme = di.inject(activeThemeInjectable);
-    const applyLensTheme = di.inject(applyLensThemeInjectable);
+  instantiate: (di) => ({
+    id: "setup-apply-active-theme",
+    run: () => {
+      const activeTheme = di.inject(activeThemeInjectable);
+      const applyLensTheme = di.inject(applyLensThemeInjectable);
 
-    return {
-      id: "setup-apply-active-theme",
-      run: () => {
-        reaction(
-          () => activeTheme.get(),
-          applyLensTheme,
-          {
-            fireImmediately: true,
-          },
-        );
-      },
-      runAfter: di.inject(initializeSystemThemeTypeInjectable),
-    };
-  },
+      reaction(
+        () => activeTheme.get(),
+        applyLensTheme,
+        {
+          fireImmediately: true,
+        },
+      );
+    },
+    runAfter: [
+      di.inject(initializeSystemThemeTypeInjectable),
+      di.inject(initUserStoreInjectable),
+    ],
+  }),
   injectionToken: beforeFrameStartsInjectionToken,
 });
 
