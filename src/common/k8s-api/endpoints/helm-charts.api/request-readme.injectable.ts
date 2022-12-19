ba5030f767af081e48833b89eaee6333f9b69bde
Alex Andreev <alex.andreev.email@gmail.com>
2022-12-19 12:17:00 +0300

    Change request readme types to be AsyncResult
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/common/k8s-api/endpoints/helm-charts.api/request-readme.injectable.ts
+++ src/common/k8s-api/endpoints/helm-charts.api/request-readme.injectable.ts
@@ -3,12 +3,13 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
+import type { AsyncResult } from "../../../utils/async-result";
 import { urlBuilderFor } from "../../../utils/buildUrl";
 import apiBaseInjectable from "../../api-base.injectable";
 
 const requestReadmeEndpoint = urlBuilderFor("/v2/charts/:repo/:name/readme");
 
-export type RequestHelmChartReadme = (repo: string, name: string, version?: string) => Promise<string>;
+export type RequestHelmChartReadme = (repo: string, name: string, version?: string) => Promise<AsyncResult<string>>;
 
 const requestHelmChartReadmeInjectable = getInjectable({
   id: "request-helm-chart-readme",
