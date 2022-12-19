e0bccfabd4b0ddf61b2a72aa9c2c051db0e08a97
Alex Andreev <alex.andreev.email@gmail.com>
2022-12-19 17:00:13 +0300

    Fix app crash opening Install Chart dock tab (#6782)
    * Change type of RequestHelmChartValues to be AsyncResult
    
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>
    
    * Fixing tests
    
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>
    
    * Linter fix
    
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>
    
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- src/features/helm-charts/installing-chart/installing-helm-chart-from-new-tab.test.ts
+++ src/features/helm-charts/installing-chart/installing-helm-chart-from-new-tab.test.ts
@@ -240,9 +240,10 @@ describe("installing helm chart from new tab", () => {
 
         describe("when default configuration and versions resolve", () => {
           beforeEach(async () => {
-            await requestHelmChartValuesMock.resolve(
-              "some-default-configuration",
-            );
+            await requestHelmChartValuesMock.resolve({
+              callWasSuccessful: true,
+              response: "some-default-configuration",
+            });
 
             await requestHelmChartVersionsMock.resolve([
               HelmChart.create({
@@ -537,9 +538,10 @@ describe("installing helm chart from new tab", () => {
 
               describe("when configuration and versions resolve", () => {
                 beforeEach(async () => {
-                  await requestHelmChartValuesMock.resolve(
-                    "some-other-default-configuration",
-                  );
+                  await requestHelmChartValuesMock.resolve({
+                    callWasSuccessful: true,
+                    response: "some-other-default-configuration",
+                  });
 
                   await requestHelmChartVersionsMock.resolve([]);
                 });
@@ -702,9 +704,10 @@ describe("installing helm chart from new tab", () => {
 
               describe("when default configuration resolves", () => {
                 beforeEach(async () => {
-                  await requestHelmChartValuesMock.resolve(
-                    "some-default-configuration-for-other-version",
-                  );
+                  await requestHelmChartValuesMock.resolve({
+                    callWasSuccessful: true,
+                    response: "some-default-configuration-for-other-version",
+                  });
                 });
 
                 it("renders", () => {
@@ -847,9 +850,10 @@ describe("installing helm chart from new tab", () => {
                 )
                 .selectOption("some-other-version");
 
-              await requestHelmChartValuesMock.resolve(
-                "some-default-configuration-for-other-version",
-              );
+              await requestHelmChartValuesMock.resolve({
+                callWasSuccessful: true,
+                response: "some-default-configuration-for-other-version",
+              });
 
               expect(installButton).not.toHaveAttribute("disabled");
             });
@@ -920,9 +924,10 @@ describe("installing helm chart from new tab", () => {
                 )
                 .selectOption("some-other-version");
 
-              await requestHelmChartValuesMock.resolve(
-                "some-default-configuration-for-other-version",
-              );
+              await requestHelmChartValuesMock.resolve({
+                callWasSuccessful: true,
+                response: "some-default-configuration-for-other-version",
+              });
 
               const input = rendered.getByTestId(
                 "monaco-editor-for-some-first-tab-id",
