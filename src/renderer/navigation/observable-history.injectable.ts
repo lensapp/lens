067b2d31d4d8d15869ff01d8fc5962e9b8b63050
Sami Tiilikainen <97873007+samitiilikainen@users.noreply.github.com>
2022-12-20 14:47:21 +0200

    Navigation logging injectable
    Move navigation logging to `setupLoggingForNavigationInjectable` to prevent cycle of injectables from occurring. Wasn't eventually needed for #6795 but still an improvement.
    
    Credit for the implementation goes to @Nokel81 , thanks!
    
    Signed-off-by: Sami Tiilikainen <97873007+samitiilikainen@users.noreply.github.com>

--- src/renderer/navigation/observable-history.injectable.ts
+++ src/renderer/navigation/observable-history.injectable.ts
@@ -4,7 +4,6 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import { createObservableHistory } from "mobx-observable-history";
-import loggerInjectable from "../../common/logger.injectable";
 import { searchParamsOptions } from "./search-params";
 import historyInjectable from "./history.injectable";
 
@@ -13,16 +12,8 @@ const observableHistoryInjectable = getInjectable({
 
   instantiate: (di) => {
     const history = di.inject(historyInjectable);
-    const logger = di.inject(loggerInjectable);
     const navigation =  createObservableHistory(history, {
       searchParams: searchParamsOptions,
-    });
-
-    navigation.listen((location, action) => {
-      const isClusterView = !process.isMainFrame;
-      const domain = global.location.href;
-
-      logger.debug(`[NAVIGATION]: ${action}-ing. Current is now:`, { isClusterView, domain, location });
     });
 
     return navigation;
