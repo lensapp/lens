4fdc92a03920e0c0f95fdd6e2ed847abe4dc8de9
Sebastian Malton <sebastian@malton.name>
2022-12-22 11:00:25 -0500

    Remove usages of legacy global podApi
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/k8s-api/endpoints/legacy-globals.ts
+++ src/common/k8s-api/endpoints/legacy-globals.ts
@@ -4,15 +4,9 @@
  */
 
 import { asLegacyGlobalForExtensionApi } from "../../../extensions/as-legacy-globals-for-extension-api/as-legacy-global-object-for-extension-api";
-import podApiInjectable from "./pod.api.injectable";
 import roleApiInjectable from "./role.api.injectable";
 
 /**
  * @deprecated use `di.inject(roleApiInjectable)` instead
  */
 export const roleApi = asLegacyGlobalForExtensionApi(roleApiInjectable);
-
-/**
- * @deprecated use `di.inject(podApiInjectable)` instead
- */
-export const podApi = asLegacyGlobalForExtensionApi(podApiInjectable);
