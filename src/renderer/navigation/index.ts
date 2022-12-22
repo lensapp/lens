88a393849bca871332882a36a42e0d53fa1399d2
Sebastian Malton <sebastian@malton.name>
2022-12-22 11:34:09 -0500

    Remove usages of legacy global navigate
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/navigation/index.ts
+++ src/renderer/navigation/index.ts
@@ -3,14 +3,6 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-import { asLegacyGlobalFunctionForExtensionApi } from "../../extensions/as-legacy-globals-for-extension-api/as-legacy-global-function-for-extension-api";
-import navigateInjectable from "./navigate.injectable";
-
 export { searchParamsOptions } from "./search-params";
-
-/**
- * @deprecated use `di.inject(navigateInjectable)` instead
- */
-export const navigate = asLegacyGlobalFunctionForExtensionApi(navigateInjectable);
 
 export * from "./page-param";
