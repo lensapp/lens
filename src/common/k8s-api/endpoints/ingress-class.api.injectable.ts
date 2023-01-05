7d25e936558101ad956507d7dbd40557043cec6b
Sebastian Malton <sebastian@malton.name>
2023-01-05 15:50:43 -0500

    Fix tests
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/k8s-api/endpoints/ingress-class.api.injectable.ts
+++ src/common/k8s-api/endpoints/ingress-class.api.injectable.ts
@@ -5,12 +5,15 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import { IngressClassApi } from "./ingress-class.api";
 import { kubeApiInjectionToken } from "../kube-api/kube-api-injection-token";
+import loggerInjectable from "../../logger.injectable";
+import maybeKubeApiInjectable from "../maybe-kube-api.injectable";
 
 const ingressClassApiInjectable = getInjectable({
   id: "ingress-class-api",
-  instantiate: () => {
-    return new IngressClassApi();
-  },
+  instantiate: (di) => new IngressClassApi({
+    logger: di.inject(loggerInjectable),
+    maybeKubeApi: di.inject(maybeKubeApiInjectable),
+  }),
 
   injectionToken: kubeApiInjectionToken,
 });
