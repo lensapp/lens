e76d45ceb8e349048d967d746fc6d7cea25635e9
Sebastian Malton <sebastian@malton.name>
2023-01-09 10:21:11 -0500

    Fix build
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/renderer/components/+network-ingresses/ingress-class-store.injectable.ts
+++ src/renderer/components/+network-ingresses/ingress-class-store.injectable.ts
@@ -4,10 +4,9 @@
  */
 import assert from "assert";
 import { getInjectable } from "@ogre-tools/injectable";
-import { kubeObjectStoreInjectionToken } from "../../../common/k8s-api/api-manager/manager.injectable";
+import { kubeObjectStoreInjectionToken } from "../../../common/k8s-api/api-manager/kube-object-store-token";
 import ingressClassApiInjectable from "../../../common/k8s-api/endpoints/ingress-class.api.injectable";
 import { IngressClassStore } from "./ingress-class-store";
-
 import storesAndApisCanBeCreatedInjectable from "../../stores-apis-can-be-created.injectable";
 import clusterFrameContextForClusterScopedResourcesInjectable from "../../cluster-frame-context/for-cluster-scoped-resources.injectable";
 import loggerInjectable from "../../../common/logger.injectable";
