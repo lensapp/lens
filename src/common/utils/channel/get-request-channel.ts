14f50a3f5899a1ef798512fc819edfff42a2e97f
Sebastian Malton <sebastian@malton.name>
2023-01-09 10:59:35 -0500

    Fix unit tests
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/utils/channel/get-request-channel.ts
+++ src/common/utils/channel/get-request-channel.ts
@@ -2,7 +2,8 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import type { RequestChannel } from "./request-channel-listener-injection-token";
+
+import type { RequestChannel } from "./request-channel";
 
 export const getRequestChannel = <Request, Response>(id: string): RequestChannel<Request, Response> => ({
   id,
