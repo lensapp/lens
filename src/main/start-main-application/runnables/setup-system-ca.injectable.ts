604dbdbbd1c300cd99ec3be5e4678416570e419d
Sebastian Malton <sebastian@malton.name>
2023-01-24 11:21:58 -0500

    Move files to features/ to simplify structure
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/main/start-main-application/runnables/setup-system-ca.injectable.ts
+++ src/main/start-main-application/runnables/setup-system-ca.injectable.ts
@@ -4,7 +4,7 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import { beforeApplicationIsLoadingInjectionToken } from "../runnable-tokens/before-application-is-loading-injection-token";
-import injectSystemCAsInjectable from "../../../common/certificate-authorities/inject-system-cas.injectable";
+import injectSystemCAsInjectable from "../../../features/certificate-authorities/common/inject-system-cas.injectable";
 
 const setupSystemCaInjectable = getInjectable({
   id: "setup-system-ca",
