f15235aff45f63bedf72d62d813fc52a811d73a3
Sebastian Malton <sebastian@malton.name>
2023-01-26 11:48:37 -0500

    Remove dead scripts
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- scripts/publish-extensions-npm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./node_modules/.bin/npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-
-NPM_RELEASE_TAG=$(cat package.json | jq .version --raw-output | rg '.*-(?P<channel>\w+).*' -r '$channel' | cat)
-
-cd packages/extensions && npm publish --access=public --tag=${NPM_RELEASE_TAG:-latest} && git restore package.json
