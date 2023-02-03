70ca90f3aec24b0a29ba56c38440bf0edbf225ea
Alex Andreev <alex.andreev.email@gmail.com>
2023-02-03 16:28:23 +0300

    Rename Icon components
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

--- packages/core/src/renderer/components/+namespaces/namespace-tree-view.tsx
+++ packages/core/src/renderer/components/+namespaces/namespace-tree-view.tsx
@@ -93,9 +93,9 @@ function NonInjectableNamespaceTreeView({ root, namespaceStore, getDetailsUrl }:
       <DrawerTitle>Tree View</DrawerTitle>
       <TreeView
         defaultExpanded={[nodeId]}
-        defaultCollapseIcon={<MinusSquare />}
-        defaultExpandIcon={<PlusSquare />}
-        defaultEndIcon={(<div style={{ opacity: 0.3 }}><MinusSquare /></div>)}
+        defaultCollapseIcon={<MinusSquareIcon />}
+        defaultExpandIcon={<PlusSquareIcon />}
+        defaultEndIcon={(<div style={{ opacity: 0.3 }}><MinusSquareIcon /></div>)}
         expanded={expandedItems}
       >
         <TreeItem
@@ -115,7 +115,7 @@ function NonInjectableNamespaceTreeView({ root, namespaceStore, getDetailsUrl }:
   );
 }
 
-function MinusSquare() {
+function MinusSquareIcon() {
   return (
     <SvgIcon style={{ width: 14, height: 14 }} data-testid="minus-square">
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
@@ -123,7 +123,7 @@ function MinusSquare() {
   );
 }
 
-function PlusSquare() {
+function PlusSquareIcon() {
   return (
     <SvgIcon style={{ width: 14, height: 14 }} data-testid="plus-square">
       <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
