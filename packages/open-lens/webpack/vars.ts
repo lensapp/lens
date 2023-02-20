a0fb46c3e65f0fdbee200857e793da9926ed0f4d
Janne Savolainen <janne.savolainen@live.fi>
2023-02-20 12:48:18 +0200

    Switch to using NPM workspaces (#7115)
    * Switch to using npm over yarn
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Remove reference to `yarn` from documentation
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    
    * Enable NPM workspaces
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Introduce script for installing all dependencies
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Remove usage of lerna bootstrap for no longer being used
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Fix build by not bundling libraries for application in node environment (main) as Electron does that
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Tweak evil static import paths in build of application
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Consolidate npmrc configs to root since required by workspaces
    
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Make application build not fail for not detecting electron version
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Update package-lock
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Make sure native dependencies are rebuilt for electron
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Remove unused binary for causing trouble when installing dependencies
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Update package-lock
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    * Disable caching in CI for Windows
    
    Installing Electron in Windows seems to be broken in this regards.
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    
    ---------
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>
    Signed-off-by: Sebastian Malton <sebastian@malton.name>
    Co-authored-by: Sebastian Malton <sebastian@malton.name>

--- packages/open-lens/webpack/vars.ts
+++ packages/open-lens/webpack/vars.ts
@@ -11,9 +11,11 @@ export const mainDir = path.join(process.cwd(), "src", "main");
 export const buildDir = path.join(process.cwd(), "static", "build");
 export const assetsFolderName = "assets";
 export const rendererDir = path.join(process.cwd(), "src", "renderer");
-export const htmlTemplate = path.resolve(__dirname, "..", "node_modules", "@k8slens", "core", "src/renderer", "template.html");
 export const publicPath = "/build/";
-export const sassCommonVars = path.resolve(__dirname, "..", "node_modules", "@k8slens", "core", "src", "renderer", "components/vars.scss");
 export const webpackDevServerPort = Number(process.env.WEBPACK_DEV_SERVER_PORT) || 9191;
+
+// TODO: Figure out a way to access these without relative paths
+export const htmlTemplate = path.resolve(__dirname, "..", "..", "..", "node_modules", "@k8slens", "core", "src/renderer", "template.html");
+export const sassCommonVars = path.resolve(__dirname, "..", "..", "..", "node_modules", "@k8slens", "core", "src", "renderer", "components/vars.scss");
 
 assert(Number.isInteger(webpackDevServerPort), "WEBPACK_DEV_SERVER_PORT environment variable must only be an integer");
