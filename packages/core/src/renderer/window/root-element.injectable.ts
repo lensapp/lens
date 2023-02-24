4fe38cc09a35c71bed7b217e86cadaaaf643abec
Sebastian Malton <sebastian@malton.name>
2023-02-24 15:30:35 -0500

    Remove finished TODO
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/window/root-element.injectable.ts
+++ packages/core/src/renderer/window/root-element.injectable.ts
@@ -3,13 +3,17 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
+import isMacInjectable from "../../common/vars/is-mac.injectable";
 
 const rootElementInjectable = getInjectable({
   id: "root-element",
-  instantiate: () => {
+  instantiate: (di) => {
+    const isMac = di.inject(isMacInjectable);
+
     const rootElement = document.createElement("div");
 
     rootElement.id = "app";
+    rootElement.classList.toggle("is-mac", isMac);
 
     document.getElementsByTagName("body")[0].append(rootElement);
 
