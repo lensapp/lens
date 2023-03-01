23a0465b7b3604943ca03da705e0e05e01edcd91
Sebastian Malton <sebastian@malton.name>
2023-03-01 11:07:01 -0500

    Fix lint again
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/frames/cluster-frame/on-cluster-frame-close.injectable.ts
+++ packages/core/src/renderer/frames/cluster-frame/on-cluster-frame-close.injectable.ts
@@ -3,10 +3,8 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import { once } from "lodash";
 import prefixedLoggerInjectable from "../../../common/logger/prefixed-logger.injectable";
 import clusterFrameClusterInjectable from "../../cluster-frame-context/cluster-frame-cluster.injectable";
-import closeRendererLogFileInjectable from "../../logger/close-renderer-log-file.injectable";
 import unmountRootComponentInjectable from "../../window/unmount-root-component.injectable";
 
 const handleOnClusterFrameCloseInjectable = getInjectable({
@@ -14,18 +12,13 @@ const handleOnClusterFrameCloseInjectable = getInjectable({
   instantiate: (di) => {
     const cluster = di.inject(clusterFrameClusterInjectable);
     const logger = di.inject(prefixedLoggerInjectable, "CLUSTER-FRAME");
-    const closeFileLogging = di.inject(closeRendererLogFileInjectable);
     const unmountRootComponent = di.inject(unmountRootComponentInjectable);
 
-    const onCloseFrame = once(() => {
-      logger.info(`Unload dashboard, clusterId=${(cluster.id)}`);
-      closeFileLogging();
-      unmountRootComponent();
-    });
-
     return () => {
-      window.addEventListener("beforeunload", onCloseFrame);
-      window.addEventListener("pagehide", onCloseFrame);
+      window.addEventListener("beforeunload", () => {
+        logger.info(`Unload dashboard, clusterId=${(cluster.id)}`);
+        unmountRootComponent();
+      });
     };
   },
   causesSideEffects: true,
