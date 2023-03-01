b5fb7b932f69f61a0a47a4b7b6d6601b843569db
Janne Savolainen <janne.savolainen@live.fi>
2023-03-01 10:11:13 +0200

    Remove non agnostic timeslots from agnostic application
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- packages/technical-features/application/agnostic/src/start-application/triggers/until-ready-to-start-injection-token.ts
+++
@@ -1,8 +0,0 @@
-import { getInjectionToken } from "@ogre-tools/injectable";
-
-export type UntilReadyToStart = () => Promise<void>;
-
-export const untilReadyToStartInjectionToken =
-  getInjectionToken<UntilReadyToStart>({
-    id: "until-ready-to-start-injection-token",
-  });
