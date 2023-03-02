0775b6a42feeedb211629e294618494a6db83072
Sebastian Malton <sebastian@malton.name>
2023-03-02 11:03:32 -0500

    Fix type errors
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/common/utils/channel/request-from-channel-injection-token.ts
+++ packages/core/src/common/utils/channel/request-from-channel-injection-token.ts
@@ -6,8 +6,8 @@ import { getInjectionToken } from "@ogre-tools/injectable";
 import type { RequestChannel } from "./request-channel-listener-injection-token";
 
 export interface RequestFromChannel {
-  <Request, Response>(channel: RequestChannel<Request, Response>, request: Request): Promise<Response>;
-  <Response>(channel: RequestChannel<void, Response>): Promise<Response>;
+  <Request, Response>(channel: RequestChannel<Request, Response>, request: Request): Promise<Awaited<Response>>;
+  <Response>(channel: RequestChannel<void, Response>): Promise<Awaited<Response>>;
 }
 
 export const requestFromChannelInjectionToken = getInjectionToken<RequestFromChannel>({
