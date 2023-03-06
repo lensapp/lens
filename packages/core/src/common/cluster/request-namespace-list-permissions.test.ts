b13e4bc2d8f31c95b9373cd31d1dd1aab3782876
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2023-03-06 08:45:26 +0200

    fix requestNamespaceListPermissionsForInjectable
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>

---
+++ packages/core/src/common/cluster/request-namespace-list-permissions.test.ts
@@ -0,0 +1,246 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+
+import type { V1SubjectRulesReviewStatus } from "@kubernetes/client-node";
+import type { DiContainer } from "@ogre-tools/injectable";
+import { getDiForUnitTesting } from "../../main/getDiForUnitTesting";
+import type { RequestNamespaceListPermissionsFor } from "./request-namespace-list-permissions.injectable";
+import requestNamespaceListPermissionsForInjectable from "./request-namespace-list-permissions.injectable";
+
+const createFakeProxyConfig = (statusResponse: Promise<V1SubjectRulesReviewStatus>) => ({
+  makeApiClient: () => ({
+    createSelfSubjectRulesReview: (): Promise<V1SubjectRulesReviewStatus> => statusResponse,
+  }),
+});
+
+describe("requestNamespaceListPermissions", () => {
+  let di: DiContainer;
+  let requestNamespaceListPermissions: RequestNamespaceListPermissionsFor;
+
+  beforeEach(() => {
+    di = getDiForUnitTesting({ doGeneralOverrides: true });
+    requestNamespaceListPermissions = di.inject(requestNamespaceListPermissionsForInjectable);
+  });
+
+  describe("when api returns incomplete data", () => {
+    it("returns truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: true,
+          resourceRules: [],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when api rejects", () => {
+    it("returns truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve, reject) => reject("unknown error")),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when first resourceRule has all permissions for everything", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: ["*"],
+              verbs: ["*"],
+            },
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when first resourceRule has list permissions for everything", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: ["*"],
+              verbs: ["list"],
+            },
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when first resourceRule has list permissions for asked resource", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: [""],
+              resources: ["pods"],
+              verbs: ["list"],
+            },
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when last resourceRule has all permissions for everything", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+            {
+              apiGroups: ["*"],
+              verbs: ["*"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when last resourceRule has list permissions for everything", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+            {
+              apiGroups: ["*"],
+              verbs: ["list"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+
+  describe("when last resourceRule has list permissions for asked resource", () => {
+    it("return truthy function", async () => {
+      const requestPermissions = requestNamespaceListPermissions(createFakeProxyConfig(
+        new Promise((resolve) => resolve({
+          incomplete: false,
+          resourceRules: [
+            {
+              apiGroups: ["*"],
+              verbs: ["get"],
+            },
+            {
+              apiGroups: [""],
+              resources: ["pods"],
+              verbs: ["list"],
+            },
+          ],
+          nonResourceRules: [],
+        })),
+      ) as any);
+
+      const permissionCheck = await requestPermissions("fake-namespace");
+
+      expect(permissionCheck({
+        apiName: "pods",
+        group: "",
+        kind: "Pod",
+        namespaced: true,
+      })).toBeTruthy();
+    });
+  });
+});
