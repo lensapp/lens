5ba8ca2e25859153946a444e03e1136cb0d94f88
Sebastian Malton <sebastian@malton.name>
2023-03-06 12:43:57 -0800

    Add support for using release branch targetted PRs (#7291)
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/release-tool/src/index.ts
+++ packages/release-tool/src/index.ts
@@ -57,6 +57,7 @@ interface GithubPrData {
 
 interface ExtendedGithubPrData extends Omit<GithubPrData, "mergedAt"> {
   mergedAt: Date;
+  shouldAttemptCherryPick: boolean;
 }
 
 async function getCurrentBranch(): Promise<string> {
@@ -190,7 +191,7 @@ function sortExtendedGithubPrData(left: ExtendedGithubPrData, right: ExtendedGit
   return -1;
 }
 
-async function getRelevantPRs(previousReleasedVersion: string): Promise<ExtendedGithubPrData[]> {
+async function getRelevantPRs(previousReleasedVersion: string, baseBranch: string): Promise<ExtendedGithubPrData[]> {
   console.log("retrieving previous 200 PRs...");
 
   const milestone = formatVersionForPickingPrs(await getCurrentVersionOfSubPackage("core"));
@@ -200,7 +201,7 @@ async function getRelevantPRs(previousReleasedVersion: string): Promise<Extended
     "list",
     "--limit=500", // Should be big enough, if not we need to release more often ;)
     "--state=merged",
-    "--base=master",
+    `--base=${baseBranch}`,
     "--json mergeCommit,title,author,labels,number,milestone,mergedAt",
   ];
 
@@ -217,7 +218,11 @@ async function getRelevantPRs(previousReleasedVersion: string): Promise<Extended
     .filter(query => query.stdout)
     .map(query => query.pr)
     .filter(pr => pr.labels.every(label => label.name !== "skip-changelog"))
-    .map(pr => ({ ...pr, mergedAt: new Date(pr.mergedAt) } as ExtendedGithubPrData))
+    .map(pr => ({
+      ...pr,
+      mergedAt: new Date(pr.mergedAt),
+      shouldAttemptCherryPick: baseBranch === "master",
+    }))
     .sort(sortExtendedGithubPrData);
 }
 
@@ -300,7 +305,11 @@ async function cherryPickCommits(prs: ExtendedGithubPrData[]): Promise<void> {
   const cherryPickCommit = cherryPickCommitWith(rl);
 
   for (const pr of prs) {
-    await cherryPickCommit(pr.mergeCommit.oid);
+    if (pr.shouldAttemptCherryPick) {
+      await cherryPickCommit(pr.mergeCommit.oid);
+    } else {
+      console.log(`Skipping cherry picking of #${pr.number} - ${pr.title}`);
+    }
   }
 
   rl.close();
@@ -335,7 +344,12 @@ async function createRelease(): Promise<void> {
     await bumpPackageVersions();
   }
 
-  const relevantPrs = await getRelevantPRs(previousReleasedVersion);
+  const relevantPrs = await getRelevantPRs(previousReleasedVersion, "master");
+
+  if (prBase !== "master") {
+    relevantPrs.push(...await getRelevantPRs(previousReleasedVersion, prBase));
+  }
+
   const selectedPrs = await pickRelevantPrs(relevantPrs, isMasterBranch);
   const prBody = formatChangelog(previousReleasedVersion, selectedPrs);
 
