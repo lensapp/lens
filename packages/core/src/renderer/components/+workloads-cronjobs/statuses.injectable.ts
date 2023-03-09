f17acccda15338d4fd156544252618916d6547ba
Sebastian Malton <sebastian@malton.name>
2023-03-09 14:58:54 -0500

    Fix bug in computation of cronJob statuses
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/components/+workloads-cronjobs/statuses.injectable.ts
+++ packages/core/src/renderer/components/+workloads-cronjobs/statuses.injectable.ts
@@ -4,16 +4,17 @@
  */
 import { getInjectable } from "@ogre-tools/injectable";
 import { computed } from "mobx";
-import computeStatusCountsForOwnersInjectable from "../../utils/compute-status-counts.injectable";
 import cronJobStoreInjectable from "./store.injectable";
 
 const totalStatusesForCronJobsInSelectedNamespacesInjectable = getInjectable({
   id: "total-statuses-for-cron-jobs-in-selected-namespaces",
   instantiate: (di) => {
     const cronJobStore = di.inject(cronJobStoreInjectable);
-    const computeStatusCountsForOwners = di.inject(computeStatusCountsForOwnersInjectable);
 
-    return computed(() => computeStatusCountsForOwners(cronJobStore.contextItems));
+    return computed(() => new Map(cronJobStore.contextItems.map(cronJob => [
+      cronJob.getId(),
+      [cronJob.getStatus()],
+    ])));
   },
 });
 
