e99828b8e463a54aa134afced4ff2156076e9ccb
Janne Savolainen <janne.savolainen@live.fi>
2023-03-13 08:50:53 +0200

    Introduce test utils for rendering and running with thrown mobx reactions
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- packages/utility-features/test-utils/index.ts
+++ packages/utility-features/test-utils/index.ts
@@ -2,3 +2,5 @@ export * from "./src/flush-promises";
 export * from "./src/get-global-override-for-function";
 export * from "./src/get-global-override";
 export * from "./src/get-promise-status";
+export * from "./src/render-for";
+export * from "./src/run-with-thrown-mobx-reactions";
