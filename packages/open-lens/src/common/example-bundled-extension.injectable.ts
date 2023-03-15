99c3b3a07ad5870205cb90f7e514aaf6b4f1ef9b
Sebastian Malton <sebastian@malton.name>
2023-03-15 11:58:32 -0400

    Add explanatory comment for inline require
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/open-lens/src/common/example-bundled-extension.injectable.ts
+++ packages/open-lens/src/common/example-bundled-extension.injectable.ts
@@ -6,6 +6,12 @@ const exampleBundledExtensionInjectable = getInjectable({
   id: "example-bundled-extension",
   instantiate: (di) => ({
     manifest: exampleBundledExtensionManifest,
+    /**
+     * Inline `require` is needed as to delay the loading and execution of the JS file until it is needed.
+     *
+     * Futhermore there might be code that runs "during load" and shouldn't be executed until everything is
+     * setup for the extensions (ie globals).
+     */
     main: () => require("@k8slens/legacy-extension-example/main").default,
     renderer: () => require("@k8slens/legacy-extension-example/renderer").default,
   }),
