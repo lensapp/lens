64fc182b465500feba0d540e1602d56e318e13d6
Iku-turso <mikko.aspiala@gmail.com>
2023-03-17 10:02:22 +0200

    Fix code style
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- packages/core/src/renderer/components/+cluster/ui-block.injectable.tsx
+++ packages/core/src/renderer/components/+cluster/ui-block.injectable.tsx
@@ -2,7 +2,7 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import React from 'react';
+import React from "react";
 import { getInjectable } from "@ogre-tools/injectable";
 import { clusterOverviewUIBlockInjectionToken } from "@k8slens/metrics";
 
