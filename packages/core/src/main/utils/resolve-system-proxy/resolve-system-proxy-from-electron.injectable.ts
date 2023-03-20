6b99b4630237a0b23cf10bc29ee7ba39c1350dc5
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2023-03-20 15:25:04 +0200

    Fix resolveSystemProxyFromElectron causing a crash on quit (#7379)
    * destroy resolve system proxy window on before quit
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    * resolve system proxy window: load blank page
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    * disable paintWhenInitiallyHidden
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    
    ---------
    
    Signed-off-by: Jari Kolehmainen <jari.kolehmainen@gmail.com>
    Signed-off-by: Gabriel <gaccettola@mirantis.com>

--- packages/core/src/main/utils/resolve-system-proxy/resolve-system-proxy-from-electron.injectable.ts
+++ packages/core/src/main/utils/resolve-system-proxy/resolve-system-proxy-from-electron.injectable.ts
@@ -3,22 +3,19 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import electronBrowserWindowInjectable from "./electron-browser-window.injectable";
 import withErrorLoggingInjectable from "../../../common/utils/with-error-logging/with-error-logging.injectable";
+import resolveSystemProxyWindowInjectable from "./resolve-system-proxy-window.injectable";
 
 const resolveSystemProxyFromElectronInjectable = getInjectable({
   id: "resolve-system-proxy-from-electron",
 
   instantiate: (di) => {
-    const browserWindow = di.inject(electronBrowserWindowInjectable);
+    const helperWindow = di.inject(resolveSystemProxyWindowInjectable);
     const withErrorLoggingFor = di.inject(withErrorLoggingInjectable);
     const withErrorLogging = withErrorLoggingFor(() => "Error resolving proxy");
-    const hiddenWindow = browserWindow({
-      show: false,
-    });
-
+    
     return withErrorLogging(async (url: string) => {
-      return await hiddenWindow.webContents.session.resolveProxy(url);
+      return await helperWindow.webContents.session.resolveProxy(url);
     });
   },
 });
