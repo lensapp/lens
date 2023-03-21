df327dc10971f5d507979e80564ae8900738ef00
Alex Andreev <alex.andreev.email@gmail.com>
2023-03-21 15:43:53 +0300

    Create cluster-settings package
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

---
+++ packages/cluster-settings/src/index.ts
@@ -0,0 +1,2 @@
+export * from "../../core/src/renderer/components/cluster-settings/cluster-settings-menu-injection-token";
+export * from "../../core/src/renderer/components/cluster-settings/cluster-settings-component-injection-token";
