dd7a289576cae32b5dd8c437772376812af28b09
Janne Savolainen <janne.savolainen@live.fi>
2023-03-30 09:36:59 +0300

    Introduce package for discovering html elements in unit tests
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

---
+++ packages/utility-features/react-testing-library-discovery/index.ts
@@ -0,0 +1,13 @@
+export type {
+  Discover,
+  GetSingleElement,
+  QueryAllElements,
+  QuerySingleElement,
+} from "./src/discovery-of-html-elements";
+
+export {
+  discoverFor,
+  getSingleElement,
+  queryAllElements,
+  querySingleElement,
+} from "./src/discovery-of-html-elements";
