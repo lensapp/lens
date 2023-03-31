a0fea203004f1a98399e0ca18b8217df8f9d453d
Sebastian Malton <sebastian@malton.name>
2023-03-31 13:37:29 -0400

    Stop all extensions before quitting app (#7450)
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/main/stop-services-and-exit-app.injectable.ts
+++ packages/core/src/main/stop-services-and-exit-app.injectable.ts
@@ -8,6 +8,7 @@ import clusterManagerInjectable from "./cluster/manager.injectable";
 import loggerInjectable from "../common/logger.injectable";
 import closeAllWindowsInjectable from "./start-main-application/lens-window/hide-all-windows/close-all-windows.injectable";
 import emitAppEventInjectable from "../common/app-event-bus/emit-event.injectable";
+import stopAllExtensionsInjectable from "../features/extensions/stopping/main/stop-all.injectable";
 
 const stopServicesAndExitAppInjectable = getInjectable({
   id: "stop-services-and-exit-app",
@@ -18,11 +19,13 @@ const stopServicesAndExitAppInjectable = getInjectable({
     const logger = di.inject(loggerInjectable);
     const closeAllWindows = di.inject(closeAllWindowsInjectable);
     const emitAppEvent = di.inject(emitAppEventInjectable);
+    const stopAllExtensions = di.inject(stopAllExtensionsInjectable);
 
-    return () => {
+    return async () => {
       emitAppEvent({ name: "service", action: "close" });
       closeAllWindows();
       clusterManager.stop();
+      await stopAllExtensions();
       logger.info("SERVICE:QUIT");
       setTimeout(exitApp, 1000);
     };
