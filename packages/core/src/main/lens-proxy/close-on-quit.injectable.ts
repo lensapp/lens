5bd814be27b7925ef37c4bd63b351071e7eead89
Sebastian Malton <sebastian@malton.name>
2023-03-31 15:27:21 -0400

    Fix cherry-pick due to files being moved between 6.4 and 6.5
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/main/lens-proxy/close-on-quit.injectable.ts
+++ packages/core/src/main/lens-proxy/close-on-quit.injectable.ts
@@ -3,12 +3,13 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import { getInjectable } from "@ogre-tools/injectable";
-import { beforeQuitOfBackEndInjectionToken } from "../start-main-application/runnable-tokens/phases";
+import { beforeQuitOfBackEndInjectionToken } from "../start-main-application/runnable-tokens/before-quit-of-back-end-injection-token";
 import lensProxyInjectable from "./lens-proxy.injectable";
 
 const closeLensProxyOnQuitInjectable = getInjectable({
   id: "close-lens-proxy-on-quit",
   instantiate: (di) => ({
+    id: "close-lens-proxy-on-quit",
     run: async () => {
       const lensProxy = di.inject(lensProxyInjectable);
 
