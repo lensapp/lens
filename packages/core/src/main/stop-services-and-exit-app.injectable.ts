980b054fad149c6451dcc00eabe0076f68e03faa
Sebastian Malton <sebastian@malton.name>
2023-04-05 10:09:04 -0400

    chore: Remove closing windows on quit
    - Electron does this already
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/main/stop-services-and-exit-app.injectable.ts
+++ packages/core/src/main/stop-services-and-exit-app.injectable.ts
@@ -6,7 +6,6 @@ import { getInjectable } from "@ogre-tools/injectable";
 import forceAppExitInjectable from "./electron-app/features/force-app-exit.injectable";
 import clusterManagerInjectable from "./cluster/manager.injectable";
 import loggerInjectable from "../common/logger.injectable";
-import closeAllWindowsInjectable from "./start-main-application/lens-window/hide-all-windows/close-all-windows.injectable";
 import emitAppEventInjectable from "../common/app-event-bus/emit-event.injectable";
 
 const stopServicesAndExitAppInjectable = getInjectable({
@@ -16,12 +15,10 @@ const stopServicesAndExitAppInjectable = getInjectable({
     const forceAppExit = di.inject(forceAppExitInjectable);
     const clusterManager = di.inject(clusterManagerInjectable);
     const logger = di.inject(loggerInjectable);
-    const closeAllWindows = di.inject(closeAllWindowsInjectable);
     const emitAppEvent = di.inject(emitAppEventInjectable);
 
     return async () => {
       emitAppEvent({ name: "service", action: "close" });
-      closeAllWindows();
       clusterManager.stop();
       logger.info("SERVICE:QUIT");
       setTimeout(forceAppExit, 1000);
