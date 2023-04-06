35c3d31f2a0fbe2944c44cf43f655f696bc990fc
Sebastian Malton <sebastian@malton.name>
2023-04-06 11:20:20 -0400

    chore: Fix lint for @k8slens/application
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/features/cluster/kube-object-details/extension-api/reactively-hide-kube-object-detail-item.test.tsx
+++ packages/core/src/features/cluster/kube-object-details/extension-api/reactively-hide-kube-object-detail-item.test.tsx
@@ -3,12 +3,8 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 import type { RenderResult } from "@testing-library/react";
-import type {
-  ApplicationBuilder,
-} from "../../../../renderer/components/test-utils/get-application-builder";
-import {
-  getApplicationBuilder,
-} from "../../../../renderer/components/test-utils/get-application-builder";
+import type { ApplicationBuilder } from "../../../../renderer/components/test-utils/get-application-builder";
+import { getApplicationBuilder } from "../../../../renderer/components/test-utils/get-application-builder";
 import type { IObservableValue } from "mobx";
 import { runInAction, computed, observable } from "mobx";
 import React from "react";
@@ -16,12 +12,9 @@ import { KubeObject } from "../../../../common/k8s-api/kube-object";
 import apiManagerInjectable from "../../../../common/k8s-api/api-manager/manager.injectable";
 import type { KubeObjectStore } from "../../../../common/k8s-api/kube-object.store";
 import type { KubeApi } from "../../../../common/k8s-api/kube-api";
-import showDetailsInjectable
-  from "../../../../renderer/components/kube-detail-params/show-details.injectable";
+import showDetailsInjectable from "../../../../renderer/components/kube-detail-params/show-details.injectable";
 import assert from "assert";
-import type {
-  FakeExtensionOptions,
-} from "../../../../renderer/components/test-utils/get-extension-fake";
+import type { FakeExtensionOptions } from "../../../../renderer/components/test-utils/get-extension-fake";
 
 describe("reactively hide kube object detail item", () => {
   let builder: ApplicationBuilder;
