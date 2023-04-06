d6a930bf54cba7a444293bd7d76bc9c875437d6c
Alex Andreev <alex.andreev.email@gmail.com>
2023-04-06 13:12:50 +0300

    Adding webhook list and store
    Signed-off-by: Alex Andreev <alex.andreev.email@gmail.com>

---
+++ packages/core/src/renderer/components/+config-mutating-webhook-configurations/mutating-webhook-configuration-store.injectable.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import { kubeObjectStoreInjectionToken } from "../../../common/k8s-api/api-manager/kube-object-store-token";
+import { MutatingWebhookConfigurationStore } from "./mutating-webhook-configuration-store";
+import clusterFrameContextForNamespacedResourcesInjectable from "../../cluster-frame-context/for-namespaced-resources.injectable";
+import loggerInjectable from "../../../common/logger.injectable";
+import mutatingWebhookConfigurationApiInjectable
+  from "../../../common/k8s-api/endpoints/mutating-webhook-configuration-api.injectable";
+
+const mutatingWebhookConfigurationStoreInjectable = getInjectable({
+  id: "mutating-webhook-configuration-store",
+  instantiate: (di) => {
+    const api = di.inject(mutatingWebhookConfigurationApiInjectable);
+
+    return new MutatingWebhookConfigurationStore({
+      context: di.inject(clusterFrameContextForNamespacedResourcesInjectable),
+      logger: di.inject(loggerInjectable),
+    }, api);
+  },
+  injectionToken: kubeObjectStoreInjectionToken,
+});
+
+export default mutatingWebhookConfigurationStoreInjectable;
