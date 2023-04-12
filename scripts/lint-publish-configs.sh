f2f23109ba066d6c81ede86c2e87abd435582435
Sebastian Malton <sebastian@malton.name>
2023-04-12 13:54:43 -0400

    feat: Add lint pass to prevent future issues
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ scripts/lint-publish-configs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+PACKAGE_JSON_PATHS=$(find packages/* -type f -name package.json -not -path "*/node_modules/*")
+exitCode=0
+
+while IFS= read -r PACKAGE_JSON_PATH; do
+  PACKAGE_NAME=$(<"${PACKAGE_JSON_PATH}" jq .name)
+  PACKAGE_IS_PRIVATE=$(<"${PACKAGE_JSON_PATH}" jq .private)
+
+  if [[ "${PACKAGE_IS_PRIVATE}" == "true" ]]; then
+    continue
+  fi
+
+  PACKAGE_HAS_PUBLISH_CONFIG=$(<"${PACKAGE_JSON_PATH}" jq '.publishConfig != null')
+
+  if [[ "${PACKAGE_HAS_PUBLISH_CONFIG}" == "false" ]]; then
+    echo "${PACKAGE_NAME} is missing publish config"
+    exitCode=1
+  fi
+done <<< "${PACKAGE_JSON_PATHS}"
+
+exit "${exitCode}"
