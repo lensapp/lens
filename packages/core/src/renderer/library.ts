fef789bd498d622a8897d15130e8fefeef29d650
Sebastian Malton <sebastian@malton.name>
2023-04-17 11:08:02 -0400

    chore: Expose api to only bundled extensions
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/library.ts
+++ packages/core/src/renderer/library.ts
@@ -5,15 +5,17 @@
 import "./components/app.scss";
 import React from "react";
 import ReactDOM from "react-dom";
+import setStatusBarStatusInjectable from "./components/status-bar/set-status-bar-status.injectable";
 
 // @experimental
-export type {
-  Environments,
-} from "../extensions/as-legacy-globals-for-extension-api/legacy-global-di-for-extension-api";
-
+export type { Environments } from "../extensions/as-legacy-globals-for-extension-api/legacy-global-di-for-extension-api";
 export { nodeEnvInjectionToken } from "../common/vars/node-env-injection-token";
 export { registerLensCore } from "./register-lens-core";
-export { React, ReactDOM };
+export {
+  React,
+  ReactDOM,
+  setStatusBarStatusInjectable,
+};
 export * as Mobx from "mobx";
 export * as MobxReact from "mobx-react";
 export * as ReactRouter from "react-router";
