934784c588bb558412929ded3b93af89b220e63b
Janne Savolainen <janne.savolainen@live.fi>
2023-04-17 11:55:06 +0300

    chore: Export types for reusable props for Elements
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- packages/technical-features/ui-components/src/element/elements.ts
+++ packages/technical-features/ui-components/src/element/elements.ts
@@ -1,5 +1,10 @@
 import { ElementFor } from "./element";
 import type React from "react";
 
+type PropsByElement<T> = T extends React.ComponentType<infer I> ? I : never;
+
 export const Div = ElementFor<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>("div");
+export type DivProps = PropsByElement<typeof Div>;
+
 export const Span = ElementFor<HTMLSpanElement, React.HTMLAttributes<HTMLSpanElement>>("span");
+export type SpanProps = PropsByElement<typeof Span>;
