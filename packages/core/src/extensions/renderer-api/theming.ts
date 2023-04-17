ebc63091a1e84f94ae61337f64e8a0408a83c962
Sebastian Malton <sebastian@malton.name>
2023-04-17 11:04:00 -0400

    chore: Revert exposing api to legacy extension API
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/extensions/renderer-api/theming.ts
+++ packages/core/src/extensions/renderer-api/theming.ts
@@ -3,12 +3,8 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-import type { StatusBarStatus } from "../../renderer/components/status-bar/current-status.injectable";
-import type { SetStatusBarStatus } from "../../renderer/components/status-bar/set-status-bar-status.injectable";
-import setStatusBarStatusInjectable from "../../renderer/components/status-bar/set-status-bar-status.injectable";
 import activeThemeInjectable from "../../renderer/themes/active.injectable";
 import type { LensTheme } from "../../renderer/themes/lens-theme";
-import { asLegacyGlobalFunctionForExtensionApi } from "../as-legacy-globals-for-extension-api/as-legacy-global-function-for-extension-api";
 import { asLegacyGlobalForExtensionApi } from "../as-legacy-globals-for-extension-api/as-legacy-global-object-for-extension-api";
 
 export const activeTheme = asLegacyGlobalForExtensionApi(activeThemeInjectable);
@@ -20,10 +16,4 @@ export function getActiveTheme() {
   return activeTheme.get();
 }
 
-export const setStatusBarStatus = asLegacyGlobalFunctionForExtensionApi(setStatusBarStatusInjectable);
-
-export type {
-  LensTheme,
-  StatusBarStatus,
-  SetStatusBarStatus,
-};
+export type { LensTheme };
