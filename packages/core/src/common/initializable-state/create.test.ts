91c8cd384a233d0a0cc460cf9e1b0d67fc756667
Sebastian Malton <sebastian@malton.name>
2023-04-17 15:47:57 -0400

    chore: Introduce competition for InitializableState
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/common/initializable-state/create.test.ts
+++ packages/core/src/common/initializable-state/create.test.ts
@@ -5,11 +5,14 @@
 
 import type { AsyncFnMock } from "@async-fn/jest";
 import asyncFn from "@async-fn/jest";
-import type { DiContainer, Injectable } from "@ogre-tools/injectable";
+import type { Runnable } from "@k8slens/run-many";
+import { runManyFor } from "@k8slens/run-many";
+import type { DiContainer, Injectable, InjectionToken } from "@ogre-tools/injectable";
+import { createContainer, getInjectionToken } from "@ogre-tools/injectable";
 import { runInAction } from "mobx";
 import { getDiForUnitTesting } from "../../main/getDiForUnitTesting";
-import type { InitializableState } from "./create";
-import { createInitializableState } from "./create";
+import type { ImplInitializableInjectionTokensArgs, Initializable, InitializableState } from "./create";
+import { getInjectablesForInitializable, getInitializable, createInitializableState } from "./create";
 
 describe("InitializableState tests", () => {
   let di: DiContainer;
@@ -70,6 +73,75 @@ describe("InitializableState tests", () => {
           it("when init is called again, throws", async () => {
             await expect(() => state.init()).rejects.toThrow("Cannot initialize InitializableState(my-state) more than once");
           });
+        });
+      });
+    });
+  });
+});
+
+describe("InitializableTokens technical tests", () => {
+  let di: DiContainer;
+  let initializableToken: Initializable<number>;
+  let phase: InjectionToken<Runnable<void>, void>;
+
+  beforeEach(() => {
+    di = createContainer("irrelevant");
+
+    initializableToken = getInitializable("some-root-id");
+    phase = getInjectionToken({ id: "some-runnable-phase" });
+  });
+
+  it("throws given attempting to inject the state token", () => {
+    expect(() => di.inject(initializableToken.stateToken)).toThrowErrorMatchingInlineSnapshot(
+      `"Tried to inject non-registered injectable "irrelevant" -> "some-root-id-state-token"."`,
+    );
+  });
+
+  describe("given some implementation for initializableToken is registered", () => {
+    let mockInit: AsyncFnMock<ImplInitializableInjectionTokensArgs<number>["init"]>;
+
+    beforeEach(() => {
+      mockInit = asyncFn();
+
+      const { initializationInjectable, stateInjectable } = getInjectablesForInitializable({
+        init: mockInit,
+        phase,
+        token: initializableToken,
+      });
+
+      di.register(initializationInjectable, stateInjectable);
+    });
+
+    it("throws given attempting to inject the state token", () => {
+      expect(() => di.inject(initializableToken.stateToken)).toThrowErrorMatchingInlineSnapshot(
+        `"Tried to inject "some-root-id" before initialization was complete"`,
+      );
+    });
+
+    describe("given the phase is started to be run", () => {
+      let runManyPromise: Promise<void>;
+
+      beforeEach(() => {
+        runManyPromise = runManyFor(di)(phase)();
+      });
+
+      it("throws given attempting to inject the state token", () => {
+        expect(() => di.inject(initializableToken.stateToken)).toThrowErrorMatchingInlineSnapshot(
+          `"Tried to inject "some-root-id" before initialization was complete"`,
+        );
+      });
+
+      describe("when initialization is complete", () => {
+        beforeEach(async () => {
+          await mockInit.resolve(10);
+        });
+
+        it("initializes the state", () => {
+          expect(di.inject(initializableToken.stateToken)).toBe(10);
+        });
+
+        it("allows the runMany to complete", async () => {
+          await runManyPromise;
         });
       });
     });
