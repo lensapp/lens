eafd1efbc0a4bc14790e0c6c9446c9089b62b14e
Sebastian Malton <sebastian@malton.name>
2023-04-17 16:45:10 -0400

    chore: Remove InitializableState for loosing competition
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/common/initializable-state/create.test.ts
+++ packages/core/src/common/initializable-state/create.test.ts
@@ -7,77 +7,10 @@ import type { AsyncFnMock } from "@async-fn/jest";
 import asyncFn from "@async-fn/jest";
 import type { Runnable } from "@k8slens/run-many";
 import { runManyFor } from "@k8slens/run-many";
-import type { DiContainer, Injectable, InjectionToken } from "@ogre-tools/injectable";
+import type { DiContainer, InjectionToken } from "@ogre-tools/injectable";
 import { createContainer, getInjectionToken } from "@ogre-tools/injectable";
-import { runInAction } from "mobx";
-import { getDiForUnitTesting } from "../../main/getDiForUnitTesting";
-import type { ImplInitializableInjectionTokensArgs, Initializable, InitializableState } from "./create";
-import { getInjectablesForInitializable, getInitializable, createInitializableState } from "./create";
-
-describe("InitializableState tests", () => {
-  let di: DiContainer;
-
-  beforeEach(() => {
-    di = getDiForUnitTesting();
-  });
-
-  describe("when created", () => {
-    let stateInjectable: Injectable<InitializableState<number>, unknown, void>;
-    let initMock: AsyncFnMock<() => number>;
-
-    beforeEach(() => {
-      initMock = asyncFn();
-      stateInjectable = createInitializableState({
-        id: "my-state",
-        init: initMock,
-      });
-
-      runInAction(() => {
-        di.register(stateInjectable);
-      });
-    });
-
-    describe("when injected", () => {
-      let state: InitializableState<number>;
-
-      beforeEach(() => {
-        state = di.inject(stateInjectable);
-      });
-
-      it("when get is called, throw", () => {
-        expect(() => state.get()).toThrowError("InitializableState(my-state) has not been initialized yet");
-      });
-
-      describe("when init is called", () => {
-        beforeEach(() => {
-          state.init();
-        });
-
-        it("should call provided initialization function", () => {
-          expect(initMock).toBeCalled();
-        });
-
-        it("when get is called, throw", () => {
-          expect(() => state.get()).toThrowError("InitializableState(my-state) has not finished initializing");
-        });
-
-        describe("when initialization resolves", () => {
-          beforeEach(async () => {
-            await initMock.resolve(42);
-          });
-
-          it("when get is called, returns value", () => {
-            expect(state.get()).toBe(42);
-          });
-
-          it("when init is called again, throws", async () => {
-            await expect(() => state.init()).rejects.toThrow("Cannot initialize InitializableState(my-state) more than once");
-          });
-        });
-      });
-    });
-  });
-});
+import type { ImplInitializableInjectionTokensArgs, Initializable } from "./create";
+import { getInjectablesForInitializable, getInitializable } from "./create";
 
 describe("InitializableTokens technical tests", () => {
   let di: DiContainer;
