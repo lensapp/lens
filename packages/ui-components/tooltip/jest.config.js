d713d796a47d0cc9bfd474229f79e0512d3a87ad
Sebastian Malton <sebastian@malton.name>
2023-04-19 09:20:37 -0400

    chore: Fix test failing
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/ui-components/tooltip/jest.config.js
+++ packages/ui-components/tooltip/jest.config.js
@@ -1,1 +1,13 @@
-module.exports = require("@k8slens/jest").monorepoPackageConfig(__dirname).configForReact;
+const { configForReact } = require("@k8slens/jest").monorepoPackageConfig(__dirname);
+
+module.exports = {
+  ...configForReact,
+  coverageThreshold: {
+    global: {
+      statements: 84,
+      branches: 67,
+      lines: 84,
+      functions: 92,
+    },
+  },
+};
