3439472065e6b850e286f6a34bccc23b827b8e28
Sebastian Malton <sebastian@malton.name>
2023-04-20 15:49:58 -0400

    feat: Improve formatting error messages from apiKube
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/common/utils/get-error-message.ts
+++ packages/core/src/common/utils/get-error-message.ts
@@ -2,6 +2,9 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
+
+import { JsonApiErrorParsed } from "../k8s-api/json-api";
+
 export const getErrorMessage = (error: unknown): string => {
   if (typeof error === "string") {
     return error;
@@ -9,6 +12,10 @@ export const getErrorMessage = (error: unknown): string => {
 
   if (error instanceof Error) {
     return error.message;
+  }
+
+  if (error instanceof JsonApiErrorParsed) {
+    return error.toString();
   }
 
   return JSON.stringify(error);
