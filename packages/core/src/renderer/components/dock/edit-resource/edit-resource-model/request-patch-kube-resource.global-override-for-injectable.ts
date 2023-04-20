5fb08960fe36f3e1b814473d5a8e8facafa7aab4
Sebastian Malton <sebastian@malton.name>
2023-04-20 16:10:23 -0400

    chore: Simplify global overrides of requestKubeResource and requestPatchKubeResource
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/components/dock/edit-resource/edit-resource-model/request-patch-kube-resource.global-override-for-injectable.ts
+++ packages/core/src/renderer/components/dock/edit-resource/edit-resource-model/request-patch-kube-resource.global-override-for-injectable.ts
@@ -2,11 +2,7 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { getGlobalOverride } from "@k8slens/test-utils";
+import { getGlobalOverrideForFunction } from "@k8slens/test-utils";
 import requestPatchKubeResourceInjectable from "./request-patch-kube-resource.injectable";
 
-export default getGlobalOverride(requestPatchKubeResourceInjectable, () => () => {
-  throw new Error(
-    "Tried to call patching of kube resource without explicit override.",
-  );
-});
+export default getGlobalOverrideForFunction(requestPatchKubeResourceInjectable);
