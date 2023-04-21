603afc19a715cf334d19f070e7efdf5a7997ecd4
Janne Savolainen <janne.savolainen@live.fi>
2023-04-21 16:01:09 +0300

    chore: Make creation of symlinks not fail when parent directory does not exist
    Co-authored-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    
    Signed-off-by: Janne Savolainen <janne.savolainen@live.fi>

--- packages/infrastructure/lens-link/src/fs/create-symlink.injectable.ts
+++ packages/infrastructure/lens-link/src/fs/create-symlink.injectable.ts
@@ -1,9 +1,15 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import fse from "fs-extra";
+import { dirname } from "path";
 
 export type CreateSymlink = (target: string, path: string, type: "dir" | "file") => Promise<void>;
 
 export const createSymlinkInjectable = getInjectable({
   id: "create-symlink",
-  instantiate: (): CreateSymlink => fse.symlink,
+
+  instantiate: (): CreateSymlink => async (target, path, type) => {
+    await fse.ensureDir(dirname(path));
+
+    return fse.symlink(target, path, type);
+  },
 });
