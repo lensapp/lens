ba8b1009e0e5e248100e24fa1107701fd7438029
Sebastian Malton <sebastian@malton.name>
2023-04-21 10:03:10 -0400

    feat: Introduce injectables to remove subNamespaces
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

---
+++ packages/core/src/renderer/components/+namespaces/request-delete-normal-namespace.injectable.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import { getInjectable } from "@ogre-tools/injectable";
+import type { Namespace } from "../../../common/k8s-api/endpoints";
+import namespaceStoreInjectable from "./store.injectable";
+
+export type RequestDeleteNormalNamespace = (namespace: Namespace) => Promise<void>;
+
+const requestDeleteNormalNamespaceInjectable = getInjectable({
+  id: "request-delete-normal-namespace",
+  instantiate: (di): RequestDeleteNormalNamespace => {
+    const namespaceStore = di.inject(namespaceStoreInjectable);
+
+    return (namespace) => namespaceStore.remove(namespace);
+  },
+});
+
+export default requestDeleteNormalNamespaceInjectable;
