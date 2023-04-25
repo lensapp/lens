e52469b387e24a6354e1ff2fea64ec838202fb3b
Gabriel <gaccettola@mirantis.com>
2023-04-25 22:01:46 +0200

    chore: updated deps
    Signed-off-by: Gabriel <gaccettola@mirantis.com>

--- packages/routing/index.ts
+++ packages/routing/index.ts
@@ -3,7 +3,7 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-export { historyInjectable, historyInjectionToken } from "./src/history.injectable";
+export { historyInjectionToken } from "./src/history.injectable";
 export { observableHistoryInjectionToken } from "./src/observable-history.injectable";
 export { searchParamsOptions } from "./src/search-params";
 export { routingFeature } from "./src/feature";
