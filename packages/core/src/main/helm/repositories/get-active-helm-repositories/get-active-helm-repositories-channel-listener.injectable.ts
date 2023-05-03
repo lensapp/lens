d8d6403b32bb8f6644b55d1ee6b5f9df85891070
Sebastian Malton <sebastian@malton.name>
2023-05-03 11:22:30 -0400

    chore: Fix type errors due to changing result error type
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/main/helm/repositories/get-active-helm-repositories/get-active-helm-repositories-channel-listener.injectable.ts
+++ packages/core/src/main/helm/repositories/get-active-helm-repositories/get-active-helm-repositories-channel-listener.injectable.ts
@@ -5,11 +5,24 @@
 import { getActiveHelmRepositoriesChannel } from "../../../../common/helm/get-active-helm-repositories-channel";
 import { getRequestChannelListenerInjectable } from "@k8slens/messaging";
 import getActiveHelmRepositoriesInjectable from "./get-active-helm-repositories.injectable";
+import { result } from "@k8slens/utilities";
 
 const getActiveHelmRepositoriesChannelListenerInjectable = getRequestChannelListenerInjectable({
   id: "get-active-helm-repositories-channel-listener",
   channel: getActiveHelmRepositoriesChannel,
-  getHandler: (di) => di.inject(getActiveHelmRepositoriesInjectable),
+  getHandler: (di) => {
+    const getActiveHelmRepositories = di.inject(getActiveHelmRepositoriesInjectable);
+
+    return async () => {
+      const helmResult = await getActiveHelmRepositories();
+
+      if (helmResult.isOk) {
+        return helmResult;
+      }
+
+      return result.error(helmResult.error.toString());
+    };
+  },
 });
 
 export default getActiveHelmRepositoriesChannelListenerInjectable;
