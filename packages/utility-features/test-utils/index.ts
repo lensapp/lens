0573fae1f605dc4218829f417408a863d9e91188
Sebastian Malton <sebastian@malton.name>
2023-05-05 12:40:08 -0400

    chore: Move describeIf and itIf to @k8slens/test-utils
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/utility-features/test-utils/index.ts
+++ packages/utility-features/test-utils/index.ts
@@ -4,3 +4,4 @@ export * from "./src/get-global-override";
 export * from "./src/get-promise-status";
 export * from "./src/render-for";
 export * from "./src/run-with-thrown-mobx-reactions";
+export * from "./src/skippers";
