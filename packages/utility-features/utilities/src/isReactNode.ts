cc9c246b659f28b538d78c173090a63e7d7f6cad
Sebastian Malton <sebastian@malton.name>
2023-05-10 10:15:27 -0400

    chore: Add documenting ref to StrictReactNode type
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/utility-features/utilities/src/isReactNode.ts
+++ packages/utility-features/utilities/src/isReactNode.ts
@@ -6,6 +6,8 @@
 import React from "react";
 import { isObject } from "./type-narrowing";
 
+// ref: https://changelog.com/posts/the-react-reactnode-type-is-a-black-hole
+
 export type StrictReactFragment =
   | {
       key?: string | number | null;
