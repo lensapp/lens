f3ccc35ed76cf4ce4406c2e45acd9a22651fe98e
Sebastian Malton <sebastian@malton.name>
2023-05-15 10:31:13 -0400

    chore: Fix IDs for some sidebar items
    - Also cleanup formatting of the sidebar item titles
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/src/renderer/components/workloads-cronjobs/sidebar-item.injectable.tsx
+++ packages/core/src/renderer/components/workloads-cronjobs/sidebar-item.injectable.tsx
@@ -17,7 +17,7 @@ const cronJobsSidebarItemInjectable = getInjectable({
 
     return {
       parentId: workloadsSidebarItemInjectable.id,
-      title: "CronJobs",
+      title: "Cron Jobs",
       onClick: di.inject(navigateToCronJobsInjectable),
       isActive: di.inject(routeIsActiveInjectable, route),
       isVisible: route.isEnabled,
