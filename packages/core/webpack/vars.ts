c054c577394f11bad88e59956eaa73d77811280c
Sebastian Malton <sebastian@malton.name>
2023-05-16 16:30:57 -0400

    chore: Simplify file route handling
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/core/webpack/vars.ts
+++ packages/core/webpack/vars.ts
@@ -12,13 +12,11 @@ export const mainDir = path.join(process.cwd(), "src", "main");
 export const buildDir = path.join(process.cwd(), "static", "build");
 export const extensionEntry = path.join(process.cwd(), "src", "extensions", "extension-api.ts");
 export const extensionOutDir = path.join(process.cwd(), "packages", "extensions", "dist");
-export const assetsFolderName = "assets";
 export const rendererDir = path.join(process.cwd(), "src", "renderer");
 export const appName = isDevelopment
   ? `${packageInfo.productName}Dev`
   : packageInfo.productName;
 export const htmlTemplate = path.resolve(rendererDir, "template.html");
-export const publicPath = "/build/";
 export const sassCommonVars = path.resolve(rendererDir, "components/vars.scss");
 export const webpackDevServerPort = Number(process.env.WEBPACK_DEV_SERVER_PORT) || 9191;
 
