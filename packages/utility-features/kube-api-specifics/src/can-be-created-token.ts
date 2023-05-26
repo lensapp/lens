347a6d396dfed40d1b78c504937acac16115cdab
Sebastian Malton <sebastian@malton.name>
2023-05-26 11:50:19 -0400

    chore: Move storesAndApisCanBeCreatedInjectionToken to @k8slens/kube-api-specifics
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/utility-features/kube-api-specifics/src/can-be-created-token.ts
+++ packages/utility-features/kube-api-specifics/src/can-be-created-token.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) OpenLens Authors. All rights reserved.
- * Licensed under MIT License. See LICENSE in root directory for more information.
- */
-
 import { getInjectionToken } from "@ogre-tools/injectable";
 
 export const storesAndApisCanBeCreatedInjectionToken = getInjectionToken<boolean>({
