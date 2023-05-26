cadc7e0a3071fc4a92837fa8d056a4008b6683ed
Sebastian Malton <sebastian@malton.name>
2023-05-26 15:41:41 -0400

    fix: Fix exported types not working correctly through @k8slens/extensions
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/extension-api/src/extension-api.ts
+++ packages/extension-api/src/extension-api.ts
@@ -2,6 +2,11 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
+import { mainExtensionApi as Main, commonExtensionApi as Common } from "@k8slens/core/main";
+import { rendererExtensionApi as Renderer } from "@k8slens/core/renderer";
 
-export { mainExtensionApi as Main, commonExtensionApi as Common } from "@k8slens/core/main";
-export { rendererExtensionApi as Renderer } from "@k8slens/core/renderer";
+export {
+  Main,
+  Common,
+  Renderer,
+};
