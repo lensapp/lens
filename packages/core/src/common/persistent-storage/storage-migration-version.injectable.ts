0a4c748c7cbadc1c0ec93426045dce765e692734
Jim Ehrismann <jehrismann@mirantis.com>
2023-05-30 08:57:59 -0400

    do not return a version if there are no migration declarations
    Signed-off-by: Jim Ehrismann <jehrismann@mirantis.com>

--- packages/core/src/common/persistent-storage/storage-migration-version.injectable.ts
+++ packages/core/src/common/persistent-storage/storage-migration-version.injectable.ts
@@ -16,6 +16,10 @@ const storageMigrationVersionInjectable = getInjectable({
   instantiate: (di, token) => {
     const declarations = di.injectMany(token);
 
+    if (declarations.length === 0) {
+      return undefined;
+    }
+
     return declarations.reduce((version, decl) => {
       if (semver.gte(decl.version, version)) {
         return decl.version;
