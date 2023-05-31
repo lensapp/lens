3be3feee39023190568d854852f97b55ca1a0153
Iku-turso <mikko.aspiala@gmail.com>
2023-05-31 17:28:02 +0300

    feat: Make lens-webpack-build cope long lasting processes, eg. watch
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    
    Signed-off-by: Mikko Aspiala <mikko.aspiala@gmail.com>
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- packages/infrastructure/webpack/src/scripts/exec.injectable.ts
+++ packages/infrastructure/webpack/src/scripts/exec.injectable.ts
@@ -1,12 +1,9 @@
 import { getInjectable } from "@ogre-tools/injectable";
 import { exec } from "child_process";
-import { promisify } from "util";
 
-const promisifiedExec = promisify(exec);
-
-export type Exec = typeof promisifiedExec;
+export type Exec = typeof exec;
 
 export const execInjectable = getInjectable({
   id: "exec",
-  instantiate: () => promisifiedExec,
+  instantiate: () => exec,
 });
