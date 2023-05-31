878ec5bbebd2daff73afe1ddf3e537a8e1695e0b
Gabriel <gaccettola@mirantis.com>
2023-05-31 14:44:11 +0200

    chore: bump package.json
    Signed-off-by: Gabriel <gaccettola@mirantis.com>

--- packages/core/src/common/utils/get-random-id.global-override-for-injectable.ts
+++ packages/core/src/common/utils/get-random-id.global-override-for-injectable.ts
@@ -4,6 +4,6 @@
  */
 
 import { getGlobalOverride } from "@k8slens/test-utils";
-import { getRandomIdInjectable } from "@k8slens/random";
+import { getRandomIdInjectionToken } from "@k8slens/random";
 
-export default getGlobalOverride(getRandomIdInjectable, () => () => "some-irrelevant-random-id");
+export default getGlobalOverride(getRandomIdInjectionToken, () => () => "some-irrelevant-random-id");
--- packages/random-id/src/get-random-id.injectable.ts
+++ packages/random-id/src/get-random-id.injectable.ts
@@ -2,11 +2,17 @@
  * Copyright (c) OpenLens Authors. All rights reserved.
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
-import { getInjectable } from "@ogre-tools/injectable";
+import { getInjectable, getInjectionToken } from "@ogre-tools/injectable";
 import { v4 as getRandomId } from "uuid";
+
+export type GetRandomId = () => string;
+export const getRandomIdInjectionToken = getInjectionToken<GetRandomId>({
+  id: "get-random-id-injection-token",
+});
 
 export const getRandomIdInjectable = getInjectable({
   id: "get-random-id",
-  instantiate: () => () => getRandomId(),
+  instantiate: (): GetRandomId => () => getRandomId(),
   causesSideEffects: true,
+  injectionToken: getRandomIdInjectionToken,
 });
