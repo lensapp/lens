dbb09b374a285ddd7a4faa1854e41476425edb9f
Juho Heikka <juho.heikka@gmail.com>
2023-06-01 15:10:43 +0300

    Add warning when no detected provider
    Signed-off-by: Juho Heikka <juho.heikka@gmail.com>

---
+++ packages/core/src/renderer/components/cluster-settings/no-prometheus-provider-detectec.tsx
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) OpenLens Authors. All rights reserved.
+ * Licensed under MIT License. See LICENSE in root directory for more information.
+ */
+import React from "react";
+import { SubTitle } from "../layout/sub-title";
+import { Icon } from "../icon";
+
+
+export const NoPrometheusProviderDetected = () => (
+  <section>
+    <SubTitle title="Auto detected prometheus details" />
+    <div className="flex gaps align-center">
+      <Icon material="error_outline" />
+      <div>Could not detect any Prometheus provider.</div>
+    </div>
+  </section>
+);
